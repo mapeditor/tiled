179081ee032d5b554cc5e0d99f4f271482e5ac64
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2009-09-09 18:04:05 +0200

    Fixed a compilation issue with Qt <= 4.5

--- src/tileselectionitem.h
+++ src/tileselectionitem.h
@@ -37,7 +37,9 @@ class TileSelectionItem : public QObject,
                           public QGraphicsItem
 {
     Q_OBJECT
+#if QT_VERSION >= 0x040600
     Q_INTERFACES(QGraphicsItem)
+#endif
 
 public:
     /**
