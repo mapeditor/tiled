8d519fa7a4c22adc8d14d40e1364ba53f3d56f52
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2010-02-09 20:40:19 +0100

    Don't show icons in menus and use the unified toolbar on Mac OS X
    Looks better.

--- src/main.cpp
+++ src/main.cpp
@@ -102,6 +102,9 @@ int main(int argc, char *argv[])
     a.setOrganizationDomain(QLatin1String("mapeditor.org"));
     a.setApplicationName(QLatin1String("Tiled"));
     a.setApplicationVersion(QLatin1String("0.4.0"));
+#ifdef Q_WS_MAC
+    a.setAttribute(Qt::AA_DontShowIconsInMenus);
+#endif
 
     LanguageManager *languageManager = LanguageManager::instance();
     languageManager->installTranslators();
