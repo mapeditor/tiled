60aecd5d2b9e1910f39352e800e8f3fafe9b8049
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2010-06-24 00:12:48 +0200

    Changed the perspective walls example to use an external tileset
    Also introduced some example tile properties, which is the main reason
    why external tilesets make sense.

---
+++ examples/perspective_walls.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="perspective_walls" tilewidth="64" tileheight="64">
+ <image source="perspective_walls.png"/>
+ <tile id="13">
+  <properties>
+   <property name="door" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="door" value="true"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="pickup" value="true"/>
+  </properties>
+ </tile>
+</tileset>
