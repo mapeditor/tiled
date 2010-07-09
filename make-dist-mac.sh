8c2edef2660249bf3b7c6c7576492f112c32c818
Jeff Bland <jksb@member.fsf.org>
2010-07-09 05:04:15 -0600

    Mac release script now uses macdeployqt and includes the examples directory

--- make-dist-mac.sh
+++ make-dist-mac.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This script generates a mac release from an already
 # compiled Tiled.app in the bin folder.
-# TODO: look at replacing some of this with macdeployqt
 
 # Get the version
 if [ "$#" -eq "0" ]; then
@@ -11,7 +10,6 @@ fi
 name="tiled-qt-$1"
 
 # Get various directories
-qtFrameworkDir="/Library/Frameworks"
 baseDir=`dirname $01`
 binDir="$baseDir/bin"
 
@@ -24,6 +22,7 @@ cp "$baseDir/AUTHORS" "$tempDir/"
 cp "$baseDir/COPYING" "$tempDir/"
 cp "$baseDir/NEWS" "$tempDir/"
 cp "$baseDir/README" "$tempDir/"
+cp -R "$baseDir/examples" "$tempDir/"
 cp -R "$binDir/Tiled.app" "$tempDir/"
 
 # Create symlink to application directory
@@ -34,31 +33,14 @@ pluginsDir="$tempDir/Tiled.app/Contents/PlugIns"
 macOSDir="$tempDir/Tiled.app/Contents/MacOS"
 frameworksDir="$tempDir/Tiled.app/Contents/Frameworks"
 
-# Copy Qt frameworks to app bundle
-cp -R "$qtFrameworkDir/QtCore.framework" "$frameworksDir/"
-cp -R "$qtFrameworkDir/QtGui.framework" "$frameworksDir/"
+# Use macdeployqt to copy Qt frameworks to the app
+macdeployqt "$tempDir/Tiled.app"
 qtCoreDir="$frameworksDir/QtCore.framework"
 qtGuiDir="$frameworksDir/QtGui.framework"
 
-# Delete debug and header files from Qt frameworks
-rm -rf "$qtCoreDir/"*debug*
-rm -rf "$qtGuiDir/"*debug*
-rm -rf "$qtCoreDir/Versions/4/"*debug*
-rm -rf "$qtGuiDir/Versions/4/"*debug*
-rm -rf "$qtCoreDir/Versions/4/Headers"
-rm -rf "$qtGuiDir/Versions/4/Headers"
-
-# Modify Qt frameworks to have correct self identification names
-install_name_tool -id "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$qtCoreDir/Versions/4/QtCore"
-install_name_tool -id "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$qtGuiDir/Versions/4/QtGui"
-
-# Modify all code to link in local Qt frameworks
-install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$qtGuiDir/Versions/4/QtGui"
-install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$macOSDir/Tiled"
+# Modify libtiled and plugins to use local Qt frameworks (perhaps theres some way to get macdeployqt to do this?)
 install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$frameworksDir/libtiled.dylib"
 install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$pluginsDir/libtmw.dylib"
-
-install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$macOSDir/Tiled"
 install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$frameworksDir/libtiled.dylib"
 install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$pluginsDir/libtmw.dylib"
 
