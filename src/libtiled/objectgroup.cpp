4e62aa88842fa5e606d78bed04d55478634dbef1
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2011-04-04 21:34:53 +0200

    Fixed object layers losing their color when resizing the map
    The color was getting lost when cloning an ObjectGroup, which affected
    map resizing and layer duplication.
    
    Mantis-issue: 83

--- src/libtiled/objectgroup.cpp
+++ src/libtiled/objectgroup.cpp
@@ -141,5 +141,6 @@ ObjectGroup *ObjectGroup::initializeClone(ObjectGroup *clone) const
     Layer::initializeClone(clone);
     foreach (MapObject *object, mObjects)
         clone->addObject(object->clone());
+    clone->setColor(mColor);
     return clone;
 }
