mapeditor/tiled

commit 10a8b0e3b4290b8abcc642b6b481c11f54fb7673

Added copy/paste support for objects
This closes #2

src/tiled/toolmanager.cpp
@@ -129,6 +129,9 @@ void ToolManager::addSeparator()
 
 void ToolManager::selectTool(AbstractTool *tool)
 {
+    if (!tool->isEnabled()) // Refuse to select disabled tools
+        return;
+
     foreach (QAction *action, mActionGroup->actions()) {
         if (action->data().value<AbstractTool*>() == tool) {
             action->trigger();

src/libtiled/layer.h
@@ -34,6 +34,7 @@
 
 #include <QPixmap>
 #include <QRect>
+#include <QSet>
 #include <QString>
 #include <QVector>
 
@@ -132,9 +133,21 @@ public:
     QRect bounds() const { return QRect(mX, mY, mWidth, mHeight); }
 
     /**
+     * Computes and returns the set of tilesets used by this layer.
+     */
+    virtual QSet<Tileset*> usedTilesets() const = 0;
+
+    /**
      * Returns whether this layer is referencing the given tileset.
      */
     virtual bool referencesTileset(const Tileset *tileset) const = 0;
+
+    /**
+     * Replaces all references to tiles from \a oldTileset with tiles from
+     * \a newTileset.
+     */
+    virtual void replaceReferencesToTileset(Tileset *oldTileset,
+                                            Tileset *newTileset) = 0;
 
     /**
      * Resizes this layer to \a size, while shifting its contents by \a offset.

src/libtiled/map.cpp
@@ -141,8 +141,7 @@ void Map::replaceTileset(Tileset *oldTileset, Tileset *newTileset)
     Q_ASSERT(index != -1);
 
     foreach (Layer *layer, mLayers)
-        if (TileLayer *tileLayer = layer->asTileLayer())
-            tileLayer->replaceReferencesToTileset(oldTileset, newTileset);
+        layer->replaceReferencesToTileset(oldTileset, newTileset);
 
     mTilesets.replace(index, newTileset);
 }

src/libtiled/objectgroup.cpp
@@ -33,6 +33,7 @@
 #include "map.h"
 #include "mapobject.h"
 #include "tile.h"
+#include "tileset.h"
 
 using namespace Tiled;
 
@@ -69,6 +70,17 @@ int ObjectGroup::removeObject(MapObject *object)
     return index;
 }
 
+QSet<Tileset*> ObjectGroup::usedTilesets() const
+{
+    QSet<Tileset*> tilesets;
+
+    foreach (const MapObject *object, mObjects)
+        if (const Tile *tile = object->tile())
+            tilesets.insert(tile->tileset());
+
+    return tilesets;
+}
+
 bool ObjectGroup::referencesTileset(const Tileset *tileset) const
 {
     foreach (const MapObject *object, mObjects) {
@@ -78,6 +90,16 @@ bool ObjectGroup::referencesTileset(const Tileset *tileset) const
     }
 
     return false;
+}
+
+void ObjectGroup::replaceReferencesToTileset(Tileset *oldTileset,
+                                             Tileset *newTileset)
+{
+    foreach (MapObject *object, mObjects) {
+        const Tile *tile = object->tile();
+        if (tile && tile->tileset() == oldTileset)
+            object->setTile(newTileset->tileAt(tile->id()));
+    }
 }
 
 void ObjectGroup::resize(const QSize &size, const QPoint &offset)

src/libtiled/objectgroup.h
@@ -64,6 +64,11 @@ public:
     const QList<MapObject*> &objects() const { return mObjects; }
 
     /**
+     * Returns the number of objects in this object group.
+     */
+    int objectCount() const { return mObjects.size(); }
+
+    /**
      * Adds an object to this object group.
      */
     void addObject(MapObject *object);
@@ -84,10 +89,21 @@ public:
     int removeObject(MapObject *object);
 
     /**
+     * Computes and returns the set of tilesets used by this object group.
+     */
+    QSet<Tileset*> usedTilesets() const;
+
+    /**
      * Returns whether any tile objects in this object group reference tiles
      * in the given tileset.
      */
     bool referencesTileset(const Tileset *tileset) const;
+
+    /**
+     * Replaces all references to tiles from \a oldTileset with tiles from
+     * \a newTileset.
+     */
+    void replaceReferencesToTileset(Tileset *oldTileset, Tileset *newTileset);
 
     /**
      * Resizes this object group to \a size, while shifting all objects by

src/libtiled/tilelayer.h
@@ -34,7 +34,6 @@
 
 #include "layer.h"
 
-#include <QSet>
 #include <QString>
 #include <QVector>
 

src/tiled/clipboardmanager.cpp
@@ -22,6 +22,8 @@
 
 #include "map.h"
 #include "mapdocument.h"
+#include "mapobject.h"
+#include "objectgroup.h"
 #include "tmxmapreader.h"
 #include "tmxmapwriter.h"
 #include "tile.h"
@@ -75,29 +77,36 @@ void ClipboardManager::copySelection(const MapDocument *mapDocument)
         return;
 
     const Map *map = mapDocument->map();
+    const QRegion &tileSelection = mapDocument->tileSelection();
+    const QList<MapObject*> &selectedObjects = mapDocument->selectedObjects();
     const Layer *layer = map->layerAt(currentLayer);
     const TileLayer *tileLayer = dynamic_cast<const TileLayer*>(layer);
-    if (!tileLayer)
-        return;
+    Layer *copyLayer = 0;
 
-    const QRegion &selection = mapDocument->tileSelection();
-    if (selection.isEmpty())
+    if (!tileSelection.isEmpty() && tileLayer) {
+        // Copy the selected part of the layer
+        copyLayer = tileLayer->copy(tileSelection.translated(-tileLayer->x(),
+                                                             -tileLayer->y()));
+    } else if (!selectedObjects.isEmpty()) {
+        // Create a new object group with clones of the selected objects
+        ObjectGroup *objectGroup = new ObjectGroup(QString(), 0, 0, 0, 0);
+        foreach (const MapObject *mapObject, selectedObjects)
+            objectGroup->addObject(mapObject->clone());
+        copyLayer = objectGroup;
+    } else {
         return;
-
-    // Copy the selected part of the layer
-    TileLayer *copy = tileLayer->copy(selection.translated(-tileLayer->x(),
-                                                           -tileLayer->y()));
+    }
 
     // Create a temporary map to write to the clipboard
     Map copyMap(map->orientation(),
-                copy->width(), copy->height(),
+                copyLayer->width(), copyLayer->height(),
                 map->tileWidth(), map->tileHeight());
 
     // Resolve the set of tilesets used by this layer
-    foreach (Tileset *tileset, copy->usedTilesets())
+    foreach (Tileset *tileset, copyLayer->usedTilesets())
         copyMap.addTileset(tileset);
 
-    copyMap.addLayer(copy);
+    copyMap.addLayer(copyLayer);
 
     setMap(&copyMap);
 }

src/tiled/clipboardmanager.h
@@ -61,6 +61,7 @@ public:
 
     /**
      * Convenience method to copy the current selection to the clipboard.
+     * Deals with either tile selection or object selection.
      */
     void copySelection(const MapDocument *mapDocument);
 

commit 0a0b2a0740fc77ffd8b6ca447c96878d7b60e43f

Fixed crash when all tools are disabled
Introduced in 10a8b0e3b4290b8abcc642b6b481c11f54fb7673.

src/tiled/toolmanager.cpp
@@ -129,7 +129,7 @@ void ToolManager::addSeparator()
 
 void ToolManager::selectTool(AbstractTool *tool)
 {
-    if (!tool->isEnabled()) // Refuse to select disabled tools
+    if (tool && !tool->isEnabled()) // Refuse to select disabled tools
         return;
 
     foreach (QAction *action, mActionGroup->actions()) {