mapeditor/tiled

commit 67515de5abea3972cc85c54738e31b6d71afc03b

Introduced the GidMapper class to convert between gids and cells
This brings the two conversions together and allows reusing this
functionality in other map readers and writers.

src/libtiled/mapreader.cpp
@@ -31,6 +31,7 @@
 #include "mapreader.h"
 
 #include "compression.h"
+#include "gidmapper.h"
 #include "objectgroup.h"
 #include "map.h"
 #include "mapobject.h"
@@ -42,15 +43,10 @@
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
-#include <QMap>
 #include <QXmlStreamReader>
 
 using namespace Tiled;
 using namespace Tiled::Internal;
-
-// Bits on the far end of the 32-bit global tile ID are used for tile flags
-const int FlippedHorizontallyFlag = 0x80000000;
-const int FlippedVerticallyFlag   = 0x40000000;
 
 namespace Tiled {
 namespace Internal {
@@ -111,7 +107,7 @@ private:
     QString mError;
     QString mPath;
     Map *mMap;
-    QMap<uint, Tileset*> mGidsToTileset;
+    GidMapper mGidMapper;
     bool mReadingExternalTileset;
 
     QXmlStreamReader xml;
@@ -134,7 +130,7 @@ Map *MapReaderPrivate::readMap(QIODevice *device, const QString &path)
         xml.raiseError(tr("Not a map file."));
     }
 
-    mGidsToTileset.clear();
+    mGidMapper.clear();
     return map;
 }
 
@@ -303,7 +299,7 @@ Tileset *MapReaderPrivate::readTileset()
     }
 
     if (tileset && !mReadingExternalTileset)
-        mGidsToTileset.insert(firstGid, tileset);
+        mGidMapper.insert(firstGid, tileset);
 
     return tileset;
 }
@@ -546,27 +542,11 @@ Cell MapReaderPrivate::cellForGid(uint gid, bool &ok)
 {
     Cell result;
 
-    if (gid == 0) {
-        ok = true;
-    } else if (mGidsToTileset.isEmpty()) {
+    if (mGidMapper.isEmpty()) {
         xml.raiseError(tr("Tile used but no tilesets specified"));
         ok = false;
     } else {
-        // Read out the flags
-        result.flippedHorizontally = (gid & FlippedHorizontallyFlag);
-        result.flippedVertically = (gid & FlippedVerticallyFlag);
-
-        // Clear the flags
-        gid &= ~(FlippedHorizontallyFlag | FlippedVerticallyFlag);
-
-        // Find the tileset containing this tile
-        QMap<uint, Tileset*>::const_iterator i = mGidsToTileset.upperBound(gid);
-        --i; // Navigate one tileset back since upper bound finds the next
-        const int tileId = gid - i.key();
-        const Tileset *tileset = i.value();
-
-        result.tile = tileset ? tileset->tileAt(tileId) : 0;
-        ok = true;
+        result = mGidMapper.gidToCell(gid, ok);
     }
 
     return result;

src/libtiled/gidmapper.cpp
@@ -1,0 +1,89 @@
+/*
+ * gidmapper.cpp
+ * Copyright 2011, Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
+ *
+ * This file is part of libtiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gidmapper.h"
+
+#include "tile.h"
+#include "tileset.h"
+#include "map.h"
+
+using namespace Tiled;
+
+// Bits on the far end of the 32-bit global tile ID are used for tile flags
+const int FlippedHorizontallyFlag = 0x80000000;
+const int FlippedVerticallyFlag   = 0x40000000;
+
+GidMapper::GidMapper()
+{
+}
+
+Cell GidMapper::gidToCell(uint gid, bool &ok) const
+{
+    Cell result;
+
+    // Read out the flags
+    result.flippedHorizontally = (gid & FlippedHorizontallyFlag);
+    result.flippedVertically = (gid & FlippedVerticallyFlag);
+
+    // Clear the flags
+    gid &= ~(FlippedHorizontallyFlag | FlippedVerticallyFlag);
+
+    if (gid == 0) {
+        ok = true;
+    } else if (isEmpty()) {
+        ok = false;
+    } else {
+        // Find the tileset containing this tile
+        QMap<uint, Tileset*>::const_iterator i = mFirstGidToTileset.upperBound(gid);
+        --i; // Navigate one tileset back since upper bound finds the next
+        const int tileId = gid - i.key();
+        const Tileset *tileset = i.value();
+
+        result.tile = tileset ? tileset->tileAt(tileId) : 0;
+        ok = true;
+    }
+
+    return result;
+}
+
+uint GidMapper::cellToGid(const Cell &cell) const
+{
+    if (cell.isEmpty())
+        return 0;
+
+    const Tileset *tileset = cell.tile->tileset();
+
+    // Find the first GID for the tileset
+    QMap<uint, Tileset*>::const_iterator i = mFirstGidToTileset.begin();
+    QMap<uint, Tileset*>::const_iterator i_end = mFirstGidToTileset.end();
+    while (i != i_end && i.value() != tileset)
+        ++i;
+
+    if (i == i_end) // tileset not found
+        return 0;
+
+    uint gid = i.key() + cell.tile->id();
+    if (cell.flippedHorizontally)
+        gid |= FlippedHorizontallyFlag;
+    if (cell.flippedVertically)
+        gid |= FlippedVerticallyFlag;
+
+    return gid;
+}

src/libtiled/gidmapper.h
@@ -1,0 +1,76 @@
+/*
+ * gidmapper.h
+ * Copyright 2011, Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
+ *
+ * This file is part of libtiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TILED_GIDMAPPER_H
+#define TILED_GIDMAPPER_H
+
+#include "tilelayer.h"
+
+#include <QMap>
+
+namespace Tiled {
+
+/**
+ * A class that maps cells to global IDs (gids) and back.
+ */
+class TILEDSHARED_EXPORT GidMapper
+{
+public:
+    /**
+     * Default constructor. Use \l insert to initialize the gid mapper
+     * incrementally.
+     */
+    GidMapper();
+
+    /**
+     * Insert the given \a tileset with \a firstGid as its first global ID.
+     */
+    void insert(uint firstGid, Tileset *tileset)
+    { mFirstGidToTileset.insert(firstGid, tileset); }
+
+    /**
+     * Clears the gid mapper, so that it can be reused.
+     */
+    void clear() { mFirstGidToTileset.clear(); }
+
+    /**
+     * Returns true when no tilesets are known to this gid mapper.
+     */
+    bool isEmpty() const { return mFirstGidToTileset.isEmpty(); }
+
+    /**
+     * Returns the cell data matched by the given \a gid. The \a ok parameter
+     * indicates whether an error occurred.
+     */
+    Cell gidToCell(uint gid, bool &ok) const;
+
+    /**
+     * Returns the global tile ID for the given \a cell. Returns 0 when the
+     * cell is empty or when its tileset isn't known.
+     */
+    uint cellToGid(const Cell &cell) const;
+
+private:
+    QMap<uint, Tileset*> mFirstGidToTileset;
+};
+
+} // namespace Tiled
+
+#endif // TILED_GIDMAPPER_H

src/libtiled/libtiled.pro
@@ -31,7 +31,8 @@ SOURCES += compression.cpp \
     orthogonalrenderer.cpp \
     properties.cpp \
     tilelayer.cpp \
-    tileset.cpp
+    tileset.cpp \
+    gidmapper.cpp
 HEADERS += compression.h \
     isometricrenderer.h \
     layer.h \
@@ -47,7 +48,8 @@ HEADERS += compression.h \
     tile.h \
     tiled_global.h \
     tilelayer.h \
-    tileset.h
+    tileset.h \
+    gidmapper.h
 macx {
     contains(QT_CONFIG, ppc):CONFIG += x86 \
         ppc

src/libtiled/mapwriter.cpp
@@ -31,6 +31,7 @@
 #include "mapwriter.h"
 
 #include "compression.h"
+#include "gidmapper.h"
 #include "map.h"
 #include "mapobject.h"
 #include "objectgroup.h"
@@ -40,15 +41,10 @@
 
 #include <QCoreApplication>
 #include <QDir>
-#include <QMap>
 #include <QXmlStreamWriter>
 
 using namespace Tiled;
 using namespace Tiled::Internal;
-
-// Bits on the far end of the 32-bit global tile ID are used for tile flags
-const int FlippedHorizontallyFlag = 0x80000000;
-const int FlippedVerticallyFlag   = 0x40000000;
 
 namespace Tiled {
 namespace Internal {
@@ -78,14 +74,13 @@ private:
                       uint firstGid);
     void writeTileLayer(QXmlStreamWriter &w, const TileLayer *tileLayer);
     void writeLayerAttributes(QXmlStreamWriter &w, const Layer *layer);
-    uint gidForCell(const Cell &cell) const;
     void writeObjectGroup(QXmlStreamWriter &w, const ObjectGroup *objectGroup);
     void writeObject(QXmlStreamWriter &w, const MapObject *mapObject);
     void writeProperties(QXmlStreamWriter &w,
                          const Properties &properties);
 
     QDir mMapDir;     // The directory in which the map is being saved
-    QMap<uint, const Tileset*> mFirstGidToTileset;
+    GidMapper mGidMapper;
     bool mUseAbsolutePaths;
 };
 
@@ -189,11 +184,11 @@ void MapWriterPrivate::writeMap(QXmlStreamWriter &w, const Map *map)
 
     writeProperties(w, map->properties());
 
-    mFirstGidToTileset.clear();
+    mGidMapper.clear();
     uint firstGid = 1;
-    foreach (const Tileset *tileset, map->tilesets()) {
+    foreach (Tileset *tileset, map->tilesets()) {
         writeTileset(w, tileset, firstGid);
-        mFirstGidToTileset.insert(firstGid, tileset);
+        mGidMapper.insert(firstGid, tileset);
         firstGid += tileset->tileCount();
     }
 
@@ -310,7 +305,7 @@ void MapWriterPrivate::writeTileLayer(QXmlStreamWriter &w,
     if (mLayerDataFormat == MapWriter::XML) {
         for (int y = 0; y < tileLayer->height(); ++y) {
             for (int x = 0; x < tileLayer->width(); ++x) {
-                const uint gid = gidForCell(tileLayer->cellAt(x, y));
+                const uint gid = mGidMapper.cellToGid(tileLayer->cellAt(x, y));
                 w.writeStartElement(QLatin1String("tile"));
                 w.writeAttribute(QLatin1String("gid"), QString::number(gid));
                 w.writeEndElement();
@@ -321,7 +316,7 @@ void MapWriterPrivate::writeTileLayer(QXmlStreamWriter &w,
 
         for (int y = 0; y < tileLayer->height(); ++y) {
             for (int x = 0; x < tileLayer->width(); ++x) {
-                const uint gid = gidForCell(tileLayer->cellAt(x, y));
+                const uint gid = mGidMapper.cellToGid(tileLayer->cellAt(x, y));
                 tileData.append(QString::number(gid));
                 if (x != tileLayer->width() - 1
                     || y != tileLayer->height() - 1)
@@ -338,7 +333,7 @@ void MapWriterPrivate::writeTileLayer(QXmlStreamWriter &w,
 
         for (int y = 0; y < tileLayer->height(); ++y) {
             for (int x = 0; x < tileLayer->width(); ++x) {
-                const uint gid = gidForCell(tileLayer->cellAt(x, y));
+                const uint gid = mGidMapper.cellToGid(tileLayer->cellAt(x, y));
                 tileData.append((char) (gid));
                 tileData.append((char) (gid >> 8));
                 tileData.append((char) (gid >> 16));
@@ -380,38 +375,6 @@ void MapWriterPrivate::writeLayerAttributes(QXmlStreamWriter &w,
         w.writeAttribute(QLatin1String("opacity"), QString::number(opacity));
 }
 
-/**
- * Returns the global tile ID for the given cell. Only valid after the
- * firstGidToTileset map has been initialized.
- *
- * @param cell the cell to return the global ID for
- * @return the appropriate global tile ID, or 0 if not found
- */
-uint MapWriterPrivate::gidForCell(const Cell &cell) const
-{
-    if (cell.isEmpty())
-        return 0;
-
-    const Tileset *tileset = cell.tile->tileset();
-
-    // Find the first GID for the tileset
-    QMap<uint, const Tileset*>::const_iterator i = mFirstGidToTileset.begin();
-    QMap<uint, const Tileset*>::const_iterator i_end = mFirstGidToTileset.end();
-    while (i != i_end && i.value() != tileset)
-        ++i;
-
-    if (i == i_end) // tileset not found
-        return 0;
-
-    uint gid = i.key() + cell.tile->id();
-    if (cell.flippedHorizontally)
-        gid |= FlippedHorizontallyFlag;
-    if (cell.flippedVertically)
-        gid |= FlippedVerticallyFlag;
-
-    return gid;
-}
-
 void MapWriterPrivate::writeObjectGroup(QXmlStreamWriter &w,
                                         const ObjectGroup *objectGroup)
 {
@@ -442,7 +405,7 @@ void MapWriterPrivate::writeObject(QXmlStreamWriter &w,
         w.writeAttribute(QLatin1String("type"), type);
 
     if (mapObject->tile()) {
-        const uint gid = gidForCell(Cell(mapObject->tile()));
+        const uint gid = mGidMapper.cellToGid(Cell(mapObject->tile()));
         w.writeAttribute(QLatin1String("gid"), QString::number(gid));
     }
 

commit 50e834a84085ff64ddaf37d6c08810bc44492ed9

Fixed reading of maps without tilesets but with a tile layer
This triggered an error even when the tile layer was completely empty.
The error was introduced in 67515de5abea3972cc85c54738e31b6d71afc03b.

Reviewed-by: Ben Longbons <b.r.longbons@gmail.com>

src/libtiled/mapreader.cpp
@@ -86,15 +86,14 @@ private:
     void decodeCSVLayerData(TileLayer *tileLayer, const QString &text);
 
     /**
-     * Returns the cell for the given global tile ID. When an error occurs,
-     * \a ok is set to false and an error is raised.
+     * Returns the cell for the given global tile ID. Errors are raised with
+     * the QXmlStreamReader.
      *
      * @param gid the global tile ID
-     * @param ok  returns whether the conversion went ok
      * @return the cell data associated with the given global tile ID, or an
      *         empty cell if not found
      */
-    Cell cellForGid(uint gid, bool &ok);
+    Cell cellForGid(uint gid);
 
     ObjectGroup *readObjectGroup();
     MapObject *readObject();
@@ -416,12 +415,7 @@ void MapReaderPrivate::readLayerData(TileLayer *tileLayer)
 
                 const QXmlStreamAttributes atts = xml.attributes();
                 uint gid = atts.value(QLatin1String("gid")).toString().toUInt();
-                bool ok;
-                Cell cell = cellForGid(gid, ok);
-                if (ok)
-                    tileLayer->setCell(x, y, cell);
-                else
-                    xml.raiseError(tr("Invalid tile: %1").arg(gid));
+                tileLayer->setCell(x, y, cellForGid(gid));
 
                 x++;
                 if (x >= tileLayer->width()) {
@@ -488,14 +482,7 @@ void MapReaderPrivate::decodeBinaryLayerData(TileLayer *tileLayer,
                          data[i + 2] << 16 |
                          data[i + 3] << 24;
 
-        bool ok;
-        Cell cell = cellForGid(gid, ok);
-        if (ok)
-            tileLayer->setCell(x, y, cell);
-        else {
-            xml.raiseError(tr("Invalid tile: %1").arg(gid));
-            return;
-        }
+        tileLayer->setCell(x, y, cellForGid(gid));
 
         x++;
         if (x == tileLayer->width()) {
@@ -527,26 +514,21 @@ void MapReaderPrivate::decodeCSVLayerData(TileLayer *tileLayer, const QString &t
                                .arg(x + 1).arg(y + 1).arg(tileLayer->name()));
                 return;
             }
-            bool gidOk;
-            Cell cell = cellForGid(gid, gidOk);
-            if (gidOk)
-                tileLayer->setCell(x, y, cell);
-            else {
-                xml.raiseError(tr("Invalid tile: %1").arg(gid));
-            }
+            tileLayer->setCell(x, y, cellForGid(gid));
         }
     }
 }
 
-Cell MapReaderPrivate::cellForGid(uint gid, bool &ok)
+Cell MapReaderPrivate::cellForGid(uint gid)
 {
-    Cell result;
+    bool ok;
+    const Cell result = mGidMapper.gidToCell(gid, ok);
 
-    if (mGidMapper.isEmpty()) {
-        xml.raiseError(tr("Tile used but no tilesets specified"));
-        ok = false;
-    } else {
-        result = mGidMapper.gidToCell(gid, ok);
+    if (!ok) {
+        if (mGidMapper.isEmpty())
+            xml.raiseError(tr("Tile used but no tilesets specified"));
+        else
+            xml.raiseError(tr("Invalid tile: %1").arg(gid));
     }
 
     return result;
@@ -617,13 +599,8 @@ MapObject *MapReaderPrivate::readObject()
     MapObject *object = new MapObject(name, type, xF, yF, widthF, heightF);
 
     if (gid) {
-        bool ok;
-        Cell cell = cellForGid(gid, ok);
-        if (ok) {
-            object->setTile(cell.tile);
-        } else {
-            xml.raiseError(tr("Invalid tile: %1").arg(gid));
-        }
+        const Cell cell = cellForGid(gid);
+        object->setTile(cell.tile);
     }
 
     while (xml.readNextStartElement()) {