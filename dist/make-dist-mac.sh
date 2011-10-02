34aa01ee60d3ac83cc0a1651e55c8e57a781d862
Jeff Bland <jksb@member.fsf.org>
2011-10-03 07:04:41 +0800

    Cleanup in make-dist-mac.sh
    - Remove CONFIG += x86 comment due to Qt mac frameworks not having 32 bit code
    - Autodetect plugins using ls
    - Better output

--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This script generates a mac release from an already compiled Tiled.app in
 # the bin folder. You should compile the release before running this:
-# qmake -r -spec macx-g++ CONFIG+=release CONFIG+=x86 CONFIG+=x86_64
-# make
+# tiled$ qmake -r -spec macx-g++ CONFIG+=release CONFIG+=x86_64
+# tiled$ make
+# tiled$ ./dist/make-dist-mac.sh version
 
 # Get the version
 if [ "$#" -eq "0" ]; then
@@ -16,19 +17,21 @@ baseDir=`dirname $01`/..
 binDir="$baseDir/bin"
 
 # Create a temporary staging directory
+echo Creating staging directory
 tempDir=`mktemp -d /tmp/${name}.XXXXXX` || exit 1
-echo $tempDir
+echo
 
 # Copy things to temp directory
+echo Copying files
 cp "$baseDir/AUTHORS" "$tempDir/"
 cp "$baseDir/COPYING" "$tempDir/"
 cp "$baseDir/NEWS" "$tempDir/"
 cp "$baseDir/README.md" "$tempDir/"
 cp -R "$baseDir/examples" "$tempDir/"
 cp -R "$binDir/Tiled.app" "$tempDir/"
-
-# Create symlink to application directory
-ln -s /Applications "$tempDir/Applications"
+ln -s /Applications "$tempDir/Applications" #Symlink to Applciations for easy install
+cp "$baseDir/src/tiled/images/tmx-icon-mac.icns" "$tempDir/Tiled.app/Contents/Resources/"
+echo
 
 # Get various in-bundle directories
 pluginsDir="$tempDir/Tiled.app/Contents/PlugIns"
@@ -36,23 +39,30 @@ macOSDir="$tempDir/Tiled.app/Contents/MacOS"
 frameworksDir="$tempDir/Tiled.app/Contents/Frameworks"
 
 # Use macdeployqt to copy Qt frameworks to the app
+echo Running macdeployqt
 macdeployqt "$tempDir/Tiled.app"
-qtCoreDir="$frameworksDir/QtCore.framework"
-qtGuiDir="$frameworksDir/QtGui.framework"
+echo
 
-# Modify plugins to use Qt frameworks contained within the app bundle (perhaps theres some way to get macdeployqt to do this?)
-install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$frameworksDir/libtiled.dylib"
-install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$pluginsDir/libtmw.dylib"
-install_name_tool -change "QtCore.framework/Versions/4/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$pluginsDir/libtengine.dylib"
-install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$frameworksDir/libtiled.dylib"
-install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$pluginsDir/libtmw.dylib"
-install_name_tool -change "QtGui.framework/Versions/4/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$pluginsDir/libtengine.dylib"
-
-# Copy tmx icon to the application
-cp "$baseDir/src/tiled/images/tmx-icon-mac.icns" "$tempDir/Tiled.app/Contents/Resources/"
+# Modify plugins to use Qt frameworks contained within the app bundle (is there some way to get macdeployqt to do this?)
+echo Fixing plugins
+for plugin in $(echo $(ls $frameworksDir/*.dylib) $(ls $pluginsDir/*.dylib) )
+do
+  pluginname=`basename $plugin`
+  echo "  Fixing $pluginname"
+  install_name_tool -change "$QTDIR"lib/QtCore.framework/Versions/4/QtCore "@executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore" "$plugin"
+  install_name_tool -change "$QTDIR"lib/QtGui.framework/Versions/4/QtGui "@executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui" "$plugin"
+done
+echo
 
 # Create dmg from the temp directory
+echo Creating dmg file
 hdiutil create "$baseDir/$name.dmg" -srcfolder "$tempDir" -volname "Tiled $1"
+echo
 
 # Delete the temp directory
+echo Removing staging directory $tempDir 
 rm -rf "$tempDir"
+echo
+
+# Exit
+echo Done
