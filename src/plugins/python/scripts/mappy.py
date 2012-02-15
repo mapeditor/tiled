7fe6d5db17f96d3819471b2bc0b83059a480db2c
Samuli Tuomola <samuli.tuomola@gmail.com>
2012-02-15 11:36:20 +0200

    Some refactoring

--- src/plugins/python/scripts/mappy.py
+++ src/plugins/python/scripts/mappy.py
@@ -43,8 +43,7 @@ def readtilegfx(img, hd, dat, cmap):
     for y in range(hd.blockheight):
       for x in range(hd.blockwidth):
         if hd.blockdepth == 8:
-          c = struct.unpack('B', dat[n])[0] & 255
-          c *= 3
+          c = struct.unpack('B', dat[n])[0] * 3
           r,g,b = struct.unpack('3B', cmap[c:c+3])
         elif hd.blockdepth == 24:
           c = n*3
@@ -53,12 +52,11 @@ def readtilegfx(img, hd, dat, cmap):
         #img[tx+x,ty+y] = (r,g,b)
         img.point(tx+x, ty+y, bytearray([r,g,b,0xff]))
         n+=1
-  #return [seq[i:i+w] for i in range(0, len(seq), w)]
 
 def readlayer(hd, dat):
   #print len(dat),'vs',hd.mapheight*hd.mapwidth*2
   for i in range(0,hd.mapheight*hd.mapwidth*2,2):
-    d = struct.unpack('<H', dat[i:i+2])[0]  #LE?
+    d = struct.unpack('<H', dat[i:i+2])[0]
     #d<0 is anim?
     if hd.type == 0: d /= hd.blockstrsize
     yield d
@@ -80,13 +78,15 @@ def poplayer(layer, fg, blocks, tileset, hd, l):
             layer.setCell(x, y, Tiled.Cell(ti))
       i += 1
 
-class blk():
-  def __init__(self, mod, i):
-    self.mod = mod
-    self.idx = list(i)
-    print self.idx,
-    self.idx = [n/mod for n in self.idx]
-    print 'after',self.idx
+def readblockdata(chunk, hd):
+  mod = hd.blockwidth * hd.blockheight * ((hd.blockdepth+1)/8)
+  for pos in range(0, hd.numblockstr*hd.blockstrsize, hd.blockstrsize):
+    b = BLKSTR()
+    b.unpack(chunk[pos:pos+hd.blockstrsize])
+    if hd.type == 0:
+      for i in range(4):
+        b.olay[i] /= mod
+    yield b
 
 def readmap(f):
   try:
@@ -94,56 +94,35 @@ def readmap(f):
     chunks = readchunks(f)
     hd = chunks['MPHD']
     m = Tiled.Map(Tiled.Map.Orthogonal, hd.mapwidth, hd.mapheight, hd.blockwidth, hd.blockheight)
+    if hd.type == 2:
+      print 'Isometric maps not supported at the moment'
+      return m
 
     if not os.path.exists(f+'.png'):
-      print 'Creating',f+'.png'
-      #PIL
-      #sheetimg = Image.new("RGB", (hd.blockwidth*10, hd.blockheight*hd.numblockgfx/10))
-      #readtilegfx(sheetimg.load(), hd, chunks['BGFX'], chunks['CMAP'])
-      #sheetimg.save(f+'.gif', 'gif')
+      print 'Creating',f+'.png','slowwwly (luckily this only happens once per map)'
       with open(f+'.png', 'wb') as fh:
-        #pngpy
-        #w = png.Writer(hd.blockwidth*10, hd.blockheight*hd.numblockgfx/10)
-        #px = readtilegfx(None, hd, chunks['BGFX'], chunks['CMAP'])
-        #img = png.from_array(px, 'RGB')
-        #w.write(fh, px)
-        #pngc, third time's the charm
         cn = PNGCanvas(hd.blockwidth*10, hd.blockheight*hd.numblockgfx/10)
         readtilegfx(cn, hd, chunks['BGFX'], chunks['CMAP'])
         fh.write(cn.dump())
 
-    t = Tiled.Tileset('Tiles', hd.blockwidth, hd.blockheight, 0, 0)
-    loadFromImage(t, f+'.png')
+    tset = Tiled.Tileset('Tiles', hd.blockwidth, hd.blockheight, 0, 0)
+    loadFromImage(tset, f+'.png')
     maps.append(m)  # w/o live ref crashes in mapscene:createLayerItem setVisible
 
-    pos = 0
-    blks = []
-    mod = hd.blockwidth * hd.blockheight * ((hd.blockdepth+1)/8)
-
-    for i in range(hd.numblockstr):
-      b = BLKSTR()
-      b.unpack(chunks['BKDT'][pos:pos+hd.blockstrsize])
-      if hd.type == 0:
-        for i in range(4):
-          b.olay[i] /= mod
-      #idx = struct.unpack('>4I', chunks['BKDT'][pos:pos+16])
-      #b = blk(mod, idx, struct.unpack('>4I', chunks['BKDT'][pos:pos+16]))
-      blks.append(b)
-      pos += hd.blockstrsize
+    blks = list(readblockdata(chunks['BKDT'], hd))
 
     for c in ['LYR'+str(i) for i in range(7,0,-1)]+['BODY']:
       if not chunks.has_key(c): continue
       print 'populating',c
-      l = Tiled.TileLayer(c,0,0,hd.mapwidth, hd.mapheight)
+      lay = Tiled.TileLayer(c,0,0,hd.mapwidth, hd.mapheight)
       lvl = list(readlayer(hd, chunks[c]))
       for o in range(4):
-        poplayer(l, o, blks, t, hd, lvl)
-      l.setMap(m)  # w/o this crashes in mapscene:createLayerItem dynamic_cast
-      m.addLayer(l)
-    m.addTileset(t)
+        poplayer(lay, o, blks, tset, hd, lvl)
+      lay.setMap(m)  # w/o this crashes in mapscene:createLayerItem dynamic_cast
+      m.addLayer(lay)
+    m.addTileset(tset)
 
     return m
-
   except Exception as e:
     exc_type, exc_obj, exc_tb = sys.exc_info()
     print 'error @%i:' % exc_tb.tb_lineno, e
