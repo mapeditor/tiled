mapeditor/tiled

commit 1afabff912bd959e866f084e9c65d89a9cbcf8db

Fixed crash when opening a map without tilesets
In particular this crash would always happen when creating a new map.
The crash was caused by TerrainBrush::mapDocumentChanged, which did not
handle the case where the new map did not have any tilesets correctly.

src/tiled/terrainbrush.cpp
@@ -195,24 +195,27 @@ void TerrainBrush::languageChanged()
     setShortcut(QKeySequence(tr("T")));
 }
 
+static Terrain *firstTerrain(MapDocument *mapDocument)
+{
+    if (!mapDocument)
+        return 0;
+
+    foreach (Tileset *tileset, mapDocument->map()->tilesets())
+        if (tileset->terrainCount() > 0)
+            return tileset->terrain(0);
+
+    return 0;
+}
+
 void TerrainBrush::mapDocumentChanged(MapDocument *oldDocument,
-                                    MapDocument *newDocument)
+                                      MapDocument *newDocument)
 {
     AbstractTileTool::mapDocumentChanged(oldDocument, newDocument);
 
     // Reset the brush, since it probably became invalid
     brushItem()->setTileRegion(QRegion());
 
-    // if the new document has any terrains defined, choose the first rather than the 'none' terrain
-    if (newDocument) {
-        Tileset *tileset = newDocument->map()->tilesets().at(0);
-        if (tileset && tileset->terrainCount() > 0)
-            setTerrain(tileset->terrain(0));
-        else
-            setTerrain(NULL);
-    } else {
-        mTerrain = NULL;
-    }
+    setTerrain(firstTerrain(newDocument));
 }
 
 void TerrainBrush::setTerrain(const Terrain *terrain)

commit 1660e7c91b9932301eea25dad23152699a49deea

Fixed crash on closing a map
This is a crash introduced with
1afabff912bd959e866f084e9c65d89a9cbcf8db. We can't always call
TerrainBrush::updateBrush since it expects the current layer to be a
tile layer, which is not the case when the map is being closed (and the
brush set back to a null map).

Reviewed-by: Manu Evans

src/tiled/terrainbrush.cpp
@@ -213,9 +213,10 @@ void TerrainBrush::mapDocumentChanged(MapDocument *oldDocument,
     AbstractTileTool::mapDocumentChanged(oldDocument, newDocument);
 
     // Reset the brush, since it probably became invalid
-    brushItem()->setTileRegion(QRegion());
+    brushItem()->setTileLayer(0);
 
-    setTerrain(firstTerrain(newDocument));
+    // Don't use setTerrain since we do not want to update the brush right now
+    mTerrain = firstTerrain(newDocument);
 }
 
 void TerrainBrush::setTerrain(const Terrain *terrain)