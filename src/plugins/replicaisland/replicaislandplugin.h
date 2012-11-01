023d43c9e963f0f2b2e7bf4917268c776e5e6114
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2012-11-01 22:16:29 +0100

    Avoid cppcheck warnings in Replica Island plugin
    "(warning) Using char type as array index"

--- src/plugins/replicaisland/replicaislandplugin.h
+++ src/plugins/replicaisland/replicaislandplugin.h
@@ -78,10 +78,11 @@ private:
                                    QList<Tiled::Tileset *> &typeTilesets,
                                    QList<Tiled::Tileset *> &tileIndexTilesets);
     Tiled::Tileset *loadTilesetFromResource(const QString &name);
-    void addTilesetsToMap(Tiled::Map *map, QList<Tiled::Tileset *> tilesets);
-    Tiled::Tileset *tilesetForLayer(char type, char tileIndex,
-                                    QList<Tiled::Tileset *> typeTilesets,
-                                    QList<Tiled::Tileset *> tileIndexTilesets);
+    void addTilesetsToMap(Tiled::Map *map,
+                          const QList<Tiled::Tileset *> &tilesets);
+    Tiled::Tileset *tilesetForLayer(int type, int tileIndex,
+                                    const QList<Tiled::Tileset *> &typeTilesets,
+                                    const QList<Tiled::Tileset *> &tileIndexTilesets);
     QString layerTypeToName(char type);
 
     // MapWriterInterface support.
