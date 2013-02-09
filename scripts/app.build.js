a5a1cc80b6fca15055e42373a2fa4d298341527f
William Kevin Manire <williamkmanire@gmail.com>
2013-02-09 01:24:15 -0800

    Reorganized files, added requireJS+jQuery1.9.1, added build file for minification with Rhino and uglify

---
+++ scripts/app.build.js
@@ -0,0 +1,20 @@
+({
+    appDir: "../",
+    baseUrl: "scripts/",
+    dir: "../../webapp-build",
+    //Comment out the optimize line if you want
+    //the code minified by UglifyJS
+    optimize: "none",
+
+    paths: {
+        "jquery": "empty:"
+    },
+
+    modules: [
+        //Optimize the application files. jQuery is not 
+        //included since it is already in require-jquery.js
+        {
+            name: "main"
+        }
+    ]
+})
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+require(["jquery"], function($) {
+  $(function() {
+    console.log("jQuery loaded via AMD");
+  });
+});
