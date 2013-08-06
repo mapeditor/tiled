mapeditor/tiled

commit f40431939986dcf71c091c4742c7cdab0342da67

Optimized hardware-accelerated tile rendering
By using QPainter::PixmapFragment the amount of API calls to draw the
same tile over and over again is much reduced. This leads to a small
improvement in rendering speed, which is mainly noticable on maps where
large areas are filled with the same tile.

The minimum Qt version was raised to Qt 4.7 since earlier versions do
not have QPainter::PixmapFragment.

src/libtiled/maprenderer.cpp
@@ -82,56 +82,68 @@ QPolygonF MapRenderer::lineToPolygon(const QPointF &start, const QPointF &end)
     return polygon;
 }
 
-/**
- * Draws a \a cell with the given \a origin at \a pos, taking into account the
- * flipping and tile offset.
- *
- * It leaves the painter transform set to the transform used to draw the cell.
- */
-void MapRenderer::drawCell(QPainter *painter,
-                           const Cell &cell,
-                           const QPointF &pos,
-                           Origin origin,
-                           const QTransform &baseTransform)
-{
-    const QPixmap &img = cell.tile->image();
-    const QPoint offset = cell.tile->tileset()->tileOffset();
-    const QSize imgSize = img.size();
 
-    qreal m11 = 1;      // Horizontal scaling factor
-    qreal m12 = 0;      // Vertical shearing factor
-    qreal m21 = 0;      // Horizontal shearing factor
-    qreal m22 = 1;      // Vertical scaling factor
-    qreal dx = offset.x() + pos.x();
-    qreal dy = offset.y() + pos.y() - imgSize.height();
+/**
+ * Renders a \a cell with the given \a origin at \a pos, taking into account
+ * the flipping and tile offset.
+ *
+ * For performance reasons, the actual drawing is delayed until a different
+ * kind of tile has to be drawn. For this reason it is necessary to call
+ * flush when finished doing drawCell calls. This function is also called by
+ * the destructor so usually an explicit call it not needed.
+ */
+void CellRenderer::render(const Cell &cell, const QPointF &pos, Origin origin)
+{
+    if (mTile != cell.tile) {
+        flush();
+        mTile = cell.tile;
+    }
+
+    const QSizeF size = cell.tile->size();
+    const QPoint offset = cell.tile->tileset()->tileOffset();
+    const QPointF sizeHalf = QPointF(size.width() / 2, size.height() / 2);
+
+    QPainter::PixmapFragment fragment;
+    fragment.x = pos.x() + offset.x() + sizeHalf.x();
+    fragment.y = pos.y() + offset.y() + sizeHalf.y() - size.height();
+    fragment.sourceLeft = 0;
+    fragment.sourceTop = 0;
+    fragment.width = size.width();
+    fragment.height = size.height();
+    fragment.scaleX = cell.flippedHorizontally ? -1 : 1;
+    fragment.scaleY = cell.flippedVertically ? -1 : 1;
+    fragment.rotation = 0;
+    fragment.opacity = 1;
 
     if (origin == BottomCenter)
-        dx += -imgSize.width() / 2;
+        fragment.x -= sizeHalf.x();
 
     if (cell.flippedAntiDiagonally) {
-        // Use shearing to swap the X/Y axis
-        m11 = 0;
-        m12 = 1;
-        m21 = 1;
-        m22 = 0;
+        fragment.rotation = 90;
+        fragment.scaleX *= -1;
 
         // Compensate for the swap of image dimensions
-        dy += imgSize.height() - imgSize.width();
-        if (origin == BottomCenter)
-            dx += (imgSize.width() - imgSize.height()) / 2;
-    }
-    if (cell.flippedHorizontally) {
-        m11 = -m11;
-        m21 = -m21;
-        dx += cell.flippedAntiDiagonally ? imgSize.height() : imgSize.width();
-    }
-    if (cell.flippedVertically) {
-        m12 = -m12;
-        m22 = -m22;
-        dy += cell.flippedAntiDiagonally ? imgSize.width() : imgSize.height();
+        const qreal halfDiff = sizeHalf.y() - sizeHalf.x();
+        fragment.y += halfDiff;
+        if (origin != BottomCenter)
+            fragment.x += halfDiff;
     }
 
-    const QTransform transform(m11, m12, m21, m22, dx, dy);
-    painter->setTransform(transform * baseTransform);
-    painter->drawPixmap(QPointF(), img);
+    mFragments.append(fragment);
+}
+
+/**
+ * Renders any remaining cells.
+ */
+void CellRenderer::flush()
+{
+    if (!mTile)
+        return;
+
+    mPainter->drawPixmapFragments(mFragments.constData(),
+                                  mFragments.size(),
+                                  mTile->image());
+
+    mTile = 0;
+    mFragments.resize(0);
 }

src/libtiled/isometricrenderer.cpp
@@ -213,7 +213,7 @@ void IsometricRenderer::drawTileLayer(QPainter *painter,
     // Determine whether the current row is shifted half a tile to the right
     bool shifted = inUpperHalf ^ inLeftHalf;
 
-    QTransform baseTransform = painter->transform();
+    CellRenderer renderer(painter);
 
     for (int y = startPos.y(); y - tileHeight < rect.bottom();
          y += tileHeight / 2)
@@ -224,10 +224,8 @@ void IsometricRenderer::drawTileLayer(QPainter *painter,
             if (layer->contains(columnItr)) {
                 const Cell &cell = layer->cellAt(columnItr);
                 if (!cell.isEmpty()) {
-                    drawCell(painter, cell,
-                             QPointF(x, y),
-                             BottomLeft,
-                             baseTransform);
+                    renderer.render(cell, QPointF(x, y),
+                                    CellRenderer::BottomLeft);
                 }
             }
 
@@ -247,8 +245,6 @@ void IsometricRenderer::drawTileLayer(QPainter *painter,
             shifted = false;
         }
     }
-
-    painter->setTransform(baseTransform);
 }
 
 void IsometricRenderer::drawTileSelection(QPainter *painter,
@@ -292,10 +288,8 @@ void IsometricRenderer::drawMapObject(QPainter *painter,
             painter->drawText(textPos, name);
         }
 
-        drawCell(painter, object->cell(),
-                 pos,
-                 BottomCenter,
-                 painter->transform());
+        CellRenderer(painter).render(object->cell(), pos,
+                                     CellRenderer::BottomCenter);
 
         if (testFlag(ShowTileObjectOutlines)) {
             pen.setStyle(Qt::SolidLine);

src/libtiled/maprenderer.h
@@ -39,6 +39,7 @@ class Cell;
 class Layer;
 class Map;
 class MapObject;
+class Tile;
 class TileLayer;
 class ImageLayer;
 
@@ -166,16 +167,6 @@ public:
 
     static QPolygonF lineToPolygon(const QPointF &start, const QPointF &end);
 
-    enum Origin {
-        BottomLeft,
-        BottomCenter
-    };
-    static void drawCell(QPainter *painter,
-                         const Cell &cell,
-                         const QPointF &pos,
-                         Origin origin,
-                         const QTransform &baseTransform);
-
 protected:
     /**
      * Returns the map this renderer is associated with.
@@ -186,6 +177,33 @@ private:
     const Map *mMap;
 
     RenderFlags mFlags;
+};
+
+/**
+ * A utility class for rendering cells.
+ */
+class CellRenderer
+{
+public:
+    enum Origin {
+        BottomLeft,
+        BottomCenter
+    };
+
+    explicit CellRenderer(QPainter *painter)
+        : mPainter(painter)
+        , mTile(0)
+    {}
+
+    ~CellRenderer() { flush(); }
+
+    void render(const Cell &cell, const QPointF &pos, Origin origin);
+    void flush();
+
+private:
+    QPainter *mPainter;
+    Tile *mTile;
+    QVector<QPainter::PixmapFragment> mFragments;
 };
 
 } // namespace Tiled

src/libtiled/orthogonalrenderer.cpp
@@ -193,7 +193,7 @@ void OrthogonalRenderer::drawTileLayer(QPainter *painter,
                                        const TileLayer *layer,
                                        const QRectF &exposed) const
 {
-    QTransform savedTransform = painter->transform();
+    const QTransform savedTransform = painter->transform();
 
     const int tileWidth = map()->tileWidth();
     const int tileHeight = map()->tileHeight();
@@ -225,7 +225,7 @@ void OrthogonalRenderer::drawTileLayer(QPainter *painter,
         endY = qMin((int) std::ceil(rect.bottom()) / tileHeight + 1, endY);
     }
 
-    QTransform baseTransform = painter->transform();
+    CellRenderer renderer(painter);
 
     for (int y = startY; y < endY; ++y) {
         for (int x = startX; x < endX; ++x) {
@@ -233,12 +233,13 @@ void OrthogonalRenderer::drawTileLayer(QPainter *painter,
             if (cell.isEmpty())
                 continue;
 
-            drawCell(painter, cell,
-                     QPointF(x * tileWidth, (y + 1) * tileHeight),
-                     BottomLeft,
-                     baseTransform);
+            renderer.render(cell,
+                            QPointF(x * tileWidth, (y + 1) * tileHeight),
+                            CellRenderer::BottomLeft);
         }
     }
+
+    renderer.flush();
 
     painter->setTransform(savedTransform);
 }
@@ -271,10 +272,8 @@ void OrthogonalRenderer::drawMapObject(QPainter *painter,
     if (!object->cell().isEmpty()) {
         const Cell &cell = object->cell();
 
-        drawCell(painter, cell,
-                 QPointF(),
-                 BottomLeft,
-                 painter->transform());
+        CellRenderer(painter).render(cell, QPointF(),
+                                     CellRenderer::BottomLeft);
 
         if (testFlag(ShowTileObjectOutlines)) {
             const QRect rect = cell.tile->image().rect();

tiled.pro
@@ -2,9 +2,9 @@
 isEmpty(QT_VERSION) {
     error("QT_VERSION not defined. Tiled does not work with Qt 3.")
 }
-contains(QT_VERSION, ^4\\.[0-5]\\..*) {
+contains(QT_VERSION, ^4\\.[0-6]\\..*) {
     message("Cannot build Tiled with Qt version $$QT_VERSION")
-    error("Use at least Qt 4.6")
+    error("Use at least Qt 4.7")
 }
 
 TEMPLATE  = subdirs

commit b04aa42cd2954c3e300048ab08186619f435e5b3

Fixed tile flipping order for rotated tiles
Since the scaling happens after the rotation, the scaleX and scaleY
factors need to be swapped when flipping anti-diagonally.

This fixes a regression that appeared in commit f40431939986dcf71c.

Thanks to @mauvecow for finding this issue, and the initial patch.

src/libtiled/maprenderer.cpp
@@ -134,6 +134,7 @@ void CellRenderer::render(const Cell &cell, const QPointF &pos, Origin origin)
     if (cell.flippedAntiDiagonally) {
         fragment.rotation = 90;
         fragment.scaleX *= -1;
+        std::swap(fragment.scaleX, fragment.scaleY);
 
         // Compensate for the swap of image dimensions
         const qreal halfDiff = sizeHalf.y() - sizeHalf.x();