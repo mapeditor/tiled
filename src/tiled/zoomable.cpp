mapeditor/tiled

commit c6bc0a1e4061cf788342b120e8abcad43064c929

Fixed unused variable warning when compiling in Release mode

src/tiled/zoomable.cpp
@@ -213,8 +213,7 @@ void Zoomable::comboActivated(int index)
 
 void Zoomable::comboEdited()
 {
-    int pos = mComboRegExp.indexIn(mComboBox->currentText());
-    Q_ASSERT(pos != -1);
+    Q_ASSERT(mComboRegExp.indexIn(mComboBox->currentText()) != -1);
 
     qreal scale = qBound(mZoomFactors.first(),
                          qreal(mComboRegExp.cap(1).toDouble() / 100.f),

commit 3e5690d7a8558eb121bd002dbf86836b88d24dcc

Fixed issue with zoom combo box jumping to 1%
Was broken by c6bc0a1e4061cf788342b120e8abcad43064c929. Thanks to
@obeea for finding this.

Fixes #665

src/tiled/zoomable.cpp
@@ -213,7 +213,9 @@ void Zoomable::comboActivated(int index)
 
 void Zoomable::comboEdited()
 {
-    Q_ASSERT(mComboRegExp.indexIn(mComboBox->currentText()) != -1);
+    int pos = mComboRegExp.indexIn(mComboBox->currentText());
+    Q_ASSERT(pos != -1);
+    Q_UNUSED(pos)
 
     qreal scale = qBound(mZoomFactors.first(),
                          qreal(mComboRegExp.cap(1).toDouble() / 100.f),