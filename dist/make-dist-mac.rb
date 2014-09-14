432c8e4eb71bf9f09330f4238099ade8e6e0766e
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-09-14 13:48:47 +0200

    Some fixes to OS X deployment script
    * Copy the tmxrasterizer executable rather than moving it,
      which makes it possible to run the deployment script
      several times without needing to redo the make step.
    
    * Run the right version of macdeployqt. For me it was
      running the Qt 4 version which somehow resulted in a
      lot more shipped plugins and libraries than necessary.

--- dist/make-dist-mac.rb
+++ dist/make-dist-mac.rb
@@ -33,13 +33,13 @@ Dir.mktmpdir do |tempDir|
     end
     FileUtils.cp_r File.join(baseDir, 'examples'), tempDir
     FileUtils.cp_r binAppDir, tempDir
-    FileUtils.mv   File.join(binDir,'tmxrasterizer'), File.join(tempDir, 'Tiled.app/Contents/MacOS')
+    FileUtils.cp   File.join(binDir,'tmxrasterizer'), File.join(tempDir, 'Tiled.app/Contents/MacOS')
     FileUtils.ln_s '/Applications', File.join(tempDir, 'Applications') #Symlink to Applications for easy install
     FileUtils.cp File.join(baseDir, 'src/tiled/images/tmx-icon-mac.icns'), File.join(tempDir, 'Tiled.app/Contents/Resources')
 
     # Use macdeployqt to copy Qt frameworks to the app
     puts "Running macdeployqt"
-    `macdeployqt "#{tempDir}/Tiled.app"`
+    `#{ENV['HOME']}/Qt/5.3/clang_64/bin/macdeployqt "#{tempDir}/Tiled.app"`
     raise "macdeployqt error #{$?}" unless $? == 0
 
     # Modify plugins to use Qt frameworks contained within the app bundle (is there some way to get macdeployqt to do this?)
