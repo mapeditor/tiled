6525aa65279bdbf34136d0b4e752919a86eb709f
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-09-28 12:08:12 -0700

    Fixed path to QML files on Windows

--- src/tiledquick/main.cpp
+++ src/tiledquick/main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+#ifdef Q_OS_WIN
+    engine.addImportPath(QApplication::applicationDirPath() + "/qml");
+#else
     engine.addImportPath(QApplication::applicationDirPath() + "/../qml");
+#endif
 
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
 
