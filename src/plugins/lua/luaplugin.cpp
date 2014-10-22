0f2ba2437953c6cd3bbc85568f3b4de2654b6b60
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-10-22 23:53:06 +0200

    Fixed compile
    It's definitely getting too late...

--- src/plugins/lua/luaplugin.cpp
+++ src/plugins/lua/luaplugin.cpp
@@ -335,8 +335,8 @@ void LuaPlugin::writeImageLayer(LuaTableWriter &writer,
 
     writer.writeKeyAndValue("type", "imagelayer");
     writer.writeKeyAndValue("name", imageLayer->name());
-    writer.writeKeyAndValue("x", x);
-    writer.writeKeyAndValue("y", y);
+    writer.writeKeyAndValue("x", imageLayer->x());
+    writer.writeKeyAndValue("y", imageLayer->y());
     writer.writeKeyAndValue("visible", imageLayer->isVisible());
     writer.writeKeyAndValue("opacity", imageLayer->opacity());
 
