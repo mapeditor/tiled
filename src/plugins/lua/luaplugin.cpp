4b723791ebc1d5f90086adacb864006b345d2480
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2014-10-22 23:41:55 +0200

    Lua plugin: Write out Image layer position
    This is the position of the image in pixels, and it was missing from the
    Lua export format.
    
    Closes #761

--- src/plugins/lua/luaplugin.cpp
+++ src/plugins/lua/luaplugin.cpp
@@ -335,6 +335,8 @@ void LuaPlugin::writeImageLayer(LuaTableWriter &writer,
 
     writer.writeKeyAndValue("type", "imagelayer");
     writer.writeKeyAndValue("name", imageLayer->name());
+    writer.writeKeyAndValue("x", x);
+    writer.writeKeyAndValue("y", y);
     writer.writeKeyAndValue("visible", imageLayer->isVisible());
     writer.writeKeyAndValue("opacity", imageLayer->opacity());
 
