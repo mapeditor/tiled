97c3c20b8e1324dc9e2adf34c8f917bfe039543f
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2015-01-18 13:21:25 +0100

    Added a separate main QML file for Tiled Quick Android
    * Has no menu bar, instead just an "Open..." button
    * Features pinch zooming and panning
    * Remembers last used folder

--- src/tiledquick/main-android.cpp
+++ src/tiledquick/main-android.cpp
@@ -7,11 +7,14 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    QCoreApplication::setOrganizationDomain("mapeditor.org");
+    QCoreApplication::setApplicationName("TiledQuick");
+
     TiledQuick::TiledQuickPlugin plugin;
     plugin.registerTypes("org.mapeditor.Tiled");
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:///main-android.qml")));
 
     return app.exec();
 }
