d4a00dce7da6fde91c8a1f5dccd0fbd231945b72
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2015-09-21 12:01:21 +0200

    Use QSignalBlocker to block unwanted signal
    When the fill tool draws its preview, it should not cause the
    MapDocument to emit the regionChanged signal.

--- src/tiled/bucketfilltool.cpp
+++ src/tiled/bucketfilltool.cpp
@@ -99,9 +99,6 @@ void BucketFillTool::tilePositionChanged(const QPoint &tilePos)
                                                               tilePos.y()))
         return;
 
-    // This clears the connections so we don't get callbacks
-    clearConnections(mapDocument());
-
     // Optimization: we don't need to recalculate the fill area
     // if the new mouse position is still over the filled region
     // and the shift modifier hasn't changed.
@@ -157,6 +154,9 @@ void BucketFillTool::tilePositionChanged(const QPoint &tilePos)
             mMissingTilesets.clear();
             mapDocument()->unifyTilesets(variation, mMissingTilesets);
 
+            // Suppress emission of regionChanged signal
+            const QSignalBlocker blocker(mapDocument());
+
             TilePainter tilePainter(mapDocument(), mFillOverlay.data());
             tilePainter.drawStamp(stampLayer, mFillRegion);
         }
@@ -169,8 +169,6 @@ void BucketFillTool::tilePositionChanged(const QPoint &tilePos)
         // Update the brush item to draw the overlay
         brushItem()->setTileLayer(mFillOverlay);
     }
-    // Create connections to know when the overlay should be cleared
-    makeConnections();
 }
 
 void BucketFillTool::mousePressed(QGraphicsSceneMouseEvent *event)
