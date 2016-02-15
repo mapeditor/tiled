701ddeea681f14e5af9e06713ac0f2fbe2121225
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2016-02-15 20:55:46 +0100

    Updated NEWS and bumped version to 0.15.2

--- src/tiled/main.cpp
+++ src/tiled/main.cpp
@@ -166,7 +166,7 @@ int main(int argc, char *argv[])
 #ifdef BUILD_INFO_VERSION
     a.setApplicationVersion(QLatin1String(AS_STRING(BUILD_INFO_VERSION)));
 #else
-    a.setApplicationVersion(QLatin1String("0.15.1"));
+    a.setApplicationVersion(QLatin1String("0.15.2"));
 #endif
 
 #ifdef Q_OS_MAC
