125089fc693df93ba8dee4823b276d0a5a47ef77
Will Luongo <me@willluongo.com>
2016-03-05 11:48:41 -0500

    Fixed compile when QT_NO_CAST_FROM_ASCII is defined

--- src/qtsingleapplication/src/qtlocalpeer.cpp
+++ src/qtsingleapplication/src/qtlocalpeer.cpp
@@ -78,7 +78,7 @@ QtLocalPeer::QtLocalPeer(QObject* parent, const QString &appId)
 #endif
         prefix = id.section(QLatin1Char('/'), -1);
     }
-    prefix.remove(QRegExp("[^a-zA-Z]"));
+    prefix.remove(QRegExp(QLatin1String("[^a-zA-Z]")));
     prefix.truncate(6);
 
     QByteArray idc = id.toUtf8();
