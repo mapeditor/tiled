54f89865e6ab1a8522646ccc80d8cdc2faabd879
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2016-08-26 11:08:23 +0200

    QtPropertyBrowser: Fixed alpha in color picker dialog
    There is a bug in QColorDialog::getRgba that makes it ignore the alpha
    value of the initial color (contrary to its documentation). That method
    is actually obsolete since Qt 4.5, but it was still used by the
    QtColorEditWidget.
    
    Closes #1345

--- src/qtpropertybrowser/src/qteditorfactory.cpp
+++ src/qtpropertybrowser/src/qteditorfactory.cpp
@@ -2332,11 +2332,9 @@ void QtColorEditWidget::setValue(const QColor &c)
 
 void QtColorEditWidget::buttonClicked()
 {
-    bool ok = false;
-    QRgb oldRgba = m_color.rgba();
-    QRgb newRgba = QColorDialog::getRgba(oldRgba, &ok, this);
-    if (ok && newRgba != oldRgba) {
-        setValue(QColor::fromRgba(newRgba));
+    QColor newColor = QColorDialog::getColor(m_color, this, QString(), QColorDialog::ShowAlphaChannel);
+    if (newColor.isValid() && newColor != m_color) {
+        setValue(newColor);
         emit valueChanged(m_color);
     }
 }
