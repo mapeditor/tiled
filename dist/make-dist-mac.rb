49a75607f2ba622de83cc006cf6bec710aba9482
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2016-11-28 10:29:57 +0100

    Switch to uppercase for distributed files
    I just think it looks better.

--- dist/make-dist-mac.rb
+++ dist/make-dist-mac.rb
@@ -60,7 +60,7 @@ Dir.mktmpdir do |tempDir|
     end
 
     # Create dmg from the temp directory
-    dmgPath = File.join(baseDir, 'tiled-' + version + '.dmg')
+    dmgPath = File.join(baseDir, 'Tiled-' + version + '.dmg')
     puts "Creating dmg at #{dmgPath}"
     `hdiutil create "#{dmgPath}" -srcfolder "#{tempDir}" -volname "Tiled #{version}"`
     raise "hdiutil error #{$?}" unless $? == 0
