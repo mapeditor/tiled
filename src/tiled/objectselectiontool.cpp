60b93cb6b35c541af6f25e0a37e45ca83c4dbb1e
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2017-03-05 15:23:45 +0530

    min. file diff

--- src/tiled/objectselectiontool.cpp
+++ src/tiled/objectselectiontool.cpp
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "objectselectiontool.h"
 
 #include "changepolygon.h"
@@ -1477,6 +1478,7 @@ void ObjectSelectionTool::refreshCursor()
     default:
         break;
     }
+
     if (cursor().shape() != cursorShape)
         setCursor(cursorShape);
 }
