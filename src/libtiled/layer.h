9f10bc03935dd07cdbd2666447b51b6c375b03e1
Kai Presler-Marshall <kpresle@ncsu.edu>
2017-03-19 19:24:25 -0400

    small changes to layer

--- src/libtiled/layer.h
+++ src/libtiled/layer.h
@@ -66,6 +66,8 @@ public:
      */
     Layer(TypeFlag type, const QString &name, int x, int y);
 
+    Layer(TypeFlag type, const QString &name, int x, int y,int width, int height);
+
     /**
      * Returns the type of this layer.
      */
@@ -151,6 +153,29 @@ public:
     void setPosition(QPoint pos) { setPosition(pos.x(), pos.y()); }
     void setPosition(int x, int y) { mX = x; mY = y; }
 
+    /**
+     * Returns the width of this layer.
+     */
+    int width() const { return mWidth; }
+
+    /**
+     * Returns the height of this layer.
+     */
+    int height() const { return mHeight; }
+
+    /**
+     * Returns the size of this layer.
+     */
+    QSize size() const { return QSize(mWidth, mHeight); }
+
+    void setSize(const QSize &size);
+
+    /**
+     * Returns the bounds of this layer.
+     */
+QRect bounds() const { return QRect(mX, mY, mWidth, mHeight); }
+
+
     void setOffset(const QPointF &offset);
     QPointF offset() const;
 
@@ -222,6 +247,8 @@ protected:
     TypeFlag mLayerType;
     int mX;
     int mY;
+    int mWidth;
+    int mHeight;
     QPointF mOffset;
     float mOpacity;
     bool mVisible;
@@ -231,6 +258,14 @@ protected:
     friend class Map;
     friend class GroupLayer;
 };
+/**
+ * Sets the size of this layer.
+ */
+inline void Layer::setSize(const QSize &size)
+{
+    mWidth = size.width();
+    mHeight = size.height();
+}
 
 
 /**
