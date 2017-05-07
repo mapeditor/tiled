c10ba1feeb871a68d74fdb513a2d5ad437af7b66
Mitch Curtis <mitch.curtis@qt.io>
2017-05-07 14:48:35 +0200

    MapItem: emit mapChanged() after calling refresh()
    This allows QML code to be guaranteed that MapItem has a renderer by
    the time onMapChanged is called, which is necessary to be able to
    e.g. set a transform using e.g. pixelToScreenCoords().

--- src/tiledquickplugin/mapitem.cpp
+++ src/tiledquickplugin/mapitem.cpp
@@ -44,9 +44,8 @@ void MapItem::setMap(Tiled::Map *map)
         return;
 
     mMap = map;
-    emit mapChanged();
-
     refresh();
+    emit mapChanged();
 }
 
 void MapItem::setVisibleArea(const QRectF &visibleArea)
