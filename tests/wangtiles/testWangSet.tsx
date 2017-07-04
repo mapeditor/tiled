d1637712fa7c4442e1e7c541a17b0f03597242ac
Benjamin Trotter <bdtrotte@ucsc.edu>
2017-07-04 13:08:32 -0700

    added test files

---
+++ tests/wangtiles/testWangSet.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WangTilesetTest" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="2CornerWangSet.jpg" width="128" height="128"/>
+ <wangsets>
+  <wangset name="A Wang Set" edges="0" corners="2" tile="0">
+   <wangtile tileid="5" wangid="270536736"/>
+   <wangtile tileid="8" wangid="537923600"/>
+   <wangtile tileid="14" wangid="538976272"/>
+   <wangtile tileid="0" wangid="269488144"/>
+   <wangtile tileid="15" wangid="538976288"/>
+   <wangtile tileid="6" wangid="270540816"/>
+   <wangtile tileid="9" wangid="537923616"/>
+   <wangtile tileid="1" wangid="269488160"/>
+   <wangtile tileid="7" wangid="270540832"/>
+   <wangtile tileid="10" wangid="537927696"/>
+   <wangtile tileid="2" wangid="269492240"/>
+   <wangtile tileid="11" wangid="537927712"/>
+   <wangtile tileid="3" wangid="269492256"/>
+   <wangtile tileid="12" wangid="538972176"/>
+   <wangtile tileid="4" wangid="270536720"/>
+   <wangtile tileid="13" wangid="538972192"/>
+  </wangset>
+ </wangsets>
+</tileset>
