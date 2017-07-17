7e5b0b38b151e2b84fc77d65cc70ccc7ae17bcc5
Benjamin Trotter <bdtrotte@ucsc.edu>
2017-07-17 07:36:47 -0700

    Added updated test files

---
+++ tests/wangtiles/PathAndObjects.tsx
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="PathAndObjects" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="PathAndObjects.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="17"/>
+  <terrain name="New Terrain" tile="177"/>
+  <terrain name="New Terrain" tile="23"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0"/>
+ <tile id="1" terrain="1,1,0,0"/>
+ <tile id="2" terrain="1,1,0,1"/>
+ <tile id="6" terrain="1,1,1,2"/>
+ <tile id="7" terrain="1,1,2,2"/>
+ <tile id="8" terrain="1,1,2,1"/>
+ <tile id="16" terrain="1,0,1,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,1,0,1"/>
+ <tile id="22" terrain="1,2,1,2"/>
+ <tile id="23" terrain="2,2,2,2"/>
+ <tile id="24" terrain="2,1,2,1"/>
+ <tile id="32" terrain="1,0,1,1"/>
+ <tile id="33" terrain="0,0,1,1"/>
+ <tile id="34" terrain="0,1,1,1"/>
+ <tile id="38" terrain="1,2,1,1"/>
+ <tile id="39" terrain="2,2,1,1"/>
+ <tile id="40" terrain="2,1,1,1"/>
+ <tile id="48" terrain="0,0,0,1"/>
+ <tile id="49" terrain="0,0,1,0"/>
+ <tile id="54" terrain="2,2,2,1"/>
+ <tile id="55" terrain="2,2,1,2"/>
+ <tile id="64" terrain="0,1,0,0"/>
+ <tile id="65" terrain="1,0,0,0"/>
+ <tile id="70" terrain="2,1,2,2"/>
+ <tile id="71" terrain="1,2,2,2"/>
+ <tile id="177" terrain="1,1,1,1"/>
+ <wangsets>
+  <wangset name="Sick" edges="1" corners="7" tile="-1">
+   <wangtile tileid="51" wangid="0x30301030"/>
+   <wangtile tileid="54" wangid="0x40401040"/>
+   <wangtile tileid="80" wangid="0x10106010"/>
+   <wangtile tileid="55" wangid="0x40104040"/>
+   <wangtile tileid="4" wangid="0x10303010"/>
+   <wangtile tileid="129" wangid="0x60106060"/>
+   <wangtile tileid="115" wangid="0x10101070"/>
+   <wangtile tileid="117" wangid="0x70101010"/>
+   <wangtile tileid="100" wangid="0x70707070"/>
+   <wangtile tileid="19" wangid="0x10103030"/>
+   <wangtile tileid="49" wangid="0x20102020"/>
+   <wangtile tileid="85" wangid="0x10701010"/>
+   <wangtile tileid="48" wangid="0x20201020"/>
+   <wangtile tileid="0" wangid="0x10102010"/>
+   <wangtile tileid="2" wangid="0x10201010"/>
+   <wangtile tileid="22" wangid="0x10104040"/>
+   <wangtile tileid="112" wangid="0x10101060"/>
+   <wangtile tileid="40" wangid="0x40101010"/>
+   <wangtile tileid="23" wangid="0x40404040"/>
+   <wangtile tileid="9" wangid="0x10105010"/>
+   <wangtile tileid="58" wangid="0x50105050"/>
+   <wangtile tileid="67" wangid="0x30303010"/>
+   <wangtile tileid="65" wangid="0x10202020"/>
+   <wangtile tileid="68" wangid="0x10303030"/>
+   <wangtile tileid="33" wangid="0x20101020"/>
+   <wangtile tileid="132" wangid="0x70107070"/>
+   <wangtile tileid="177" wangid="0x10101010"/>
+   <wangtile tileid="145" wangid="0x10606060"/>
+   <wangtile tileid="52" wangid="0x30103030"/>
+   <wangtile tileid="148" wangid="0x10707070"/>
+   <wangtile tileid="71" wangid="0x10404040"/>
+   <wangtile tileid="113" wangid="0x60101060"/>
+   <wangtile tileid="147" wangid="0x70707010"/>
+   <wangtile tileid="74" wangid="0x10505050"/>
+   <wangtile tileid="42" wangid="0x50101050"/>
+   <wangtile tileid="73" wangid="0x50505010"/>
+   <wangtile tileid="24" wangid="0x40401010"/>
+   <wangtile tileid="27" wangid="0x50501010"/>
+   <wangtile tileid="81" wangid="0x10606010"/>
+   <wangtile tileid="114" wangid="0x60101010"/>
+   <wangtile tileid="97" wangid="0x60606060"/>
+   <wangtile tileid="16" wangid="0x10102020"/>
+   <wangtile tileid="8" wangid="0x10401010"/>
+   <wangtile tileid="6" wangid="0x10104010"/>
+   <wangtile tileid="5" wangid="0x10301010"/>
+   <wangtile tileid="99" wangid="0x10107070"/>
+   <wangtile tileid="20" wangid="0x30303030"/>
+   <wangtile tileid="64" wangid="0x20202010"/>
+   <wangtile tileid="37" wangid="0x30101010"/>
+   <wangtile tileid="35" wangid="0x10101030"/>
+   <wangtile tileid="84" wangid="0x10707010"/>
+   <wangtile tileid="144" wangid="0x60606010"/>
+   <wangtile tileid="70" wangid="0x40404010"/>
+   <wangtile tileid="131" wangid="0x70701070"/>
+   <wangtile tileid="32" wangid="0x10101020"/>
+   <wangtile tileid="7" wangid="0x10404010"/>
+   <wangtile tileid="82" wangid="0x10601010"/>
+   <wangtile tileid="128" wangid="0x60601060"/>
+   <wangtile tileid="18" wangid="0x20201010"/>
+   <wangtile tileid="25" wangid="0x10105050"/>
+   <wangtile tileid="21" wangid="0x30301010"/>
+   <wangtile tileid="83" wangid="0x10107010"/>
+   <wangtile tileid="36" wangid="0x30101030"/>
+   <wangtile tileid="41" wangid="0x10101050"/>
+   <wangtile tileid="116" wangid="0x70101070"/>
+   <wangtile tileid="10" wangid="0x10505010"/>
+   <wangtile tileid="43" wangid="0x50101010"/>
+   <wangtile tileid="3" wangid="0x10103010"/>
+   <wangtile tileid="98" wangid="0x60601010"/>
+   <wangtile tileid="39" wangid="0x40101040"/>
+   <wangtile tileid="101" wangid="0x70701010"/>
+   <wangtile tileid="26" wangid="0x50505050"/>
+   <wangtile tileid="11" wangid="0x10501010"/>
+   <wangtile tileid="17" wangid="0x20202020"/>
+   <wangtile tileid="1" wangid="0x10202010"/>
+   <wangtile tileid="96" wangid="0x10106060"/>
+   <wangtile tileid="34" wangid="0x20101010"/>
+   <wangtile tileid="57" wangid="0x50501050"/>
+   <wangtile tileid="38" wangid="0x10101040"/>
+  </wangset>
+  <wangset name="New Wang Set" edges="1" corners="2" tile="-1">
+   <wangtile tileid="2" wangid="0x20102020"/>
+   <wangtile tileid="18" wangid="0x10102020"/>
+   <wangtile tileid="177" wangid="0x20202020"/>
+   <wangtile tileid="1" wangid="0x20101020"/>
+   <wangtile tileid="48" wangid="0x10102010"/>
+   <wangtile tileid="34" wangid="0x10202020"/>
+   <wangtile tileid="64" wangid="0x10101020"/>
+   <wangtile tileid="0" wangid="0x20201020"/>
+   <wangtile tileid="32" wangid="0x20202010"/>
+   <wangtile tileid="65" wangid="0x20101010"/>
+   <wangtile tileid="33" wangid="0x10202010"/>
+   <wangtile tileid="17" wangid="0x10101010"/>
+   <wangtile tileid="16" wangid="0x20201010"/>
+   <wangtile tileid="49" wangid="0x10201010"/>
+  </wangset>
+ </wangsets>
+</tileset>
