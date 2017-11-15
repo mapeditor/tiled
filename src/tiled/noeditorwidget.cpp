244f4d769fd26292f1683403a9b0827faa18fd71
RhenaudTheLukark <Rhenaud@LAPTOP-IUGKLDLC>
2017-11-15 17:11:07 +0100

    Changes requested n°1 applied

--- src/tiled/noeditorwidget.cpp
+++ src/tiled/noeditorwidget.cpp
@@ -32,10 +32,10 @@ NoEditorWidget::NoEditorWidget(QWidget *parent) :
     ui(new Ui::NoEditorWidget)
 {
     ui->setupUi(this);
+
     connect(ui->newMapButton, &QPushButton::clicked, this, &NoEditorWidget::newMap);
     connect(ui->newTilesetButton, &QPushButton::clicked, this, &NoEditorWidget::newTileset);
-    connect(ui->openFileButton, &QPushButton::clicked, this, &NoEditorWidget::openFile);
-}
+    connect(ui->openFileButton, &QPushButton::clicked, DocumentManager::instance(), &DocumentManager::openFile);
 
 NoEditorWidget::~NoEditorWidget()
 {
@@ -62,12 +62,6 @@ void NoEditorWidget::newMap()
 void NoEditorWidget::newTileset()
 {
     ActionManager::action("file.new_tileset")->trigger();
-}
-
-void NoEditorWidget::openFile()
-{
-    DocumentManager *documentManager = DocumentManager::instance();
-    documentManager->openFile();
 }
 
 } // namespace Internal
