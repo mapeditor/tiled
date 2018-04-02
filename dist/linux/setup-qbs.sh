9eed2bcf36480e808a84baf39e6dee63c4db9437
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-04-03 00:15:09 +0200

    Updated automated builds to use Qbs 1.11
    Fixed the macOS build on Travis, which was already using 1.11 from brew
    and thus broken due to a change in the command-line for choosing the
    build config.

--- dist/linux/setup-qbs.sh
+++ dist/linux/setup-qbs.sh
@@ -3,7 +3,7 @@ then
     CXX=g++
 fi
 
-git clone --depth 1 -b v1.9.1 https://github.com/qbs/qbs.git qbs-build
+git clone --depth 1 -b v1.11.0 git://code.qt.io/qbs/qbs.git qbs-build
 pushd qbs-build
 qmake -r qbs.pro QMAKE_CXX=$CXX QMAKE_LINK=$CXX \
                  "CONFIG+=qbs_no_dev_install qbs_no_man_install" \
