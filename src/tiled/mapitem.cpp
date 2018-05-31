mapeditor/tiled

commit aa85acf7f787bf36605a5dacf24194f34c460852

Worlds: Report errors and refresh on load/unload
Added error reporting when loading a world fails.

Also added a worldsChanged signal, which is used to immediately update
the map scenes. While refreshing a scene, existing MapItem instances are
now reused when applicable.

Issue #1669

src/tiled/mapitem.cpp
@@ -20,6 +20,7 @@
 
 #include "mapitem.h"
 
+#include "documentmanager.h"
 #include "grouplayer.h"
 #include "grouplayeritem.h"
 #include "imagelayeritem.h"
@@ -40,35 +41,25 @@
 #include <QPen>
 #include <QWidget>
 
+#include "qtcompat_p.h"
+
 namespace Tiled {
 namespace Internal {
 
 static const qreal darkeningFactor = 0.6;
 static const qreal opacityFactor = 0.4;
 
-MapItem::MapItem(MapDocument *mapDocument, DisplayMode displayMode,
+MapItem::MapItem(const MapDocumentPtr &mapDocument, DisplayMode displayMode,
                  QGraphicsItem *parent)
     : QGraphicsObject(parent)
-    , mMapDocument(mapDocument->sharedFromThis())
+    , mMapDocument(mapDocument)
     , mDarkRectangle(new QGraphicsRectItem(this))
-    , mDisplayMode(displayMode)
+    , mDisplayMode(Editable)
 {
     // Since we don't do any painting, we can spare us the call to paint()
     setFlag(QGraphicsItem::ItemHasNoContents);
 
-    // In read-only display mode, we are a link to the editable view for our map
-    if (displayMode == ReadOnly)
-        setCursor(Qt::PointingHandCursor);
-
     createLayerItems(mapDocument->map()->layers());
-
-    if (displayMode == Editable) {
-        auto tileSelectionItem = new TileSelectionItem(mapDocument, this);
-        tileSelectionItem->setZValue(10000 - 2);
-
-        auto objectSelectionItem = new ObjectSelectionItem(mapDocument, this);
-        objectSelectionItem->setZValue(10000 - 1);
-    }
 
     Preferences *prefs = Preferences::instance();
 
@@ -81,22 +72,22 @@ MapItem::MapItem(MapDocument *mapDocument, DisplayMode displayMode,
     connect(prefs, &Preferences::highlightCurrentLayerChanged, this, &MapItem::updateCurrentLayerHighlight);
     connect(prefs, &Preferences::objectTypesChanged, this, &MapItem::syncAllObjectItems);
 
-    connect(mapDocument, &MapDocument::mapChanged, this, &MapItem::mapChanged);
-    connect(mapDocument, &MapDocument::regionChanged, this, &MapItem::repaintRegion);
-    connect(mapDocument, &MapDocument::tileLayerChanged, this, &MapItem::tileLayerChanged);
-    connect(mapDocument, &MapDocument::layerAdded, this, &MapItem::layerAdded);
-    connect(mapDocument, &MapDocument::layerRemoved, this, &MapItem::layerRemoved);
-    connect(mapDocument, &MapDocument::layerChanged, this, &MapItem::layerChanged);
-    connect(mapDocument, &MapDocument::objectGroupChanged, this, &MapItem::objectGroupChanged);
-    connect(mapDocument, &MapDocument::imageLayerChanged, this, &MapItem::imageLayerChanged);
-    connect(mapDocument, &MapDocument::selectedLayersChanged, this, &MapItem::updateCurrentLayerHighlight);
-    connect(mapDocument, &MapDocument::tilesetTileOffsetChanged, this, &MapItem::adaptToTilesetTileSizeChanges);
-    connect(mapDocument, &MapDocument::tileImageSourceChanged, this, &MapItem::adaptToTileSizeChanges);
-    connect(mapDocument, &MapDocument::tilesetReplaced, this, &MapItem::tilesetReplaced);
-    connect(mapDocument, &MapDocument::objectsInserted, this, &MapItem::objectsInserted);
-    connect(mapDocument, &MapDocument::objectsRemoved, this, &MapItem::objectsRemoved);
-    connect(mapDocument, &MapDocument::objectsChanged, this, &MapItem::objectsChanged);
-    connect(mapDocument, &MapDocument::objectsIndexChanged, this, &MapItem::objectsIndexChanged);
+    connect(mapDocument.data(), &MapDocument::mapChanged, this, &MapItem::mapChanged);
+    connect(mapDocument.data(), &MapDocument::regionChanged, this, &MapItem::repaintRegion);
+    connect(mapDocument.data(), &MapDocument::tileLayerChanged, this, &MapItem::tileLayerChanged);
+    connect(mapDocument.data(), &MapDocument::layerAdded, this, &MapItem::layerAdded);
+    connect(mapDocument.data(), &MapDocument::layerRemoved, this, &MapItem::layerRemoved);
+    connect(mapDocument.data(), &MapDocument::layerChanged, this, &MapItem::layerChanged);
+    connect(mapDocument.data(), &MapDocument::objectGroupChanged, this, &MapItem::objectGroupChanged);
+    connect(mapDocument.data(), &MapDocument::imageLayerChanged, this, &MapItem::imageLayerChanged);
+    connect(mapDocument.data(), &MapDocument::selectedLayersChanged, this, &MapItem::updateCurrentLayerHighlight);
+    connect(mapDocument.data(), &MapDocument::tilesetTileOffsetChanged, this, &MapItem::adaptToTilesetTileSizeChanges);
+    connect(mapDocument.data(), &MapDocument::tileImageSourceChanged, this, &MapItem::adaptToTileSizeChanges);
+    connect(mapDocument.data(), &MapDocument::tilesetReplaced, this, &MapItem::tilesetReplaced);
+    connect(mapDocument.data(), &MapDocument::objectsInserted, this, &MapItem::objectsInserted);
+    connect(mapDocument.data(), &MapDocument::objectsRemoved, this, &MapItem::objectsRemoved);
+    connect(mapDocument.data(), &MapDocument::objectsChanged, this, &MapItem::objectsChanged);
+    connect(mapDocument.data(), &MapDocument::objectsIndexChanged, this, &MapItem::objectsIndexChanged);
 
     updateBoundingRect();
 
@@ -105,6 +96,48 @@ MapItem::MapItem(MapDocument *mapDocument, DisplayMode displayMode,
     mDarkRectangle->setOpacity(darkeningFactor);
     mDarkRectangle->setRect(QRect(INT_MIN / 512, INT_MIN / 512,
                                   INT_MAX / 256, INT_MAX / 256));
+
+    if (displayMode == ReadOnly)
+        setDisplayMode(displayMode);
+    else
+        updateCurrentLayerHighlight();
+}
+
+MapItem::~MapItem() = default;
+
+void MapItem::setDisplayMode(DisplayMode displayMode)
+{
+    if (mDisplayMode == displayMode)
+        return;
+
+    mDisplayMode = displayMode;
+
+    // Enabled state is checked by selection tools
+    for (LayerItem *layerItem : qAsConst(mLayerItems))
+        layerItem->setEnabled(displayMode == Editable);
+
+    if (displayMode == ReadOnly) {
+        // In read-only display mode, we are a link to the editable view for our map
+        setCursor(Qt::PointingHandCursor);
+
+        setOpacity(0.5);
+        setZValue(-1);
+
+        mTileSelectionItem.reset(new TileSelectionItem(mapDocument(), this));
+        mTileSelectionItem->setZValue(10000 - 2);
+
+        mObjectSelectionItem.reset(new ObjectSelectionItem(mapDocument(), this));
+        mObjectSelectionItem->setZValue(10000 - 1);
+    } else {
+        unsetCursor();
+
+        setOpacity(1.0);
+        setZValue(0);
+
+        mTileSelectionItem.reset();
+        mObjectSelectionItem.reset();
+    }
+
     updateCurrentLayerHighlight();
 }
 

src/libtiled/worldmanager.cpp
@@ -34,6 +34,7 @@
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QScopedPointer>
 
 #include <QDebug>
 
@@ -41,9 +42,7 @@ namespace Tiled {
 
 WorldManager *WorldManager::mInstance;
 
-WorldManager::WorldManager()
-{
-}
+WorldManager::WorldManager() = default;
 
 WorldManager::~WorldManager()
 {
@@ -64,18 +63,30 @@ void WorldManager::deleteInstance()
     mInstance = nullptr;
 }
 
-void WorldManager::loadWorld(const QString &fileName)
+/**
+ * Loads the world with the given \a fileName.
+ *
+ * \returns whether the world was loaded succesfully, optionally setting
+ *          \a errorString when not.
+ */
+bool WorldManager::loadWorld(const QString &fileName, QString *errorString)
 {
     unloadWorld(fileName);  // unload possibly existing world
 
     QFile file(fileName);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        return;
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        if (errorString)
+            *errorString = tr("Could not open file for reading.");
+        return false;
+    }
 
     QJsonParseError error;
     const QJsonObject object = QJsonDocument::fromJson(file.readAll(), &error).object();
-    if (error.error != QJsonParseError::NoError)
-        return;
+    if (error.error != QJsonParseError::NoError) {
+        if (errorString)
+            *errorString = tr("JSON parse error at offset %1:\n%2.").arg(error.offset).arg(error.errorString());
+        return false;
+    }
 
     QDir dir = QFileInfo(fileName).dir();
     QScopedPointer<World> world(new World);
@@ -126,16 +137,24 @@ void WorldManager::loadWorld(const QString &fileName)
     world->onlyShowAdjacentMaps = object.value(QLatin1String("onlyShowAdjacentMaps")).toBool();
 
     mWorlds.insert(fileName, world.take());
+    emit worldsChanged();
+
+    return true;
 }
 
+/**
+ * Unloads the world with the given \a fileName.
+ */
 void WorldManager::unloadWorld(const QString &fileName)
 {
-    delete mWorlds.take(fileName);
+    QScopedPointer<World> world(mWorlds.take(fileName));
+    if (world)
+        emit worldsChanged();
 }
 
 const World *WorldManager::worldForMap(const QString &fileName) const
 {
-    for (const World *world : mWorlds)
+    for (auto world : mWorlds)
         if (world->containsMap(fileName))
             return world;
 

src/libtiled/worldmanager.h
@@ -31,7 +31,9 @@
 
 #include "tiled_global.h"
 
-#include <QHash>
+#include <QCoreApplication>
+#include <QMap>
+#include <QObject>
 #include <QPoint>
 #include <QRect>
 #include <QRegularExpression>
@@ -69,26 +71,30 @@ struct TILEDSHARED_EXPORT World
     QVector<MapEntry> contextMaps(const QString &fileName) const;
 };
 
-class TILEDSHARED_EXPORT WorldManager
+class TILEDSHARED_EXPORT WorldManager : public QObject
 {
-    Q_DISABLE_COPY(WorldManager)
+    Q_OBJECT
 
 public:
     static WorldManager &instance();
     static void deleteInstance();
 
-    void loadWorld(const QString &fileName);
+    bool loadWorld(const QString &fileName, QString *errorString = nullptr);
     void unloadWorld(const QString &fileName);
 
-    const QHash<QString, World*> &worlds() const { return mWorlds; }
+    const QMap<QString, World*> &worlds() const { return mWorlds; }
+    QStringList loadedWorldFiles() const { return mWorlds.keys(); }
 
     const World *worldForMap(const QString &fileName) const;
+
+signals:
+    void worldsChanged();
 
 private:
     WorldManager();
     ~WorldManager();
 
-    QHash<QString, World*> mWorlds;
+    QMap<QString, World*> mWorlds;
 
     static WorldManager *mInstance;
 };

src/tiled/mainwindow.cpp
@@ -418,20 +418,26 @@ MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
             return;
 
         preferences->setLastPath(Preferences::WorldFile, QFileInfo(worldFile).path());
-        WorldManager::instance().loadWorld(worldFile);
-        mSettings.setValue(QLatin1String("LoadedWorlds"),
-                           QVariant(WorldManager::instance().worlds().keys()));
-        mUi->menuUnloadWorld->setEnabled(!WorldManager::instance().worlds().isEmpty());
+        QString errorString;
+        if (!WorldManager::instance().loadWorld(worldFile, &errorString)) {
+            QMessageBox::critical(this, tr("Error Loading World"), errorString);
+        } else {
+            const auto worldFiles = WorldManager::instance().loadedWorldFiles();
+            mSettings.setValue(QLatin1String("LoadedWorlds"), QVariant(worldFiles));
+            mUi->menuUnloadWorld->setEnabled(!worldFiles.isEmpty());
+        }
     });
     connect(mUi->menuUnloadWorld, &QMenu::aboutToShow, this, [this] {
         mUi->menuUnloadWorld->clear();
-        for (const QString &fileName : WorldManager::instance().worlds().keys()) {
+
+        const auto worldFiles = WorldManager::instance().loadedWorldFiles();
+        for (const QString &fileName : worldFiles) {
             QAction *unloadAction = mUi->menuUnloadWorld->addAction(fileName);
             connect(unloadAction, &QAction::triggered, this, [this,fileName] {
                 WorldManager::instance().unloadWorld(fileName);
-                mSettings.setValue(QLatin1String("LoadedWorlds"),
-                                   QVariant(WorldManager::instance().worlds().keys()));
-                mUi->menuUnloadWorld->setEnabled(!WorldManager::instance().worlds().isEmpty());
+                const auto worldFiles = WorldManager::instance().loadedWorldFiles();
+                mSettings.setValue(QLatin1String("LoadedWorlds"), QVariant(worldFiles));
+                mUi->menuUnloadWorld->setEnabled(!worldFiles.isEmpty());
             });
         }
     });

src/tiled/mapitem.h
@@ -20,12 +20,11 @@
 
 #pragma once
 
-#include "documentmanager.h"
 #include "mapdocument.h"
 
 #include <QGraphicsObject>
 #include <QMap>
-#include <QSet>
+#include <QScopedPointer>
 
 namespace Tiled {
 
@@ -41,6 +40,8 @@ namespace Internal {
 
 class LayerItem;
 class MapObjectItem;
+class ObjectSelectionItem;
+class TileSelectionItem;
 
 /**
  * A graphics item that represents the contents of a map.
@@ -58,10 +59,13 @@ public:
         Editable
     };
 
-    MapItem(MapDocument *mapDocument, DisplayMode displayMode,
+    MapItem(const MapDocumentPtr &mapDocument, DisplayMode displayMode,
             QGraphicsItem *parent = nullptr);
+    ~MapItem() override;
 
     MapDocument *mapDocument() const;
+
+    void setDisplayMode(DisplayMode displayMode);
 
     // QGraphicsItem
     QRectF boundingRect() const override;
@@ -115,6 +119,8 @@ private:
 
     MapDocumentPtr mMapDocument;
     QGraphicsRectItem *mDarkRectangle;
+    QScopedPointer<TileSelectionItem> mTileSelectionItem;
+    QScopedPointer<ObjectSelectionItem> mObjectSelectionItem;
     QMap<Layer*, LayerItem*> mLayerItems;
     QMap<MapObject*, MapObjectItem*> mObjectItems;
     DisplayMode mDisplayMode;

src/tiled/mapscene.cpp
@@ -26,8 +26,8 @@
 #include "abstracttool.h"
 #include "addremovemapobject.h"
 #include "containerhelpers.h"
+#include "documentmanager.h"
 #include "map.h"
-#include "mapitem.h"
 #include "mapobject.h"
 #include "maprenderer.h"
 #include "objectgroup.h"
@@ -74,6 +74,9 @@ MapScene::MapScene(QObject *parent):
     connect(prefs, &Preferences::gridColorChanged, this, [this] { update(); });
 
     mGridVisible = prefs->showGrid();
+
+    WorldManager &worldManager = WorldManager::instance();
+    connect(&worldManager, &WorldManager::worldsChanged, this, &MapScene::refreshScene);
 
     // Install an event filter so that we can get key events on behalf of the
     // active tool without having to have the current focus.
@@ -126,11 +129,12 @@ void MapScene::setSelectedTool(AbstractTool *tool)
  */
 void MapScene::refreshScene()
 {
-    clear();
-    mMapItems.clear();
+    QHash<MapDocument*, MapItem*> mapItems;
 
     if (!mMapDocument) {
-        setSceneRect(QRectF());
+        mMapItems.swap(mapItems);
+        qDeleteAll(mapItems);
+        updateSceneRect();
         return;
     }
 
@@ -155,24 +159,18 @@ void MapScene::refreshScene()
                 if (mapDocument == mMapDocument)
                     displayMode = MapItem::Editable;
 
-                auto mapItem = new MapItem(mapDocument.data(), displayMode);
+                auto mapItem = takeOrCreateMapItem(mapDocument, displayMode);
                 mapItem->setPos(mapEntry.rect.topLeft() - currentMapPosition);
-                connect(mapItem, &MapItem::boundingRectChanged, this, &MapScene::updateSceneRect);
-                mMapItems.insert(mapDocument.data(), mapItem);
-                addItem(mapItem);
-
-                if (mapDocument != mMapDocument) {
-                    mapItem->setOpacity(0.5);
-                    mapItem->setZValue(-1);
-                }
+                mapItems.insert(mapDocument.data(), mapItem);
             }
         }
     } else {
-        auto mapItem = new MapItem(mMapDocument, MapItem::Editable);
-        connect(mapItem, &MapItem::boundingRectChanged, this, &MapScene::updateSceneRect);
-        mMapItems.insert(mMapDocument, mapItem);
-        addItem(mapItem);
+        auto mapItem = takeOrCreateMapItem(mMapDocument->sharedFromThis(), MapItem::Editable);
+        mapItems.insert(mMapDocument, mapItem);
     }
+
+    mMapItems.swap(mapItems);
+    qDeleteAll(mapItems);       // delete all map items that didn't get reused
 
     updateSceneRect();
 
@@ -200,6 +198,20 @@ void MapScene::updateSceneRect()
         sceneRect |= mapItem->boundingRect().translated(mapItem->pos());
 
     setSceneRect(sceneRect);
+}
+
+MapItem *MapScene::takeOrCreateMapItem(const MapDocumentPtr &mapDocument, MapItem::DisplayMode displayMode)
+{
+    // Try to reuse an existing map item
+    auto mapItem = mMapItems.take(mapDocument.data());
+    if (!mapItem) {
+        mapItem = new MapItem(mapDocument, displayMode);
+        connect(mapItem, &MapItem::boundingRectChanged, this, &MapScene::updateSceneRect);
+        addItem(mapItem);
+    } else {
+        mapItem->setDisplayMode(displayMode);
+    }
+    return mapItem;
 }
 
 /**

src/tiled/mapscene.h
@@ -24,6 +24,7 @@
 #pragma once
 
 #include "mapdocument.h"
+#include "mapitem.h"
 
 #include <QColor>
 #include <QGraphicsScene>
@@ -45,7 +46,6 @@ class LayerItem;
 class MapDocument;
 class MapObjectItem;
 class MapScene;
-class MapItem;
 class ObjectGroupItem;
 
 /**
@@ -103,6 +103,9 @@ private slots:
 private:
     void updateDefaultBackgroundColor();
     void updateSceneRect();
+
+    MapItem *takeOrCreateMapItem(const MapDocumentPtr &mapDocument,
+                                 MapItem::DisplayMode displayMode);
 
     bool eventFilter(QObject *object, QEvent *event) override;
 

commit 668831b5d71d33914e724def482d930d445a9af2

Fixed display of tile selection
Was broken by aa85acf7f787bf36605a5dacf24194f34c460852.

src/tiled/mapitem.cpp
@@ -97,10 +97,17 @@ MapItem::MapItem(const MapDocumentPtr &mapDocument, DisplayMode displayMode,
     mDarkRectangle->setRect(QRect(INT_MIN / 512, INT_MIN / 512,
                                   INT_MAX / 256, INT_MAX / 256));
 
-    if (displayMode == ReadOnly)
+    if (displayMode == ReadOnly) {
         setDisplayMode(displayMode);
-    else
+    } else {
         updateCurrentLayerHighlight();
+
+        mTileSelectionItem.reset(new TileSelectionItem(mapDocument.data(), this));
+        mTileSelectionItem->setZValue(10000 - 2);
+
+        mObjectSelectionItem.reset(new ObjectSelectionItem(mapDocument.data(), this));
+        mObjectSelectionItem->setZValue(10000 - 1);
+    }
 }
 
 MapItem::~MapItem() = default;
@@ -123,19 +130,19 @@ void MapItem::setDisplayMode(DisplayMode displayMode)
         setOpacity(0.5);
         setZValue(-1);
 
-        mTileSelectionItem.reset(new TileSelectionItem(mapDocument(), this));
-        mTileSelectionItem->setZValue(10000 - 2);
-
-        mObjectSelectionItem.reset(new ObjectSelectionItem(mapDocument(), this));
-        mObjectSelectionItem->setZValue(10000 - 1);
+        mTileSelectionItem.reset();
+        mObjectSelectionItem.reset();
     } else {
         unsetCursor();
 
         setOpacity(1.0);
         setZValue(0);
 
-        mTileSelectionItem.reset();
-        mObjectSelectionItem.reset();
+        mTileSelectionItem.reset(new TileSelectionItem(mapDocument(), this));
+        mTileSelectionItem->setZValue(10000 - 2);
+
+        mObjectSelectionItem.reset(new ObjectSelectionItem(mapDocument(), this));
+        mObjectSelectionItem->setZValue(10000 - 1);
     }
 
     updateCurrentLayerHighlight();