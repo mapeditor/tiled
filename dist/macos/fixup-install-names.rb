e97600a24a84d9566796689b0e2e8da47df43438
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-08-06 22:54:29 +0200

    macOS: Small wording correction in distribution script
    This message also applies to executables.

--- dist/macos/fixup-install-names.rb
+++ dist/macos/fixup-install-names.rb
@@ -18,7 +18,7 @@ Dir["Tiled.app/**/*.dylib",
         next if qtdependency.include? "@executable_path"
 
         #if we get here, this library has a dependency on a qtlib with a hard path on the build systems disk
-        puts "Fixing library #{library} dependency on #{qtlib}"
+        puts "Fixing #{library} dependency on #{qtlib}"
         `install_name_tool -change "#{qtdependency}" "@executable_path/../Frameworks/#{qtlib}.framework/Versions/5/#{qtlib}" "#{library}"`
         raise "install_name_tool error #{$?}" unless $? == 0
     end
