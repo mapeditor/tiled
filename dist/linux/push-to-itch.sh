a8207bd65e41d3632ada3c6c7797e0fb1760c0de
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-11-13 23:10:38 +0100

    Travis CI: Fixed deployment to itch.io
    wget was downloading "butler" as "default" due to the URL used.

--- dist/linux/push-to-itch.sh
+++ dist/linux/push-to-itch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
-wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
+wget -O butler https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
 chmod +x ./butler
 ./butler push --userversion=$TILED_VERSION Tiled thorbjorn/tiled:linux-64bit-snapshot
