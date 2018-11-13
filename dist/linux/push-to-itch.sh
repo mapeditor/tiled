5fbce71b3ee3586c8c9ea69b35441ad909e2d828
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-11-13 23:33:23 +0100

    Travis CI: Fixed butler installation
    Getting a zip archive from this URL now...

--- dist/linux/push-to-itch.sh
+++ dist/linux/push-to-itch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
-wget -O butler https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
-chmod +x ./butler
+wget https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
+unzip default
 ./butler push --userversion=$TILED_VERSION Tiled thorbjorn/tiled:linux-64bit-snapshot
--- dist/macos/push-to-itch.sh
+++ dist/macos/push-to-itch.sh
@@ -2,6 +2,6 @@
 set -ev
 mkdir itch
 mv install/Tiled.app itch/
-curl https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default --output butler
-chmod +x ./butler
+curl https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default --output default
+unzip default
 ./butler push --userversion=$TILED_VERSION itch thorbjorn/tiled:macos-snapshot
