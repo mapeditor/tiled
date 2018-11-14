af6f8b71bbb4dd8b267824cd42f14d94b8b51376
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-11-14 08:33:14 +0100

    Travis CI: Fixed butler installation on macOS
    curl was not following the redirect.
    
    Also changed the Linux build to use curl as well, for consistency.

--- dist/macos/push-to-itch.sh
+++ dist/macos/push-to-itch.sh
@@ -2,6 +2,6 @@
 set -ev
 mkdir itch
 mv install/Tiled.app itch/
-curl https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default --output default
-unzip default
+curl -JOL https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default
+unzip butler-darwin-amd64.zip
 ./butler push --userversion=$TILED_VERSION itch thorbjorn/tiled:macos-snapshot
