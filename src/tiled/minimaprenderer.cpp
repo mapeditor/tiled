f66e0b9601d7bab2acce3ea6e17731b1b1c0a581
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2018-12-16 15:57:14 +0100

    Fixed small issue in bounding rect calculation in mini-map renderer
    Was not taking into account tiles extending from nested tile layers.
    This issue is not visible anywhere, since this extension is only done
    for the thumbnails in the tile stamps, which never use nested layers.

--- src/tiled/minimaprenderer.cpp
+++ src/tiled/minimaprenderer.cpp
@@ -109,10 +109,7 @@ static void extendMapRect(QRect &mapBoundingRect, const MapRenderer &renderer)
     QRectF rect(mapBoundingRect);
 
     // Take into account large tiles extending beyond their cell
-    for (const Layer *layer : renderer.map()->layers()) {
-        if (layer->layerType() != Layer::TileLayerType)
-            continue;
-
+    for (const Layer *layer : renderer.map()->tileLayers()) {
         const TileLayer *tileLayer = static_cast<const TileLayer*>(layer);
         const QPointF offset = tileLayer->totalOffset();
 
