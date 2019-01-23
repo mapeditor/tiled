43017b124e0f5acfa7faef22b26d7ffe568f955f
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2019-01-23 15:58:20 +0100

    Travis CI: Build Qbs 1.11.1 on macOS against Qt 5.9.7
    Qbs in Homebrew is built against Qt 5.12, which causes an issue.
    
    Getting this to work took way more time than it should have...

--- dist/linux/setup-qbs.sh
+++ dist/linux/setup-qbs.sh
@@ -3,6 +3,9 @@ then
     CXX=g++
 fi
 
+# - Not using Qbs 1.12.0, 1.12.1 or 1.12.2 because of a false "Duplicate source file" error (QBS-1416)
+# - Make sure not to build against Qt 5.12 to avoid build issues on macOS (QBS-1417)
+
 git clone --depth 1 -b v1.11.1 git://code.qt.io/qbs/qbs.git qbs-build
 pushd qbs-build
 qmake -r qbs.pro QMAKE_CXX=$CXX QMAKE_LINK=$CXX \
