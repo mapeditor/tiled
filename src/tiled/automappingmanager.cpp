mapeditor/tiled

commit 2eaed1fb593f7bb72e174ab664f73e86e2de8479

More cleanups based on Clazy checks (level 0)

src/tiled/automappingmanager.cpp
@@ -166,10 +166,12 @@ bool AutomappingManager::loadFile(const QString &filePath)
                 || rulePath.startsWith(QLatin1String("//")))
             continue;
 
-        if (QFileInfo(rulePath).isRelative())
+        const QFileInfo rulePathInfo(rulePath);
+
+        if (rulePathInfo.isRelative())
             rulePath = absPath + QLatin1Char('/') + rulePath;
 
-        if (!QFileInfo(rulePath).exists()) {
+        if (!rulePathInfo.exists()) {
             mError += tr("File not found:\n%1").arg(rulePath) + QLatin1Char('\n');
             ret = false;
             continue;

src/libtiled/map.h
@@ -65,12 +65,6 @@ class TILEDSHARED_EXPORT Map : public Object
     Q_PROPERTY(int tileHeight READ tileHeight NOTIFY tileHeightChanged)
     Q_PROPERTY(QSize size READ size NOTIFY sizeChanged)
 
-    Q_ENUMS(Orientation
-            LayerDataFormat
-            RenderOrder
-            StaggerAxis
-            StaggerIndex)
-
     class LayerIteratorHelper
     {
     public:
@@ -100,6 +94,7 @@ public:
         Staggered,
         Hexagonal
     };
+    Q_ENUM(Orientation)
 
     /**
      * The different formats in which the tile layer data can be stored.
@@ -111,6 +106,7 @@ public:
         Base64Zlib = 3,
         CSV        = 4
     };
+    Q_ENUM(LayerDataFormat)
 
     /**
      * The order in which tiles are rendered on screen.
@@ -121,6 +117,7 @@ public:
         LeftDown   = 2,
         LeftUp     = 3
     };
+    Q_ENUM(RenderOrder)
 
     /**
      * Which axis is staggered. Only used by the isometric staggered and
@@ -130,6 +127,7 @@ public:
         StaggerX,
         StaggerY
     };
+    Q_ENUM(StaggerAxis)
 
     /**
      * When staggering, specifies whether the odd or the even rows/columns are
@@ -140,6 +138,7 @@ public:
         StaggerOdd  = 0,
         StaggerEven = 1
     };
+    Q_ENUM(StaggerIndex)
 
     Map();
 

src/libtiled/mapreader.cpp
@@ -1120,10 +1120,10 @@ QPolygonF MapReaderPrivate::readPolygon()
             break;
         }
 
-        const qreal x = point.left(commaPos).toDouble(&ok);
+        const qreal x = point.leftRef(commaPos).toDouble(&ok);
         if (!ok)
             break;
-        const qreal y = point.mid(commaPos + 1).toDouble(&ok);
+        const qreal y = point.midRef(commaPos + 1).toDouble(&ok);
         if (!ok)
             break;
 

src/tiled/changetileterrain.h
@@ -20,7 +20,7 @@
 
 #pragma once
 
-#include <QMap>
+#include <QHash>
 #include <QUndoCommand>
 
 #include "undocommands.h"
@@ -44,7 +44,7 @@ public:
         unsigned to;
     };
 
-    typedef QMap<Tile *, Change> Changes;
+    typedef QHash<Tile *, Change> Changes;
 
     /**
      * Constructs an empty command that changes no terrain. When merged into

src/tiled/editpolygontool.cpp
@@ -104,7 +104,7 @@ void EditPolygonTool::deactivate(MapScene *scene)
     abortCurrentAction();
 
     // Delete all handles
-    QMapIterator<MapObject*, QList<PointHandle*> > i(mHandles);
+    QHashIterator<MapObject*, QList<PointHandle*> > i(mHandles);
     while (i.hasNext())
         qDeleteAll(i.next().value());
 
@@ -399,7 +399,7 @@ void EditPolygonTool::updateHandles()
     };
 
     // First destroy the handles for objects that are no longer selected
-    QMutableMapIterator<MapObject*, QList<PointHandle*> > i(mHandles);
+    QMutableHashIterator<MapObject*, QList<PointHandle*> > i(mHandles);
     while (i.hasNext()) {
         i.next();
         if (!selection.contains(i.key())) {
@@ -595,7 +595,7 @@ void EditPolygonTool::finishMoving(const QPointF &pos)
 
     // TODO: This isn't really optimal. Would be better to have a single undo
     // command that supports changing multiple map objects.
-    QMapIterator<MapObject*, QPolygonF> i(mOldPolygons);
+    QHashIterator<MapObject*, QPolygonF> i(mOldPolygons);
     while (i.hasNext()) {
         i.next();
         undoStack->push(new ChangePolygon(mapDocument(), i.key(), i.value()));
@@ -617,7 +617,7 @@ void EditPolygonTool::abortCurrentAction(const QList<MapObject *> &removedObject
         break;
     case Moving:
         // Reset the polygons
-        QMapIterator<MapObject*, QPolygonF> i(mOldPolygons);
+        QHashIterator<MapObject*, QPolygonF> i(mOldPolygons);
         while (i.hasNext()) {
             i.next();
 
@@ -718,7 +718,7 @@ QSet<PointHandle *> EditPolygonTool::clickedHandles() const
     return handles;
 }
 
-typedef QMap<MapObject*, RangeSet<int> > PointIndexesByObject;
+typedef QHash<MapObject*, RangeSet<int> > PointIndexesByObject;
 static PointIndexesByObject
 groupIndexesByObject(const QSet<PointHandle*> &handles)
 {
@@ -739,7 +739,7 @@ void EditPolygonTool::deleteNodes()
         return;
 
     PointIndexesByObject p = groupIndexesByObject(mSelectedHandles);
-    QMapIterator<MapObject*, RangeSet<int> > i(p);
+    QHashIterator<MapObject*, RangeSet<int> > i(p);
 
     QUndoStack *undoStack = mapDocument()->undoStack();
 
@@ -902,7 +902,7 @@ void EditPolygonTool::joinNodes()
         return;
 
     const PointIndexesByObject p = groupIndexesByObject(mSelectedHandles);
-    QMapIterator<MapObject*, RangeSet<int> > i(p);
+    QHashIterator<MapObject*, RangeSet<int> > i(p);
 
     QUndoStack *undoStack = mapDocument()->undoStack();
     bool macroStarted = false;
@@ -938,7 +938,7 @@ void EditPolygonTool::splitSegments()
         return;
 
     const PointIndexesByObject p = groupIndexesByObject(mSelectedHandles);
-    QMapIterator<MapObject*, RangeSet<int> > i(p);
+    QHashIterator<MapObject*, RangeSet<int> > i(p);
 
     QUndoStack *undoStack = mapDocument()->undoStack();
     bool macroStarted = false;

src/tiled/editpolygontool.h
@@ -22,7 +22,7 @@
 
 #include "abstractobjecttool.h"
 
-#include <QMap>
+#include <QHash>
 #include <QSet>
 
 #include <memory>
@@ -121,7 +121,7 @@ private:
     InteractedSegment mClickedSegment;
     MapObject *mClickedObject;
     QVector<QPointF> mOldHandlePositions;
-    QMap<MapObject*, QPolygonF> mOldPolygons;
+    QHash<MapObject*, QPolygonF> mOldPolygons;
     QPointF mAlignPosition;
     Action mAction;
     QPointF mStart;
@@ -130,7 +130,7 @@ private:
     Qt::KeyboardModifiers mModifiers;
 
     /// The list of handles associated with each selected map object
-    QMap<MapObject*, QList<PointHandle*> > mHandles;
+    QHash<MapObject*, QList<PointHandle*> > mHandles;
     QSet<PointHandle*> mSelectedHandles;
     QSet<PointHandle*> mHighlightedHandles;
 };

src/tiled/mapdocument.cpp
@@ -1221,8 +1221,8 @@ void MapDocument::moveObjectsToGroup(const QList<MapObject *> &objects,
     undoStack()->endMacro();
 }
 
-typedef QMap<ObjectGroup*, RangeSet<int>>           Ranges;
-typedef QMapIterator<ObjectGroup*, RangeSet<int>>   RangesIterator;
+typedef QHash<ObjectGroup*, RangeSet<int>>           Ranges;
+typedef QHashIterator<ObjectGroup*, RangeSet<int>>   RangesIterator;
 
 static Ranges computeRanges(const QList<MapObject *> &objects)
 {

src/tiled/preferencesdialog.cpp
@@ -51,8 +51,8 @@ PreferencesDialog::PreferencesDialog(QWidget *parent)
     for (const QString &name : qAsConst(mLanguages)) {
         QLocale locale(name);
         QString string = QString(QLatin1String("%1 (%2)"))
-            .arg(QLocale::languageToString(locale.language()))
-            .arg(QLocale::countryToString(locale.country()));
+            .arg(QLocale::languageToString(locale.language()),
+                 QLocale::countryToString(locale.country()));
         mUi->languageCombo->addItem(string, name);
     }
 

commit 19c508d26cb770afe6491c106712c5bd0776a8cb

AutoMap: Fixed handling of relative paths in rules.txt
Broke in change 2eaed1fb593f7bb72e174ab664f73e86e2de8479 due to reusing
a QFileInfo that actually wasn't instantiated with the same path before.

src/tiled/automappingmanager.cpp
@@ -27,9 +27,10 @@
 #include "tmxmapformat.h"
 #include "preferences.h"
 
+#include <QDir>
 #include <QFileInfo>
-#include <QTextStream>
 #include <QFileSystemWatcher>
+#include <QTextStream>
 
 #include "qtcompat_p.h"
 
@@ -140,7 +141,7 @@ void AutomappingManager::autoMapInternal(const QRegion &where,
 bool AutomappingManager::loadFile(const QString &filePath)
 {
     bool ret = true;
-    const QString absPath = QFileInfo(filePath).path();
+    const QDir absPath = QFileInfo(filePath).dir();
     QFile rulesFile(filePath);
 
     if (!rulesFile.exists()) {
@@ -166,10 +167,12 @@ bool AutomappingManager::loadFile(const QString &filePath)
                 || rulePath.startsWith(QLatin1String("//")))
             continue;
 
-        const QFileInfo rulePathInfo(rulePath);
+        QFileInfo rulePathInfo(rulePath);
 
-        if (rulePathInfo.isRelative())
-            rulePath = absPath + QLatin1Char('/') + rulePath;
+        if (rulePathInfo.isRelative()) {
+            rulePath = absPath.filePath(rulePath);
+            rulePathInfo.setFile(rulePath);
+        }
 
         if (!rulePathInfo.exists()) {
             mError += tr("File not found:\n%1").arg(rulePath) + QLatin1Char('\n');