a538984271e93c113cfc84f5b2e9c51b96270325
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2019-07-10 10:45:57 +0200

    Tweaked usage instructions
    * Don't mention TMX format, since plugins can add support for other
      formats (JSON and tBIN are now supported, for example).
    
    * Make it more clear that world files are also accepted.

--- src/tmxrasterizer/main.cpp
+++ src/tmxrasterizer/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
     PluginManager::instance()->loadPlugins();
 
     QCommandLineParser parser;
-    parser.setApplicationDescription(QCoreApplication::translate("main", "Renders a Tiled map (TMX format) or a World map (WORLD format) to an image."));
+    parser.setApplicationDescription(QCoreApplication::translate("main", "Renders a Tiled map or world to an image."));
     parser.addHelpOption();
     parser.addVersionOption();
     parser.addOptions({
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
                             QCoreApplication::translate("main", "Specifies a layer to omit from the output image. Can be repeated to hide multiple layers."),
                             QCoreApplication::translate("main", "name") },
                       });
-    parser.addPositionalArgument("map", QCoreApplication::translate("main", "Map file to render."));
+    parser.addPositionalArgument("map|world", QCoreApplication::translate("main", "Map or world file to render."));
     parser.addPositionalArgument("image", QCoreApplication::translate("main", "Image file to output."));
     parser.process(app);
 
