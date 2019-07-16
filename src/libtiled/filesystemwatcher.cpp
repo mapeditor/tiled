b1ab96e10698c4be7e22ee92fc93ec9e53002727
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2019-07-16 10:26:11 +0200

    Some tweaks
    * Reuse the list that is created to ease iteration.
    * Re-watch the files before emitting filesChanged (probably doesn't
      matter, but I think it's a better behavior regarding outside visibility)
    * Make sure we don't re-watch removed files by checking mWatchCount.

--- src/libtiled/filesystemwatcher.cpp
+++ src/libtiled/filesystemwatcher.cpp
@@ -100,8 +100,6 @@ void FileSystemWatcher::clear()
 
 void FileSystemWatcher::onFileChanged(const QString &path)
 {
-
-
     mChangedFiles.insert(path);
     mChangedFilesTimer.start();
 
@@ -115,19 +113,19 @@ void FileSystemWatcher::onDirectoryChanged(const QString &path)
 
 void FileSystemWatcher::filesChangedTimeout()
 {
-    emit filesChanged(mChangedFiles.toList());
+    const auto changedFiles = mChangedFiles.toList();
 
     // If the file was replaced, the watcher is automatically removed and needs
     // to be re-added to keep watching it for changes. This happens commonly
     // with applications that do atomic saving.
-    QSetIterator<QString> changedFilesIterator(mChangedFiles);
-    while (changedFilesIterator.hasNext()) {
-        QString path = changedFilesIterator.next();
-        if (!mWatcher->files().contains(path))
+    for (const QString &path : changedFiles) {
+        if (mWatchCount.contains(path) && !mWatcher->files().contains(path)) {
             if (QFile::exists(path))
                 mWatcher->addPath(path);
+        }
     }
 
+    emit filesChanged(changedFiles);
     mChangedFiles.clear();
 }
 
