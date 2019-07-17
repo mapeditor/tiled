mapeditor/tiled

commit 651c3d0b39d71780a8f1d9aecb6bb801c44ccdbf

Fixed adjacent maps within a world not being properly clickable
Due to the layer bounding rect being only a rough approximation based on
which chunks are allocated, the clickable region of a map could extend
beyond the map's boundaries, blocking clicks to neighboring maps.

Now the bounding rect of tile layer items is now more precise for
fixed-size maps, like it was before change c9047b4f236fc68a06d.

src/tiled/tilelayeritem.cpp
@@ -44,8 +44,11 @@ void TileLayerItem::syncWithTileLayer()
 {
     prepareGeometryChange();
 
-    MapRenderer *renderer = mMapDocument->renderer();
-    QRectF boundingRect = renderer->boundingRect(tileLayer()->bounds());
+    const MapRenderer *renderer = mMapDocument->renderer();
+    const bool infinite = mMapDocument->map()->infinite();
+
+    QRectF boundingRect = renderer->boundingRect(infinite ? tileLayer()->bounds()
+                                                          : tileLayer()->rect());
 
     QMargins margins = tileLayer()->drawMargins();
     if (const Map *map = tileLayer()->map()) {

commit 4f2ead300da19fe90f09fec2de456f44f022da1e

Restored optimization when rendering fixed-size tile layers
Using TileLayer::bounds() to determine the bounding rect of a tile layer
item avoids looping over large empty areas of a layer while trying to
draw it. Hence, for fixed-size maps the intersection with
TileLayer::rect() should be used rather than only using the fixed size.

This optimization was broken in change 651c3d0b39d7.

src/tiled/tilelayeritem.cpp
@@ -44,11 +44,12 @@ void TileLayerItem::syncWithTileLayer()
 {
     prepareGeometryChange();
 
-    const MapRenderer *renderer = mMapDocument->renderer();
-    const bool infinite = mMapDocument->map()->infinite();
+    QRect layerBounds = tileLayer()->bounds();
+    if (!mMapDocument->map()->infinite())
+        layerBounds &= tileLayer()->rect();
 
-    QRectF boundingRect = renderer->boundingRect(infinite ? tileLayer()->bounds()
-                                                          : tileLayer()->rect());
+    const MapRenderer *renderer = mMapDocument->renderer();
+    QRectF boundingRect = renderer->boundingRect(layerBounds);
 
     QMargins margins = tileLayer()->drawMargins();
     if (const Map *map = tileLayer()->map()) {