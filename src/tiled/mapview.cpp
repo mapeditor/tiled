mapeditor/tiled

commit 3b27c6a39196ea253131bc52c66ac4fd052f272f

Fit map in view when opening for the first time
Normally the view would initialize the show the center of the map at
100%. And in case of a map that is part of a world it was broken,
showing the center of the world instead.

src/tiled/mapview.cpp
@@ -46,7 +46,6 @@ using namespace Tiled;
 
 MapView::MapView(QWidget *parent, Mode mode)
     : QGraphicsView(parent)
-    , mHandScrolling(false)
     , mMode(mode)
     , mZoomable(new Zoomable(this))
 {
@@ -302,6 +301,16 @@ bool MapView::event(QEvent *e)
     }
 
     return QGraphicsView::event(e);
+}
+
+void MapView::showEvent(QShowEvent *event)
+{
+    if (!mViewInitialized) {
+        fitMapInView();
+        mViewInitialized = true;
+    }
+
+    QGraphicsView::showEvent(event);
 }
 
 void MapView::hideEvent(QHideEvent *event)

src/tiled/documentmanager.cpp
@@ -532,9 +532,6 @@ void DocumentManager::insertDocument(int index, const DocumentPtr &document)
     if (mBrokenLinksModel->hasBrokenLinks())
         mBrokenLinksWidget->show();
 
-    // todo: fix this (move to MapEditor)
-    //    centerViewOn(0, 0);
-
     emit documentOpened(documentPtr);
 }
 

src/tiled/mapeditor.cpp
@@ -372,6 +372,9 @@ void MapEditor::addDocument(Document *document)
         int layerIndex = mapState.value(QLatin1String("selectedLayer")).toInt();
         if (Layer *layer = layerAtGlobalIndex(mapDocument->map(), layerIndex))
             mapDocument->switchCurrentLayer(layer);
+
+        // suppress fitting map in view upon show event
+        view->setViewInitialized();
     }
 }
 

src/tiled/mapview.h
@@ -61,6 +61,8 @@ public:
     MapView(QWidget *parent = nullptr, Mode mode = StaticContents);
     ~MapView() override;
 
+    void setViewInitialized();
+
     void setScene(MapScene *scene);
     MapScene *mapScene() const;
 
@@ -82,6 +84,7 @@ public:
 protected:
     bool event(QEvent *event) override;
 
+    void showEvent(QShowEvent *) override;
     void hideEvent(QHideEvent *) override;
     void resizeEvent(QResizeEvent *event) override;
 
@@ -114,10 +117,17 @@ private:
     MapDocument *mMapDocument = nullptr;
     QPoint mLastMousePos;
     QPointF mLastMouseScenePos;
-    bool mHandScrolling;
+    bool mHandScrolling = false;
+    bool mViewInitialized = false;
     Mode mMode;
     Zoomable *mZoomable;
 };
+
+
+inline void MapView::setViewInitialized()
+{
+    mViewInitialized = true;
+}
 
 } // namespace Tiled
 

commit 9dc290e6fcb520fb10b7a936d0c8bdad46a51420

Fixed edit template and tile collision editor views
This broke in commit 3b27c6a39196ea253131bc52c66ac4fd052f272f.

The addition of making the map fit into view by default caused a near
infinite scale to be set on these views because their map has 0 size,
making them appear entirely white and perform badly.

src/tiled/mapview.cpp
@@ -122,8 +122,11 @@ void MapView::setScale(qreal scale)
 
 void MapView::fitMapInView()
 {
+    const QRectF rect = mapScene()->mapBoundingRect();
+    if (rect.isEmpty())
+        return;
+
     // Scale and center map to fit in view
-    QRectF rect = mapScene()->mapBoundingRect();
     centerOn(rect.center());
     setScale(std::min(width() / rect.width(), height() / rect.height()) * 0.95);
 }