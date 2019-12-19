47ced52d334b30a6aa3f57f33014dbf733a2e8cb
Phlosioneer <mattmdrr2@gmail.com>
2019-12-18 20:17:58 -0500

    Revert "Speed up first-draw by reducing initial map size"
    This reverts commit 162c324b18d35adf4052d9842650f4547db9f2a8.

--- src/tiled/mapview.cpp
+++ src/tiled/mapview.cpp
@@ -126,15 +126,9 @@ void MapView::fitMapInView()
     if (rect.isEmpty())
         return;
 
-    // Scale and center map to fit in view. For extremely large maps (by pixels), avoid going below
-    // 1% scale. For extremely large maps (by tiles), avoid putting more than 4,000 tiles within the
-    // view. Approximated to 4096 tiles, 64 * 64.
+    // Scale and center map to fit in view
     centerOn(rect.center());
-    qreal pixelScale = std::min(width() / rect.width(), height() / rect.height()) * 0.95;
-    auto tileSize = mapScene()->mapDocument()->map()->tileSize();
-    qreal minimumScale = std::min(width() / (64.0 * tileSize.width()), height() / (64.0 * tileSize.height()));
-    qreal scale = std::max(pixelScale, minimumScale);
-    setScale(std::max(scale, 0.01));
+    setScale(std::min(width() / rect.width(), height() / rect.height()) * 0.95);
 }
 
 void MapView::adjustScale(qreal scale)
