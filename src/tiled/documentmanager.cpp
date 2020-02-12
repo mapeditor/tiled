mapeditor/tiled

commit f511b75ea7a799372f1bc9e45b9f2e4cb924126c

Scripting: Enabled scripted formats to be used on the command-line
Extensions now get loaded before handling the export-related command-line
arguments.

A number of small refactorings were done:

* Moved deletion of singletons from MainWindow to TiledApplication, to
  make sure they get deleted also after processing command-line arguments
  that cause the MainWindow never to be constructed.

* ClipboardManager, CommandManager and LanguageManager singletons were
  simplified since they did not require a manual "deleteInstance" call.

* Simplified the ActionManager singleton, removing ActionManagerPrivate
  and avoiding the need for explicit construction elsewhere.

src/tiled/documentmanager.cpp
@@ -311,13 +311,11 @@ Editor *DocumentManager::editor(Document::DocumentType documentType) const
     return mEditorForType.value(documentType);
 }
 
-void DocumentManager::deleteEditor(Document::DocumentType documentType)
+void DocumentManager::deleteEditors()
 {
-    Q_ASSERT(mEditorForType.contains(documentType));
-    Editor *editor = mEditorForType.take(documentType);
-    if (editor == mMapEditor)
-        mMapEditor = nullptr;
-    delete editor;
+    qDeleteAll(mEditorForType);
+    mEditorForType.clear();
+    mMapEditor = nullptr;
 }
 
 QList<Editor *> DocumentManager::editors() const

src/tiled/documentmanager.h
@@ -71,7 +71,7 @@ public:
 
     void setEditor(Document::DocumentType documentType, Editor *editor);
     Editor *editor(Document::DocumentType documentType) const;
-    void deleteEditor(Document::DocumentType documentType);
+    void deleteEditors();
     QList<Editor*> editors() const;
 
     Editor *currentEditor() const;

src/tiled/actionmanager.cpp
@@ -22,104 +22,34 @@
 
 #include "preferences.h"
 
-#include <QHash>
 #include <QMenu>
 #include <QSettings>
 
 namespace Tiled {
 
-class ActionManagerPrivate
-{
-public:
-    QMultiHash<Id, QAction*> mIdToActions;
-    QHash<Id, QMenu*> mIdToMenu;
-
-    QHash<Id, QKeySequence> mDefaultShortcuts;      // for resetting to default
-    QHash<Id, QKeySequence> mCustomShortcuts;
-    QHash<Id, QKeySequence> mLastKnownShortcuts;    // for detecting shortcut changes
-
-    bool mApplyingShortcut = false;
-    bool mApplyingToolTipWithShortcut = false;
-    bool mResettingShortcut = false;
-};
-
-static ActionManager *m_instance;
-static ActionManagerPrivate *d;
-
-static void readCustomShortcuts()
-{
-    const auto settings = Preferences::instance()->settings();
-    settings->beginGroup(QLatin1String("CustomShortcuts"));
-
-    const auto keys = settings->childKeys();
-    for (const auto &key : keys) {
-        auto keySequence = QKeySequence::fromString(settings->value(key).toString());
-        d->mCustomShortcuts.insert(Id(key.toUtf8()), keySequence);
-    }
-
-    settings->endGroup();
-}
-
-static void applyShortcut(QAction *action, const QKeySequence &shortcut)
-{
-    d->mApplyingShortcut = true;
-    action->setShortcut(shortcut);
-    d->mApplyingShortcut = false;
-}
-
-static void updateToolTipWithShortcut(QAction *action)
-{
-    d->mApplyingToolTipWithShortcut = true;
-
-    QString toolTip = action->toolTip();
-
-    // If shortcut present, unset shortut and retrieve stripped text
-    if (toolTip.contains(QLatin1String(" <span "))) {
-        action->setToolTip(QString());
-        toolTip = action->toolTip();
-    }
-
-    if (!action->shortcut().isEmpty()) {
-        toolTip.append(QString::fromLatin1(" <span style=\"color: gray;\">(%1)<span>")
-                       .arg(action->shortcut().toString(QKeySequence::NativeText)));
-    }
-
-    action->setToolTip(toolTip);
-
-    d->mApplyingToolTipWithShortcut = false;
-}
-
-
 ActionManager::ActionManager(QObject *parent)
     : QObject(parent)
 {
-    Q_ASSERT(!m_instance);
-    m_instance = this;
-    d = new ActionManagerPrivate;
-
     readCustomShortcuts();
 }
 
-ActionManager::~ActionManager()
-{
-    m_instance = nullptr;
-    delete d;
-    d = nullptr;
-}
+ActionManager::~ActionManager() = default;
 
 ActionManager *ActionManager::instance()
 {
-    Q_ASSERT(m_instance);
-    return m_instance;
+    static ActionManager instance;
+    return &instance;
 }
 
 void ActionManager::registerAction(QAction *action, Id id)
 {
+    auto d = instance();
+
     Q_ASSERT_X(!d->mIdToActions.contains(id, action), "ActionManager::registerAction", "duplicate action");
     d->mIdToActions.insert(id, action);
     d->mLastKnownShortcuts.insert(id, action->shortcut());
 
-    connect(action, &QAction::changed, m_instance, [id,action] {
+    connect(action, &QAction::changed, d, [=] {
         if (d->mApplyingToolTipWithShortcut)
             return;
 
@@ -129,52 +59,60 @@ void ActionManager::registerAction(QAction *action, Id id)
 
             // Reset back to user-defined shortcut if set
             if (d->mCustomShortcuts.contains(id)) {
-                applyShortcut(action, d->mCustomShortcuts.value(id));
+                d->applyShortcut(action, d->mCustomShortcuts.value(id));
                 return;
             }
         }
 
         d->mLastKnownShortcuts.insert(id, action->shortcut());
 
-        updateToolTipWithShortcut(action);
+        d->updateToolTipWithShortcut(action);
 
-        emit m_instance->actionChanged(id);
+        emit d->actionChanged(id);
     });
 
-    if (m_instance->hasCustomShortcut(id)) {
+    if (d->hasCustomShortcut(id)) {
         d->mDefaultShortcuts.insert(id, action->shortcut());
-        applyShortcut(action, d->mCustomShortcuts.value(id));
+        d->applyShortcut(action, d->mCustomShortcuts.value(id));
     }
 
-    updateToolTipWithShortcut(action);
+    d->updateToolTipWithShortcut(action);
 
-    emit m_instance->actionsChanged();
+    emit d->actionsChanged();
 }
 
 void ActionManager::unregisterAction(QAction *action, Id id)
 {
+    auto d = instance();
+
     Q_ASSERT_X(d->mIdToActions.contains(id, action), "ActionManager::unregisterAction", "unknown action");
     d->mIdToActions.remove(id, action);
-    action->disconnect(m_instance);
+    action->disconnect(d);
     d->mDefaultShortcuts.remove(id);
     d->mLastKnownShortcuts.remove(id);
-    emit m_instance->actionsChanged();
+    emit d->actionsChanged();
 }
 
 void ActionManager::registerMenu(QMenu *menu, Id id)
 {
+    auto d = instance();
+
     Q_ASSERT_X(!d->mIdToMenu.contains(id), "ActionManager::registerMenu", "duplicate id");
     d->mIdToMenu.insert(id, menu);
 }
 
 void ActionManager::unregisterMenu(Id id)
 {
+    auto d = instance();
+
     Q_ASSERT_X(d->mIdToMenu.contains(id), "ActionManager::unregisterMenu", "unknown id");
     d->mIdToMenu.remove(id);
 }
 
 QAction *ActionManager::action(Id id)
 {
+    auto d = instance();
+
     auto action = d->mIdToActions.value(id);
     Q_ASSERT_X(action, "ActionManager::action", "unknown id");
     return action;
@@ -182,11 +120,15 @@ QAction *ActionManager::action(Id id)
 
 QAction *ActionManager::findAction(Id id)
 {
+    auto d = instance();
+
     return d->mIdToActions.value(id);
 }
 
 QMenu *ActionManager::menu(Id id)
 {
+    auto d = instance();
+
     auto menu = d->mIdToMenu.value(id);
     Q_ASSERT_X(menu, "ActionManager::menu", "unknown id");
     return menu;
@@ -194,30 +136,30 @@ QMenu *ActionManager::menu(Id id)
 
 QMenu *ActionManager::findMenu(Id id)
 {
-    return d->mIdToMenu.value(id);
+    return instance()->mIdToMenu.value(id);
 }
 
 QList<Id> ActionManager::actions()
 {
-    return d->mIdToActions.uniqueKeys();
+    return instance()->mIdToActions.uniqueKeys();
 }
 
 QList<Id> ActionManager::menus()
 {
-    return d->mIdToMenu.keys();
+    return instance()->mIdToMenu.keys();
 }
 
 void ActionManager::setCustomShortcut(Id id, const QKeySequence &keySequence)
 {
-    Q_ASSERT(!d->mResettingShortcut);
+    Q_ASSERT(!mResettingShortcut);
 
-    const auto actions = d->mIdToActions.values(id);
+    const auto actions = mIdToActions.values(id);
     Q_ASSERT_X(!actions.isEmpty(), "ActionManager::setCustomShortcut", "unknown id");
 
     if (!hasCustomShortcut(id))
-        d->mDefaultShortcuts.insert(id, actions.first()->shortcut());
+        mDefaultShortcuts.insert(id, actions.first()->shortcut());
 
-    d->mCustomShortcuts.insert(id, keySequence);
+    mCustomShortcuts.insert(id, keySequence);
 
     for (QAction *a : actions)
         applyShortcut(a, keySequence);
@@ -229,7 +171,7 @@ void ActionManager::setCustomShortcut(Id id, const QKeySequence &keySequence)
 
 bool ActionManager::hasCustomShortcut(Id id) const
 {
-    return d->mCustomShortcuts.contains(id);
+    return mCustomShortcuts.contains(id);
 }
 
 void ActionManager::resetCustomShortcut(Id id)
@@ -237,17 +179,17 @@ void ActionManager::resetCustomShortcut(Id id)
     if (!hasCustomShortcut(id))
         return;
 
-    const auto actions = d->mIdToActions.values(id);
+    const auto actions = mIdToActions.values(id);
     Q_ASSERT_X(!actions.isEmpty(), "ActionManager::resetCustomShortcut", "unknown id");
 
-    d->mResettingShortcut = true;
+    mResettingShortcut = true;
 
-    const QKeySequence defaultShortcut = d->mDefaultShortcuts.take(id);
+    const QKeySequence defaultShortcut = mDefaultShortcuts.take(id);
     for (QAction *a : actions)
         applyShortcut(a, defaultShortcut);
-    d->mCustomShortcuts.remove(id);
+    mCustomShortcuts.remove(id);
 
-    d->mResettingShortcut = false;
+    mResettingShortcut = false;
 
     auto settings = Preferences::instance()->settings();
     settings->remove(QLatin1String("CustomShortcuts/") + id.toString());
@@ -255,15 +197,15 @@ void ActionManager::resetCustomShortcut(Id id)
 
 void ActionManager::resetAllCustomShortcuts()
 {
-    QHashIterator<Id, QKeySequence> iterator(d->mDefaultShortcuts);
+    QHashIterator<Id, QKeySequence> iterator(mDefaultShortcuts);
     while (iterator.hasNext()) {
         iterator.next();
-        const auto actions = d->mIdToActions.values(iterator.key());
+        const auto actions = mIdToActions.values(iterator.key());
         for (QAction *a : actions)
             applyShortcut(a, iterator.value());
     }
-    d->mDefaultShortcuts.clear();
-    d->mCustomShortcuts.clear();
+    mDefaultShortcuts.clear();
+    mCustomShortcuts.clear();
 
     auto settings = Preferences::instance()->settings();
     settings->remove(QLatin1String("CustomShortcuts"));
@@ -271,8 +213,8 @@ void ActionManager::resetAllCustomShortcuts()
 
 QKeySequence ActionManager::defaultShortcut(Id id)
 {
-    if (d->mDefaultShortcuts.contains(id))
-        return d->mDefaultShortcuts.value(id);
+    if (mDefaultShortcuts.contains(id))
+        return mDefaultShortcuts.value(id);
     if (auto a = findAction(id))
         return a->shortcut();
     return QKeySequence();
@@ -293,8 +235,8 @@ void ActionManager::setCustomShortcuts(const QHash<Id, QKeySequence> &shortcuts)
         const QKeySequence &shortcut = iterator.value();
 
         if (auto a = findAction(id)) {
-            if (d->mDefaultShortcuts.contains(id)
-                    ? d->mDefaultShortcuts.value(id) == shortcut
+            if (mDefaultShortcuts.contains(id)
+                    ? mDefaultShortcuts.value(id) == shortcut
                     : a->shortcut() == shortcut) {
                 resetCustomShortcut(id);
             } else {
@@ -302,6 +244,49 @@ void ActionManager::setCustomShortcuts(const QHash<Id, QKeySequence> &shortcuts)
             }
         }
     }
+}
+
+void ActionManager::readCustomShortcuts()
+{
+    const auto settings = Preferences::instance()->settings();
+    settings->beginGroup(QLatin1String("CustomShortcuts"));
+
+    const auto keys = settings->childKeys();
+    for (const auto &key : keys) {
+        auto keySequence = QKeySequence::fromString(settings->value(key).toString());
+        mCustomShortcuts.insert(Id(key.toUtf8()), keySequence);
+    }
+
+    settings->endGroup();
+}
+
+void ActionManager::applyShortcut(QAction *action, const QKeySequence &shortcut)
+{
+    mApplyingShortcut = true;
+    action->setShortcut(shortcut);
+    mApplyingShortcut = false;
+}
+
+void ActionManager::updateToolTipWithShortcut(QAction *action)
+{
+    mApplyingToolTipWithShortcut = true;
+
+    QString toolTip = action->toolTip();
+
+    // If shortcut present, unset shortut and retrieve stripped text
+    if (toolTip.contains(QLatin1String(" <span "))) {
+        action->setToolTip(QString());
+        toolTip = action->toolTip();
+    }
+
+    if (!action->shortcut().isEmpty()) {
+        toolTip.append(QString::fromLatin1(" <span style=\"color: gray;\">(%1)<span>")
+                       .arg(action->shortcut().toString(QKeySequence::NativeText)));
+    }
+
+    action->setToolTip(toolTip);
+
+    mApplyingToolTipWithShortcut = false;
 }
 
 } // namespace Tiled

src/tiled/actionmanager.h
@@ -22,6 +22,7 @@
 
 #include "id.h"
 
+#include <QHash>
 #include <QObject>
 
 class QAction;
@@ -37,6 +38,9 @@ class MainWindow;
 class ActionManager : public QObject
 {
     Q_OBJECT
+
+    explicit ActionManager(QObject *parent = nullptr);
+    ~ActionManager();
 
 public:
     static ActionManager *instance();
@@ -69,10 +73,20 @@ signals:
     void actionsChanged();
 
 private:
-    explicit ActionManager(QObject *parent = nullptr);
-    ~ActionManager();
+    void readCustomShortcuts();
+    void applyShortcut(QAction *action, const QKeySequence &shortcut);
+    void updateToolTipWithShortcut(QAction *action);
 
-    friend class Tiled::MainWindow;   // creation
+    QMultiHash<Id, QAction*> mIdToActions;
+    QHash<Id, QMenu*> mIdToMenu;
+
+    QHash<Id, QKeySequence> mDefaultShortcuts;      // for resetting to default
+    QHash<Id, QKeySequence> mCustomShortcuts;
+    QHash<Id, QKeySequence> mLastKnownShortcuts;    // for detecting shortcut changes
+
+    bool mApplyingShortcut = false;
+    bool mApplyingToolTipWithShortcut = false;
+    bool mResettingShortcut = false;
 };
 
 } // namespace Tiled

src/tiled/clipboardmanager.cpp
@@ -45,8 +45,6 @@ static const char * const TMX_MIMETYPE = "text/tmx";
 
 using namespace Tiled;
 
-ClipboardManager *ClipboardManager::mInstance;
-
 ClipboardManager::ClipboardManager()
     : mClipboard(QApplication::clipboard())
     , mHasMap(false)
@@ -62,18 +60,8 @@ ClipboardManager::ClipboardManager()
  */
 ClipboardManager *ClipboardManager::instance()
 {
-    if (!mInstance)
-        mInstance = new ClipboardManager;
-    return mInstance;
-}
-
-/**
- * Deletes the clipboard manager instance if it exists.
- */
-void ClipboardManager::deleteInstance()
-{
-    delete mInstance;
-    mInstance = nullptr;
+    static ClipboardManager instance;
+    return &instance;
 }
 
 /**

src/tiled/clipboardmanager.h
@@ -48,7 +48,6 @@ class ClipboardManager : public QObject
 
 public:
     static ClipboardManager *instance();
-    static void deleteInstance();
 
     bool hasMap() const;
     std::unique_ptr<Map> map() const;
@@ -83,8 +82,6 @@ private:
     QClipboard *mClipboard;
     bool mHasMap;
     bool mHasProperties;
-
-    static ClipboardManager *mInstance;
 };
 
 /**

src/tiled/commandmanager.cpp
@@ -31,11 +31,7 @@
 #include <QLatin1String>
 #include <QMenu>
 
-#include "qtcompat_p.h"
-
 namespace Tiled {
-
-CommandManager *CommandManager::mInstance;
 
 CommandManager::CommandManager()
     : mModel(new CommandDataModel(this))
@@ -49,16 +45,8 @@ CommandManager::~CommandManager()
 
 CommandManager *CommandManager::instance()
 {
-    if (!mInstance)
-        mInstance = new CommandManager;
-
-    return mInstance;
-}
-
-void CommandManager::deleteInstance()
-{
-    delete mInstance;
-    mInstance = nullptr;
+    static CommandManager instance;
+    return &instance;
 }
 
 CommandDataModel *CommandManager::commandDataModel()
@@ -79,9 +67,9 @@ void CommandManager::showDialog()
     dialog.exec();
 }
 
-void CommandManager::populateMenus()
+void CommandManager::populateMenus() const
 {
-    for (QMenu *menu : qAsConst(mMenus)) {
+    for (QMenu *menu : mMenus) {
         menu->clear();
         menu->addActions(mActions);
     }

src/tiled/commandmanager.h
@@ -36,8 +36,6 @@ class CommandManager : public QObject
 public:
     static CommandManager *instance();
 
-    static void deleteInstance();
-
     /**
      * Returns the CommandDataModel instance stored
      */
@@ -46,14 +44,13 @@ public:
     /**
      * Registers a new QMenu with the CommandManager
      */
-    void registerMenu(QMenu* menu);
+    void registerMenu(QMenu *menu);
 
     void updateActions();
 
     void retranslateUi();
 
 public slots:
-
     /**
      * Displays the dialog to edit the commands
      */
@@ -68,9 +65,8 @@ private:
     /**
      * Populates all the menus registered in CommandManager
      */
-    void populateMenus();
+    void populateMenus() const;
 
-    static CommandManager *mInstance;
     CommandDataModel *mModel;
     QList<QMenu*> mMenus;
     QList<QAction*> mActions;

commit 1a155dcb5cc4c9ad117e787fcce6399a31b13b14

Fixed crash on shutdown
This was a regression introduced by f511b75ea7a799372f1bc9e45b9f2e4cb92.
The DocumentManager destructor was referencing widget pointers that got
deleted already now that it was being called later.

I've introduced a custom TabBar class (which also replaces the
WheelEnabledTabBar) to avoid the need to register an event filter for
making tabs closeable using middle-click.

The remaining mWidget pointer is now wrapped in a QPointer, which will
automatically become null when the widget is already deleted as part of
the UI hierarchy.

src/tiled/documentmanager.cpp
@@ -38,6 +38,7 @@
 #include "mapview.h"
 #include "noeditorwidget.h"
 #include "preferences.h"
+#include "tabbar.h"
 #include "terrain.h"
 #include "tilesetdocument.h"
 #include "tilesetdocumentsmodel.h"
@@ -88,7 +89,7 @@ DocumentManager::DocumentManager(QObject *parent)
     , mTilesetDocumentsModel(new TilesetDocumentsModel(this))
     , mWidget(new QWidget)
     , mNoEditorWidget(new NoEditorWidget(mWidget))
-    , mTabBar(new QTabBar(mWidget))
+    , mTabBar(new TabBar(mWidget))
     , mFileChangedWarning(new FileChangedWarning(mWidget))
     , mBrokenLinksModel(new BrokenLinksModel(this))
     , mBrokenLinksWidget(new BrokenLinksWidget(mBrokenLinksModel, mWidget))
@@ -273,14 +274,10 @@ DocumentManager::DocumentManager(QObject *parent)
             }
         }
     };
-
-    mTabBar->installEventFilter(this);
 }
 
 DocumentManager::~DocumentManager()
 {
-    mTabBar->removeEventFilter(this);
-
     // All documents should be closed gracefully beforehand
     Q_ASSERT(mDocuments.isEmpty());
     Q_ASSERT(mTilesetDocumentsModel->rowCount() == 0);
@@ -1272,25 +1269,6 @@ bool DocumentManager::askForAdjustment(const Tileset &tileset)
                                   QMessageBox::Yes);
 
     return r == QMessageBox::Yes;
-}
-
-bool DocumentManager::eventFilter(QObject *object, QEvent *event)
-{
-    if (object == mTabBar && event->type() == QEvent::MouseButtonRelease) {
-        // middle-click tab closing
-        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-
-        if (mouseEvent->button() == Qt::MidButton) {
-            int index = mTabBar->tabAt(mouseEvent->pos());
-
-            if (index != -1) {
-                documentCloseRequested(index);
-                return true;
-            }
-        }
-    }
-
-    return false;
 }
 
 /**

src/tiled/documentmanager.h
@@ -28,6 +28,7 @@
 #include <QList>
 #include <QObject>
 #include <QPointF>
+#include <QPointer>
 #include <QVector>
 
 class QTabWidget;
@@ -130,8 +131,6 @@ public:
 
     void abortMultiDocumentClose();
 
-    bool eventFilter(QObject *object, QEvent *event) override;
-
 signals:
     void documentCreated(Document *document);
     void documentOpened(Document *document);
@@ -210,7 +209,8 @@ private:
     QVector<DocumentPtr> mDocuments;
     TilesetDocumentsModel *mTilesetDocumentsModel;
 
-    QWidget *mWidget;
+    // Pointer becomes null when deleted as part of the UI, to prevent double-deletion
+    QPointer<QWidget> mWidget;
     QWidget *mNoEditorWidget;
     QTabBar *mTabBar;
     FileChangedWarning *mFileChangedWarning;

src/tiled/tabbar.cpp
@@ -1,0 +1,66 @@
+/*
+ * tabbar.cpp
+ * Copyright 2016-2020, Thorbjørn Lindeijer <bjorn@lindeijer.nl>
+ *
+ * This file is part of Tiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tabbar.h"
+
+#include <QMouseEvent>
+#include <QWheelEvent>
+
+namespace Tiled {
+
+TabBar::TabBar(QWidget *parent)
+    : QTabBar(parent)
+{}
+
+void TabBar::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::MidButton)
+        mPressedIndex = tabAt(event->pos());
+
+    QTabBar::mousePressEvent(event);
+}
+
+void TabBar::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::MidButton && tabsClosable()) {
+        if (mPressedIndex != -1 && mPressedIndex == tabAt(event->pos())) {
+            emit tabCloseRequested(mPressedIndex);
+            return;
+        }
+    }
+
+    QTabBar::mouseReleaseEvent(event);
+}
+
+void TabBar::wheelEvent(QWheelEvent *event)
+{
+    // Qt excludes OS X when implementing mouse wheel for switching tabs.
+    // However, we explicitly want this feature on the tilesets and open
+    // documents tab bars as a possible means of navigation.
+
+    int index = currentIndex();
+    if (index != -1) {
+        index += event->delta() > 0 ? -1 : 1;
+        if (index >= 0 && index < count())
+            setCurrentIndex(index);
+    }
+}
+
+} // namespace Tiled

src/tiled/tabbar.h
@@ -1,0 +1,47 @@
+/*
+ * tabbar.h
+ * Copyright 2016-2020, Thorbjørn Lindeijer <bjorn@lindeijer.nl>
+ *
+ * This file is part of Tiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QTabBar>
+
+namespace Tiled {
+
+/**
+ * TabBar is different from QTabBar in that when tabs are closable, they can
+ * also be closed using middle-click and the mouse wheel can always be used to
+ * change the current index.
+ */
+class TabBar : public QTabBar
+{
+public:
+    explicit TabBar(QWidget *parent = nullptr);
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void wheelEvent(QWheelEvent *event) override;
+
+private:
+    int mPressedIndex = -1;
+};
+
+} // namespace Tiled

src/tiled/tiled.pro
@@ -221,6 +221,7 @@ SOURCES += aboutdialog.cpp \
     stampbrush.cpp \
     stylehelper.cpp \
     swaptiles.cpp \
+    tabbar.cpp \
     templatesdock.cpp \
     terrainbrush.cpp \
     terraindock.cpp \
@@ -455,6 +456,7 @@ HEADERS += aboutdialog.h \
     stampbrush.h \
     stylehelper.h \
     swaptiles.h \
+    tabbar.h \
     templatesdock.h \
     terrainbrush.h \
     terraindock.h \

src/tiled/tiled.qbs
@@ -445,6 +445,8 @@ QtGuiApplication {
         "stylehelper.h",
         "swaptiles.cpp",
         "swaptiles.h",
+        "tabbar.cpp",
+        "tabbar.h",
         "templatesdock.cpp",
         "templatesdock.h",
         "terrainbrush.cpp",

src/tiled/tilesetdock.cpp
@@ -38,15 +38,16 @@
 #include "replacetileset.h"
 #include "scriptmanager.h"
 #include "swaptiles.h"
+#include "tabbar.h"
 #include "terrain.h"
 #include "tile.h"
 #include "tilelayer.h"
 #include "tilesetdocument.h"
 #include "tilesetdocumentsmodel.h"
 #include "tilesetformat.h"
+#include "tilesetmanager.h"
 #include "tilesetmodel.h"
 #include "tilesetview.h"
-#include "tilesetmanager.h"
 #include "tilestamp.h"
 #include "tmxmapformat.h"
 #include "utils.h"
@@ -125,30 +126,6 @@ protected:
 };
 
 
-/**
- * Qt excludes OS X when implementing mouse wheel for switching tabs. However,
- * we explicitly want this feature on the tileset tab bar as a possible means
- * of navigation.
- */
-class WheelEnabledTabBar : public QTabBar
-{
-public:
-    explicit WheelEnabledTabBar(QWidget *parent = nullptr)
-       : QTabBar(parent)
-    {}
-
-    void wheelEvent(QWheelEvent *event) override
-    {
-        int index = currentIndex();
-        if (index != -1) {
-            index += event->delta() > 0 ? -1 : 1;
-            if (index >= 0 && index < count())
-                setCurrentIndex(index);
-        }
-    }
-};
-
-
 static void removeTileReferences(MapDocument *mapDocument,
                                  std::function<bool(const Cell &)> condition)
 {
@@ -190,7 +167,7 @@ TilesetDock::TilesetDock(QWidget *parent)
     : QDockWidget(parent)
     , mMapDocument(nullptr)
     , mTilesetDocumentsFilterModel(new TilesetDocumentsFilterModel(this))
-    , mTabBar(new WheelEnabledTabBar)
+    , mTabBar(new TabBar)
     , mSuperViewStack(new QStackedWidget)
     , mViewStack(new QStackedWidget)
     , mToolBar(new QToolBar)