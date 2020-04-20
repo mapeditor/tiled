aaf437d3cdee03659d6c35a314ee2f4f971e1c73
David Konsumer <konsumer@jetboystudio.com>
2020-04-20 16:25:57 -0700

    new ideas with QFileInfo, still needs FileTime and Permissions

--- src/tiled/scriptfileinfo.cpp
+++ src/tiled/scriptfileinfo.cpp
@@ -19,39 +19,286 @@
  */
 
 #include "scriptfileinfo.h"
-#include <QtCore/qcoreapplication.h>
-#include <QtCore/qdatetime.h>
-#include <QtCore/qdir.h>
-#include <QtCore/qfileinfo.h>
-#include <QtCore/qregexp.h>
 
-#if defined(Q_OS_UNIX)
-#include <cerrno>
-#include <sys/stat.h>
-#include <unistd.h>
-#elif defined(Q_OS_WIN)
-#include <QtCore/qt_windows.h>
-#endif
 
 ScriptFileInfo::ScriptFileInfo(QObject *parent)
 		: QObject(parent)
-{
+{}
 
+QString ScriptFileInfo::absoluteDir(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	QDir dir = fp.dir();
+	return dir.absolutePath();
 }
 
-QString ScriptFileInfo::fileName(const QString &fp)
+QString ScriptFileInfo::absoluteFilePath(QString file)
 {
-	int last = fp.lastIndexOf(QLatin1Char('/'));
-	if (last < 0)
-		return fp;
-	return fp.mid(last + 1);
+	QFileInfo fp = QFileInfo(file);
+	return fp.absoluteFilePath();
 }
 
-QString ScriptFileInfo::baseName(const QString &fp)
+QString ScriptFileInfo::absolutePath(QString file)
 {
-	QString fn = fileName(fp);
-	int dot = fn.indexOf(QLatin1Char('.'));
-	if (dot < 0)
-		return fn;
-	return fn.mid(0, dot);
+	QFileInfo fp = QFileInfo(file);
+	return fp.absolutePath();
+}
+
+QString ScriptFileInfo::baseName(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.baseName();
+}
+
+QDateTime ScriptFileInfo::birthTime(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.birthTime();
+}
+
+QString ScriptFileInfo::bundleName(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.bundleName();
+}
+
+bool ScriptFileInfo::caching(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.caching();
+}
+
+QString ScriptFileInfo::canonicalFilePath(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.canonicalFilePath();
+}
+
+QString ScriptFileInfo::canonicalPath(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.canonicalPath();
+}
+
+QString ScriptFileInfo::completeBaseName(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.completeBaseName();
+}
+
+QString ScriptFileInfo::completeSuffix(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.completeSuffix();
+}
+
+QString ScriptFileInfo::dir(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	QDir dir = fp.dir();
+	return dir.path();
+}
+
+bool  ScriptFileInfo::exists(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.exists();
+}
+
+QString ScriptFileInfo::fileName(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.fileName();
+}
+
+QString ScriptFileInfo::filePath(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.filePath();
+}
+
+// QDateTime ScriptFileInfo::fileTime(QString file, QFile::FileTime time)
+// {
+// 	QFileInfo fp = QFileInfo(file);
+// 	return fp.fileTime(time);
+// }
+
+QString ScriptFileInfo::group(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.group();
+}
+
+uint  ScriptFileInfo::groupId(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.groupId();
+}
+
+bool  ScriptFileInfo::isAbsolute(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isAbsolute();
+}
+
+bool  ScriptFileInfo::isBundle(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isBundle();
+}
+
+bool  ScriptFileInfo::isDir(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isDir();
+}
+
+bool  ScriptFileInfo::isExecutable(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isExecutable();
+}
+
+bool  ScriptFileInfo::isFile(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isFile();
+}
+
+bool  ScriptFileInfo::isHidden(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isHidden();
+}
+
+bool  ScriptFileInfo::isNativePath(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isNativePath();
+}
+
+bool  ScriptFileInfo::isReadable(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isReadable();
+}
+
+bool  ScriptFileInfo::isRelative(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isRelative();
+}
+
+bool  ScriptFileInfo::isRoot(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isRoot();
+}
+
+bool  ScriptFileInfo::isShortcut(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isShortcut();
+}
+
+bool  ScriptFileInfo::isSymLink(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isSymLink();
+}
+
+bool  ScriptFileInfo::isSymbolicLink(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isSymbolicLink();
+}
+
+bool  ScriptFileInfo::isWritable(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.isWritable();
+}
+
+QDateTime ScriptFileInfo::lastModified(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.lastModified();
+}
+
+QDateTime ScriptFileInfo::lastRead(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.lastRead();
+}
+
+bool  ScriptFileInfo::makeAbsolute(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.makeAbsolute();
+}
+
+QDateTime ScriptFileInfo::metadataChangeTime(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.metadataChangeTime();
+}
+
+QString ScriptFileInfo::owner(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.owner();
+}
+
+uint  ScriptFileInfo::ownerId(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.ownerId();
+}
+
+QString ScriptFileInfo::path(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.path();
+}
+
+// bool  ScriptFileInfo::permission(QString file, QFile::Permissions permissions)
+// {
+// 	QFileInfo fp = QFileInfo(file);
+// 	return fp.permission();
+// }
+
+// QFile::Permissions  ScriptFileInfo::permissions(QString file)
+// {
+// 	QFileInfo fp = QFileInfo(file);
+// 	return fp.permissions();
+// }
+
+void  ScriptFileInfo::refresh(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.refresh();
+}
+
+void  ScriptFileInfo::setCaching(QString file, bool enable)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.setCaching(enable);
+}
+
+qint64  ScriptFileInfo::size(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.size();
+}
+
+QString ScriptFileInfo::suffix(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.suffix();
+}
+
+QString ScriptFileInfo::symLinkTarget(QString file)
+{
+	QFileInfo fp = QFileInfo(file);
+	return fp.symLinkTarget();
 }
--- src/tiled/scriptfileinfo.h
+++ src/tiled/scriptfileinfo.h
@@ -21,6 +21,10 @@
 #pragma once
 
 #include <QObject>
+#include <QtCore/qfileinfo.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qfileinfo.h>
+#include <QtCore/qdatetime.h>
 
 class ScriptFileInfo : public QObject
 {
@@ -28,7 +32,51 @@ class ScriptFileInfo : public QObject
 
 public:
 	ScriptFileInfo(QObject *parent = nullptr);
-
-	Q_INVOKABLE QString fileName(const QString &fp);
-	Q_INVOKABLE QString baseName(const QString &fp);
+  
+  Q_INVOKABLE QString absoluteDir(QString file);
+  Q_INVOKABLE QString absoluteFilePath(QString file);
+  Q_INVOKABLE QString absolutePath(QString file);
+  Q_INVOKABLE QString baseName(QString file);
+  Q_INVOKABLE QDateTime birthTime(QString file);
+  Q_INVOKABLE QString bundleName(QString file);
+  Q_INVOKABLE bool  caching(QString file);
+  Q_INVOKABLE QString canonicalFilePath(QString file);
+  Q_INVOKABLE QString canonicalPath(QString file);
+  Q_INVOKABLE QString completeBaseName(QString file);
+  Q_INVOKABLE QString completeSuffix(QString file);
+  Q_INVOKABLE QString dir(QString file);
+  Q_INVOKABLE bool  exists(QString file);
+  Q_INVOKABLE QString fileName(QString file);
+  Q_INVOKABLE QString filePath(QString file);
+  // Q_INVOKABLE QDateTime fileTime(QString file, QFile::FileTime time);
+  Q_INVOKABLE QString group(QString file);
+  Q_INVOKABLE uint  groupId(QString file);
+  Q_INVOKABLE bool  isAbsolute(QString file);
+  Q_INVOKABLE bool  isBundle(QString file);
+  Q_INVOKABLE bool  isDir(QString file);
+  Q_INVOKABLE bool  isExecutable(QString file);
+  Q_INVOKABLE bool  isFile(QString file);
+  Q_INVOKABLE bool  isHidden(QString file);
+  Q_INVOKABLE bool  isNativePath(QString file);
+  Q_INVOKABLE bool  isReadable(QString file);
+  Q_INVOKABLE bool  isRelative(QString file);
+  Q_INVOKABLE bool  isRoot(QString file);
+  Q_INVOKABLE bool  isShortcut(QString file);
+  Q_INVOKABLE bool  isSymLink(QString file);
+  Q_INVOKABLE bool  isSymbolicLink(QString file);
+  Q_INVOKABLE bool  isWritable(QString file);
+  Q_INVOKABLE QDateTime lastModified(QString file);
+  Q_INVOKABLE QDateTime lastRead(QString file);
+  Q_INVOKABLE bool  makeAbsolute(QString file);
+  Q_INVOKABLE QDateTime metadataChangeTime(QString file);
+  Q_INVOKABLE QString owner(QString file);
+  Q_INVOKABLE uint  ownerId(QString file);
+  Q_INVOKABLE QString path(QString file);
+  // Q_INVOKABLE bool  permission(QString file, QFile::Permissions permissions);
+  // Q_INVOKABLE QFile::Permissions  permissions(QString file);
+  Q_INVOKABLE void  refresh(QString file);
+  Q_INVOKABLE void  setCaching(QString file, bool enable);
+  Q_INVOKABLE qint64  size(QString file);
+  Q_INVOKABLE QString suffix(QString file);
+  Q_INVOKABLE QString symLinkTarget(QString file);
 };
