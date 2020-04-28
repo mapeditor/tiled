mapeditor/tiled

commit 5e338bc69928ed7159752bcbc1cad23eaa94bc28

Tiled Quick: Enable HighDpi scaling
* Disable rounding of the scaling factor since the UI can handle
  fractional scaling factors.

* Reverse the scaling factor for the map view, since this scaling factor
  should only apply to the UI and not the map.

This option works well for the currently used Material theme, but long
term we should reconsider relying on a scaling factor as opposed to
handling the DPI awareness in the theme.

src/tiledquick/qml/main.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.10
 import QtQuick.Controls 2.3
 import QtQuick.Layouts 1.3
+import QtQuick.Window 2.11
 import org.mapeditor.Tiled 1.0 as Tiled
 import Qt.labs.settings 1.0
 import Qt.labs.platform 1.0 as Platform
@@ -118,7 +119,11 @@ ApplicationWindow {
 
     Item {
         id: mapView
-        anchors.fill: parent
+
+        scale: 1 / Screen.devicePixelRatio
+        width: parent.width * Screen.devicePixelRatio
+        height: parent.height * Screen.devicePixelRatio
+        transformOrigin: Item.TopLeft
 
         Item {
             id: mapContainer

src/tiledquick/main.cpp
@@ -3,10 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
     QCoreApplication::setOrganizationDomain("mapeditor.org");
     QCoreApplication::setApplicationName("TiledQuick");
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    // We don't need the scaling factor to be rounded
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+
+    QApplication app(argc, argv);
 
     QString qmlDir = QApplication::applicationDirPath();
 #ifdef Q_OS_WIN

commit 0002171a6bd0699ddad27fc8c195643367f91913

Tiled Quick: Fixed panning speed when HiDpi scaling is used
Broke in 5e338bc69928ed7159752bcbc1cad23eaa94bc28.

src/tiledquick/qml/main.qml
@@ -79,19 +79,9 @@ ApplicationWindow {
     menuBar: MenuBar {
         Menu {
             title: qsTr("File")
-            MenuItem {
-                action: openAction
-                text: qsTr("Open...")
-                onTriggered: {
-                    fileDialog.open()
-                }
-            }
+            MenuItem { action: openAction }
             MenuSeparator {}
-            MenuItem {
-                text: qsTr("Exit")
-                action: exitAction
-                onTriggered: Qt.quit()
-            }
+            MenuItem { action: exitAction }
         }
         Menu {
             title: qsTr("Help")
@@ -165,6 +155,9 @@ ApplicationWindow {
         anchors.fill: parent
 
         onDragged: {
+            dx *= Screen.devicePixelRatio
+            dy *= Screen.devicePixelRatio
+
             if (containerAnimation.running) {
                 containerAnimation.stop()
                 containerAnimation.x += dx