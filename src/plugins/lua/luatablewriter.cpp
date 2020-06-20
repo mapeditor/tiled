mapeditor/tiled

commit 093cd3dea526c5152320fcd2b669accedb4be2a7

Lua plugin: Use a distinctive format to export object references
Rather than exporting as plain integers, object references now export as
"{ id = X }", making them recognizable. This is helpful since in the Lua
format no explicit type information is available for custom property
values.

Closes #2812

src/plugins/lua/luatablewriter.cpp
@@ -58,10 +58,11 @@ void LuaTableWriter::writeStartReturnTable()
     m_valueWritten = false;
 }
 
-void LuaTableWriter::writeStartTable(const QByteArray &name)
+void LuaTableWriter::writeStartTable(const char *name)
 {
     prepareNewLine();
-    write(name + (m_minimize ? "={" : " = {"));
+    write(name);
+    write(m_minimize ? "={" : " = {");
     ++m_indent;
     m_newLine = false;
     m_valueWritten = false;

src/plugins/lua/luatablewriter.h
@@ -41,7 +41,8 @@ public:
 
     void writeStartTable();
     void writeStartReturnTable();
-    void writeStartTable(const QByteArray &name);
+    void writeStartTable(const char *name);
+    void writeStartTable(const QString &name);
     void writeEndTable();
 
     void writeValue(int value);
@@ -95,6 +96,9 @@ private:
     bool m_valueWritten { false };
     bool m_error { false };
 };
+
+inline void LuaTableWriter::writeStartTable(const QString &name)
+{ writeStartTable(quote(name).toUtf8().constData()); }
 
 inline void LuaTableWriter::writeValue(int value)
 { writeUnquotedValue(QByteArray::number(value)); }

src/plugins/lua/luaplugin.cpp
@@ -79,7 +79,7 @@ public:
                             QRect bounds,
                             int compressionLevel);
     void writeObjectGroup(const Tiled::ObjectGroup *,
-                          const QByteArray &key = QByteArray());
+                          const char *key = nullptr);
     void writeImageLayer(const Tiled::ImageLayer *);
     void writeGroupLayer(const Tiled::GroupLayer *,
                          Tiled::Map::LayerDataFormat,
@@ -262,8 +262,16 @@ void LuaWriter::writeProperties(const Properties &properties)
     Properties::const_iterator it = properties.constBegin();
     Properties::const_iterator it_end = properties.constEnd();
     for (; it != it_end; ++it) {
-        const QVariant value = toExportValue(it.value(), mDir);
-        mWriter.writeQuotedKeyAndValue(it.key(), value);
+        if (it.value().userType() == objectRefTypeId()) {
+            mWriter.writeStartTable(it.key());
+            mWriter.setSuppressNewlines(true);
+            mWriter.writeKeyAndValue("id", it.value().value<ObjectRef>().id);
+            mWriter.writeEndTable();
+            mWriter.setSuppressNewlines(false);
+        } else {
+            const QVariant value = toExportValue(it.value(), mDir);
+            mWriter.writeQuotedKeyAndValue(it.key(), value);
+        }
     }
 
     mWriter.writeEndTable();
@@ -558,12 +566,12 @@ void LuaWriter::writeTileLayerData(const TileLayer *tileLayer,
 }
 
 void LuaWriter::writeObjectGroup(const ObjectGroup *objectGroup,
-                                 const QByteArray &key)
+                                 const char *key)
 {
-    if (key.isEmpty())
-        mWriter.writeStartTable();
-    else
+    if (key)
         mWriter.writeStartTable(key);
+    else
+        mWriter.writeStartTable();
 
     mWriter.writeKeyAndValue("type", "objectgroup");
     mWriter.writeKeyAndValue("draworder", drawOrderToString(objectGroup->drawOrder()));

commit 4bd95f39b0cd19273c977451fa1f618552f324c2

Lua plugin: Fixed syntax used for object properties
Was broken in change 093cd3dea526c5152320fcd2b669accedb4be2a7.

Closes #2839

src/plugins/lua/luatablewriter.cpp
@@ -68,6 +68,17 @@ void LuaTableWriter::writeStartTable(const char *name)
     m_valueWritten = false;
 }
 
+void LuaTableWriter::writeStartTable(const QString &name)
+{
+    prepareNewLine();
+    write('[');
+    write(quote(name).toUtf8());
+    write(m_minimize ? "]={" : "] = {");
+    ++m_indent;
+    m_newLine = false;
+    m_valueWritten = false;
+}
+
 void LuaTableWriter::writeEndTable()
 {
     Q_ASSERT(m_indent > 0);

src/plugins/lua/luatablewriter.h
@@ -97,9 +97,6 @@ private:
     bool m_error { false };
 };
 
-inline void LuaTableWriter::writeStartTable(const QString &name)
-{ writeStartTable(quote(name).toUtf8().constData()); }
-
 inline void LuaTableWriter::writeValue(int value)
 { writeUnquotedValue(QByteArray::number(value)); }
 