725c717fba7a707147637d6558da156f37b78b62
sverx <sverx@users.noreply.github.com>
2020-09-20 11:33:09 +0100

    just some better code for modifiers handling
    (what was I thinking??? :| )

--- src/tiled/snaphelper.cpp
+++ src/tiled/snaphelper.cpp
@@ -34,10 +34,13 @@ SnapHelper::SnapHelper(const MapRenderer *renderer,
     mSnapToFineGrid = preferences->snapToFineGrid();
     mSnapToPixels = preferences->snapToPixels();
 
-    if ((modifiers & (Qt::ControlModifier|Qt::ShiftModifier))==(Qt::ControlModifier|Qt::ShiftModifier))
-        toggleFineSnap();
-    else if (modifiers & Qt::ControlModifier)
-        toggleSnap();
+    if (modifiers & Qt::ControlModifier) {
+        if (modifiers & Qt::ShiftModifier) {
+            toggleFineSnap();
+        } else { 
+            toggleSnap();
+        }
+    }
 }
 
 void SnapHelper::toggleSnap()
