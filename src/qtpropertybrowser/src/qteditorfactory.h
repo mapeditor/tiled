45bd6dfa6377cc8b38a46e58ed70fde27c57631f
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2020-10-08 12:31:46 +0200

    qtpropertybrowser: Port to Qt 6
    * QRegExp -> QRegularExpression
    * QLocale::decimalPoint now returns QString
    * QStyleOption::init -> initFrom
    * QLayout::setMargin -> setContentsMargins
    * QFont::resolve -> resolveMask (hidden API...)

--- src/qtpropertybrowser/src/qteditorfactory.h
+++ src/qtpropertybrowser/src/qteditorfactory.h
@@ -188,7 +188,7 @@ private:
     Q_DECLARE_PRIVATE(QtLineEditFactory)
     Q_DISABLE_COPY(QtLineEditFactory)
     Q_PRIVATE_SLOT(d_func(), void slotPropertyChanged(QtProperty *, const QString &))
-    Q_PRIVATE_SLOT(d_func(), void slotRegExpChanged(QtProperty *, const QRegExp &))
+    Q_PRIVATE_SLOT(d_func(), void slotRegExpChanged(QtProperty *, const QRegularExpression &))
     Q_PRIVATE_SLOT(d_func(), void slotEchoModeChanged(QtProperty *, int))
     Q_PRIVATE_SLOT(d_func(), void slotReadOnlyChanged(QtProperty *, bool))
     Q_PRIVATE_SLOT(d_func(), void slotSetValue(const QString &))
