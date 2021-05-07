mapeditor/tiled

commit 837fdd88caee3e65743c1137a0c394fa75e4b197

Remove usage of deprecated enum QVariant::Type
Deprecated in Qt 6.0.

src/libtiled/varianttomapconverter.cpp
@@ -161,8 +161,8 @@ Properties VariantToMapConverter::toProperties(const QVariant &propertiesVariant
     QVariantMap::const_iterator it_end = propertiesMap.constEnd();
     for (; it != it_end; ++it) {
         int type = nameToType(propertyTypesMap.value(it.key()).toString());
-        if (type == QVariant::Invalid)
-            type = QVariant::String;
+        if (type == QMetaType::UnknownType)
+            type = QMetaType::QString;
 
         const QVariant value = fromExportValue(it.value(), type, mDir);
         properties[it.key()] = value;
@@ -176,8 +176,8 @@ Properties VariantToMapConverter::toProperties(const QVariant &propertiesVariant
         const QString propertyType = propertyVariantMap[QStringLiteral("type")].toString();
         const QVariant propertyValue = propertyVariantMap[QStringLiteral("value")];
         int type = nameToType(propertyType);
-        if (type == QVariant::Invalid)
-            type = QVariant::String;
+        if (type == QMetaType::UnknownType)
+            type = QMetaType::QString;
         properties[propertyName] = fromExportValue(propertyValue, type, mDir);
     }
 

src/tiled/variantpropertymanager.cpp
@@ -118,9 +118,9 @@ int VariantPropertyManager::attributeType(int propertyType,
 {
     if (propertyType == filePathTypeId()) {
         if (attribute == mFilterAttribute)
-            return QVariant::String;
+            return QMetaType::QString;
         if (attribute == mDirectoryAttribute)
-            return QVariant::Bool;
+            return QMetaType::Bool;
         return 0;
     }
     return QtVariantPropertyManager::attributeType(propertyType, attribute);
@@ -314,7 +314,7 @@ void VariantPropertyManager::setAttribute(QtProperty *property,
     if (mFilePathAttributes.contains(property)) {
         FilePathAttributes &attributes = mFilePathAttributes[property];
         if (attribute == mFilterAttribute) {
-            if (val.type() != QVariant::String && !val.canConvert(QVariant::String))
+            if (val.type() != QMetaType::QString && !val.canConvert(QMetaType::QString))
                 return;
             QString filter = val.toString();
             if (attributes.filter == filter)
@@ -354,7 +354,7 @@ void VariantPropertyManager::initializeProperty(QtProperty *property)
         mValues[property] = QVariant();
         if (type == filePathTypeId())
             mFilePathAttributes[property] = FilePathAttributes();
-    } else if (type == QVariant::String) {
+    } else if (type == QMetaType::QString) {
         mStringAttributes[property] = StringAttributes();
     } else if (type == alignmentTypeId()) {
         const Qt::Alignment align = Qt::AlignLeft | Qt::AlignVCenter;

src/libtiled/properties.cpp
@@ -129,11 +129,11 @@ int objectRefTypeId()
 QString typeToName(int type)
 {
     switch (type) {
-    case QVariant::String:
+    case QMetaType::QString:
         return QStringLiteral("string");
-    case QVariant::Double:
+    case QMetaType::Double:
         return QStringLiteral("float");
-    case QVariant::Color:
+    case QMetaType::QColor:
         return QStringLiteral("color");
     default:
         if (type == filePathTypeId())
@@ -147,11 +147,11 @@ QString typeToName(int type)
 int nameToType(const QString &name)
 {
     if (name == QLatin1String("string"))
-        return QVariant::String;
+        return QMetaType::QString;
     if (name == QLatin1String("float"))
-        return QVariant::Double;
+        return QMetaType::Double;
     if (name == QLatin1String("color"))
-        return QVariant::Color;
+        return QMetaType::QColor;
     if (name == QLatin1String("file"))
         return filePathTypeId();
     if (name == QLatin1String("object"))
@@ -164,7 +164,7 @@ QVariant toExportValue(const QVariant &value)
 {
     int type = value.userType();
 
-    if (type == QVariant::Color) {
+    if (type == QMetaType::QColor) {
         const QColor color = value.value<QColor>();
         return color.isValid() ? color.name(QColor::HexArgb) : QString();
     }
@@ -180,7 +180,7 @@ QVariant toExportValue(const QVariant &value)
 
 QVariant fromExportValue(const QVariant &value, int type)
 {
-    if (type == QVariant::Invalid)
+    if (type == QMetaType::UnknownType)
         return value;
 
     if (value.userType() == type)

src/plugins/lua/luatablewriter.cpp
@@ -140,13 +140,13 @@ void LuaTableWriter::writeQuotedKeyAndValue(const QString &key,
     write(quote(key).toUtf8());
     write(m_minimize ? "]=" : "] = ");
 
-    switch (value.type()) {
-    case QVariant::Int:
-    case QVariant::UInt:
-    case QVariant::LongLong:
-    case QVariant::ULongLong:
-    case QVariant::Double:
-    case QVariant::Bool:
+    switch (value.userType()) {
+    case QMetaType::Int:
+    case QMetaType::UInt:
+    case QMetaType::LongLong:
+    case QMetaType::ULongLong:
+    case QMetaType::Double:
+    case QMetaType::Bool:
         write(value.toString().toLatin1());
         break;
     default:

src/plugins/tbin/tbinplugin.cpp
@@ -76,20 +76,20 @@ namespace
             tbin::PropertyValue prop;
 
             switch (it.value().userType()) {
-            case QVariant::Bool:
+            case QMetaType::Bool:
                 prop.type = tbin::PropertyValue::Bool;
                 prop.data.b = it.value().toBool();
                 break;
-            case QVariant::Double:
+            case QMetaType::Double:
             case QMetaType::Float:
                 prop.type = tbin::PropertyValue::Float;
                 prop.data.f = it.value().toFloat();
                 break;
-            case QVariant::Int:
+            case QMetaType::Int:
                 prop.type = tbin::PropertyValue::Integer;
                 prop.data.i = it.value().toInt();
                 break;
-            case QVariant::String:
+            case QMetaType::QString:
                 prop.type = tbin::PropertyValue::String;
                 prop.dataStr = it.value().toString().toStdString();
                 break;

src/plugins/yy/yyplugin.cpp
@@ -389,11 +389,11 @@ static unsigned colorToAbgr(const QColor &color)
 
 static QString toOverriddenPropertyValue(const QVariant &value)
 {
-    switch (value.type()) {
-    case QVariant::Bool:
+    switch (value.userType()) {
+    case QMetaType::Bool:
         return value.toBool() ? QStringLiteral("True") : QStringLiteral("False");
 
-    case QVariant::Color: {
+    case QMetaType::QColor: {
         const unsigned abgr = colorToAbgr(value.value<QColor>());
         return QColor(abgr).name(QColor::HexArgb).replace(QLatin1Char('#'), QLatin1Char('$'));
     }

src/tiled/addpropertydialog.cpp
@@ -47,13 +47,13 @@ AddPropertyDialog::AddPropertyDialog(QWidget *parent)
     resize(Utils::dpiScaled(size()));
 
     // Add possible types from QVariant
-    mUi->typeBox->addItem(typeToName(QVariant::Bool),    false);
-    mUi->typeBox->addItem(typeToName(QVariant::Color),   QColor());
-    mUi->typeBox->addItem(typeToName(QVariant::Double),  0.0);
-    mUi->typeBox->addItem(typeToName(filePathTypeId()),  QVariant::fromValue(FilePath()));
-    mUi->typeBox->addItem(typeToName(QVariant::Int),     0);
-    mUi->typeBox->addItem(typeToName(objectRefTypeId()), QVariant::fromValue(ObjectRef()));
-    mUi->typeBox->addItem(typeToName(QVariant::String),  QString());
+    mUi->typeBox->addItem(typeToName(QMetaType::Bool),      false);
+    mUi->typeBox->addItem(typeToName(QMetaType::QColor),    QColor());
+    mUi->typeBox->addItem(typeToName(QMetaType::Double),    0.0);
+    mUi->typeBox->addItem(typeToName(filePathTypeId()),     QVariant::fromValue(FilePath()));
+    mUi->typeBox->addItem(typeToName(QMetaType::Int),       0);
+    mUi->typeBox->addItem(typeToName(objectRefTypeId()),    QVariant::fromValue(ObjectRef()));
+    mUi->typeBox->addItem(typeToName(QMetaType::QString),   QString());
 
     mUi->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 

commit 3cb0f1fed58b974ad8d4adf3e74467769bafb35c

Fix some warnings
Mostly comparison warnings caused by 837fdd88caee3e65743c1137a0c394.

src/libtiled/varianttomapconverter.cpp
@@ -733,12 +733,12 @@ std::unique_ptr<MapObject> VariantToMapConverter::toMapObject(const QVariantMap 
     const QVariant pointVariant = variantMap[QStringLiteral("point")];
     const QVariant textVariant = variantMap[QStringLiteral("text")];
 
-    if (polygonVariant.type() == QVariant::List) {
+    if (polygonVariant.userType() == QVariant::List) {
         object->setShape(MapObject::Polygon);
         object->setPolygon(toPolygon(polygonVariant));
         object->setPropertyChanged(MapObject::ShapeProperty);
     }
-    if (polylineVariant.type() == QVariant::List) {
+    if (polylineVariant.userType() == QVariant::List) {
         object->setShape(MapObject::Polyline);
         object->setPolygon(toPolygon(polylineVariant));
         object->setPropertyChanged(MapObject::ShapeProperty);
@@ -751,7 +751,7 @@ std::unique_ptr<MapObject> VariantToMapConverter::toMapObject(const QVariantMap 
         object->setShape(MapObject::Point);
         object->setPropertyChanged(MapObject::ShapeProperty);
     }
-    if (textVariant.type() == QVariant::Map) {
+    if (textVariant.userType() == QVariant::Map) {
         object->setTextData(toTextData(textVariant.toMap()));
         object->setShape(MapObject::Text);
         object->setPropertyChanged(MapObject::TextProperty);

src/tiled/propertybrowser.cpp
@@ -1597,7 +1597,7 @@ QtVariantProperty *PropertyBrowser::createCustomProperty(const QString &name, co
     mCustomPropertiesGroup->insertSubProperty(property, precedingProperty);
 
     // Collapse custom color properties, to save space
-    if (value.type() == QMetaType::QColor)
+    if (value.userType() == QMetaType::QColor)
         setExpanded(items(property).constFirst(), false);
 
     return property;

src/tiled/utils.cpp
@@ -102,8 +102,7 @@ QStringList cleanFilterList(const QString &filter)
     QRegularExpression regexp(QString::fromLatin1(filterRegExp));
     Q_ASSERT(regexp.isValid());
     QString f = filter;
-    QRegularExpressionMatch match;
-    filter.indexOf(regexp, 0, &match);
+    QRegularExpressionMatch match = regexp.match(filter);
     if (match.hasMatch())
         f = match.captured(2);
 #if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)

src/tiled/variantpropertymanager.cpp
@@ -314,7 +314,7 @@ void VariantPropertyManager::setAttribute(QtProperty *property,
     if (mFilePathAttributes.contains(property)) {
         FilePathAttributes &attributes = mFilePathAttributes[property];
         if (attribute == mFilterAttribute) {
-            if (val.type() != QMetaType::QString && !val.canConvert(QMetaType::QString))
+            if (val.userType() != QMetaType::QString && !val.canConvert(QMetaType::QString))
                 return;
             QString filter = val.toString();
             if (attributes.filter == filter)