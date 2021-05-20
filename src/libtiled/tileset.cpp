mapeditor/tiled

commit aac7b4159b26ad58a4f0c29e9933e383b52c3286

Simplified patch by keeping list of Tile* instead of tile IDs
Also addressed two clang warnings in the TilesetModel

src/libtiled/tileset.cpp
@@ -122,9 +122,9 @@ void Tileset::setMargin(int margin)
 /**
  * Returns the location of the tile with the given ID.
  */
-int Tileset::findTileLocation(int id)
+int Tileset::findTileLocation(Tile *tile) const
 {
-    return mSortedTileIds.indexOf(id);
+    return mTiles.indexOf(tile);
 }
 
 /**
@@ -132,12 +132,16 @@ int Tileset::findTileLocation(int id)
  */
 Tile *Tileset::findOrCreateTile(int id)
 {
-    if (Tile *tile = mTiles.value(id))
+    if (Tile *tile = mTilesById.value(id))
         return tile;
 
     mNextTileId = std::max(mNextTileId, id + 1);
-    mSortedTileIds.append(id);
-    return mTiles[id] = new Tile(id, this);
+
+    auto tile = new Tile(id, this);
+    mTilesById[id] = tile;
+    mTiles.append(tile);
+
+    return tile;
 }
 
 /**
@@ -229,12 +233,13 @@ bool Tileset::loadFromImage(const QImage &image, const QUrl &source)
                 tilePixmap.setMask(QBitmap::fromImage(mask));
             }
 
-            auto it = mTiles.find(tileNum);
-            if (it != mTiles.end()) {
+            auto it = mTilesById.find(tileNum);
+            if (it != mTilesById.end()) {
                 it.value()->setImage(tilePixmap);
             } else {
-                mTiles.insert(tileNum, new Tile(tilePixmap, tileNum, this));
-                mSortedTileIds.append(tileNum);
+                auto tile = new Tile(tilePixmap, tileNum, this);
+                mTilesById.insert(tileNum, tile);
+                mTiles.append(tile);
             }
 
             ++tileNum;
@@ -314,12 +319,13 @@ bool Tileset::loadImage()
     auto tiles = ImageCache::cutTiles(p);
 
     for (int tileNum = 0; tileNum < tiles.size(); ++tileNum) {
-        auto it = mTiles.find(tileNum);
-        if (it != mTiles.end()) {
+        auto it = mTilesById.find(tileNum);
+        if (it != mTilesById.end()) {
             it.value()->setImage(tiles.at(tileNum));
         } else {
-            mTiles.insert(tileNum, new Tile(tiles.at(tileNum), tileNum, this));
-            mSortedTileIds.append(tileNum);
+            auto tile = new Tile(tiles.at(tileNum), tileNum, this);
+            mTilesById.insert(tileNum, tile);
+            mTiles.append(tile);
         }
     }
 
@@ -481,8 +487,8 @@ Tile *Tileset::addTile(const QPixmap &image, const QUrl &source)
     newTile->setImage(image);
     newTile->setImageSource(source);
 
-    mTiles.insert(newTile->id(), newTile);
-    mSortedTileIds.append(newTile->id());
+    mTilesById.insert(newTile->id(), newTile);
+    mTiles.append(newTile);
     if (mTileHeight < image.height())
         mTileHeight = image.height();
     if (mTileWidth < image.width())
@@ -498,9 +504,9 @@ Tile *Tileset::addTile(const QPixmap &image, const QUrl &source)
 void Tileset::addTiles(const QList<Tile *> &tiles)
 {
     for (Tile *tile : tiles) {
-        Q_ASSERT(tile->tileset() == this && !mTiles.contains(tile->id()));
-        mTiles.insert(tile->id(), tile);
-        mSortedTileIds.append(tile->id());
+        Q_ASSERT(tile->tileset() == this && !mTilesById.contains(tile->id()));
+        mTilesById.insert(tile->id(), tile);
+        mTiles.append(tile);
     }
 
     updateTileSize();
@@ -514,9 +520,9 @@ void Tileset::addTiles(const QList<Tile *> &tiles)
 void Tileset::removeTiles(const QList<Tile *> &tiles)
 {
     for (Tile *tile : tiles) {
-        Q_ASSERT(tile->tileset() == this && mTiles.contains(tile->id()));
-        mTiles.remove(tile->id());
-        mSortedTileIds.removeOne(tile->id());
+        Q_ASSERT(tile->tileset() == this && mTilesById.contains(tile->id()));
+        mTilesById.remove(tile->id());
+        mTiles.removeOne(tile);
     }
 
     updateTileSize();
@@ -527,16 +533,17 @@ void Tileset::removeTiles(const QList<Tile *> &tiles)
  */
 void Tileset::deleteTile(int id)
 {
-    delete mTiles.take(id);
-    mSortedTileIds.removeOne(id);
+    auto tile = mTilesById.take(id);
+    mTiles.removeOne(tile);
+    delete tile;
 }
 
 /**
- * Move the tile with the given \a id to the \a position
+ * Moves the \a tile from its current position the given \a position.
  */
-void Tileset::relocateTile(int id, int location)
+void Tileset::relocateTile(Tile *tile, int location)
 {
-    mSortedTileIds.move(mSortedTileIds.indexOf(id), location);
+    mTiles.move(mTiles.indexOf(tile), location);
 }
 
 /**
@@ -551,7 +558,7 @@ void Tileset::setTileImage(Tile *tile,
                            const QUrl &source)
 {
     Q_ASSERT(isCollection());
-    Q_ASSERT(mTiles.value(tile->id()) == tile);
+    Q_ASSERT(mTilesById.value(tile->id()) == tile);
 
     const QSize previousImageSize = tile->image().size();
     const QSize newImageSize = image.size();
@@ -612,8 +619,8 @@ void Tileset::swap(Tileset &other)
     std::swap(mColumnCount, other.mColumnCount);
     std::swap(mExpectedColumnCount, other.mExpectedColumnCount);
     std::swap(mExpectedRowCount, other.mExpectedRowCount);
+    std::swap(mTilesById, other.mTilesById);
     std::swap(mTiles, other.mTiles);
-    std::swap(mSortedTileIds, other.mSortedTileIds);
     std::swap(mNextTileId, other.mNextTileId);
     std::swap(mWangSets, other.mWangSets);
     std::swap(mStatus, other.mStatus);
@@ -651,17 +658,13 @@ SharedTileset Tileset::clone() const
     c->mFormat = mFormat;
     c->mTransformationFlags = mTransformationFlags;
 
-    QMapIterator<int, Tile*> tileIterator(mTiles);
-    while (tileIterator.hasNext()) {
-        tileIterator.next();
+    for (auto tile : mTiles) {
+        const int id = tile->id();
+        Tile *clonedTile = tile->clone(c.data());
 
-        const int id = tileIterator.key();
-        const Tile *tile = tileIterator.value();
-
-        c->mTiles.insert(id, tile->clone(c.data()));
+        c->mTilesById.insert(id, tile->clone(c.data()));
+        c->mTiles.append(clonedTile);
     }
-
-    c->mSortedTileIds = mSortedTileIds;
 
     c->mWangSets.reserve(mWangSets.size());
     for (WangSet *wangSet : mWangSets)

src/libtiled/maptovariantconverter.cpp
@@ -248,7 +248,7 @@ QVariant MapToVariantConverter::toVariant(const Tileset &tileset,
     // Used for version 2
     QVariantList tilesVariant;
 
-    for (const Tile *tile  : tileset.tiles()) {
+    for (const Tile *tile : tileset.tiles()) {
         const Properties properties = tile->properties();
         QVariantMap tileVariant;
 

src/libtiled/mapwriter.cpp
@@ -410,8 +410,7 @@ void MapWriterPrivate::writeTileset(QXmlStreamWriter &w, const Tileset &tileset,
     }
 
     // Write the properties for those tiles that have them
-    for (int id : tileset.sortedTileIds()) {
-        const Tile *tile = tileset.findTile(id);
+    for (const Tile *tile : tileset.tiles()) {
         if (imageSource.isEmpty() || includeTile(tile)) {
             w.writeStartElement(QStringLiteral("tile"));
             w.writeAttribute(QStringLiteral("id"), QString::number(tile->id()));

src/libtiled/tileset.h
@@ -136,11 +136,10 @@ public:
     QSize gridSize() const;
     void setGridSize(QSize gridSize);
 
-    const QMap<int, Tile*> &tiles() const;
-    const QList<int> &sortedTileIds() const;
+    const QList<Tile*> &tiles() const;
     inline Tile *findTile(int id) const;
     Tile *tileAt(int id) const { return findTile(id); } // provided for Python
-    int findTileLocation(int id);
+    int findTileLocation(Tile *tile) const;
     Tile *findOrCreateTile(int id);
     int tileCount() const;
 
@@ -191,7 +190,7 @@ public:
     void addTiles(const QList<Tile*> &tiles);
     void removeTiles(const QList<Tile *> &tiles);
     void deleteTile(int id);
-    void relocateTile(int id, int location);
+    void relocateTile(Tile *tile, int location);
 
     void setNextTileId(int nextId);
     int nextTileId() const;
@@ -262,8 +261,8 @@ private:
     int mExpectedColumnCount;
     int mExpectedRowCount;
     int mNextTileId;
-    QMap<int, Tile*> mTiles;
-    QList<int> mSortedTileIds;
+    QMap<int, Tile*> mTilesById;
+    QList<Tile*> mTiles;
     QList<WangSet*> mWangSets;
     LoadingStatus mStatus;
     QColor mBackgroundColor;
@@ -425,17 +424,9 @@ inline void Tileset::setGridSize(QSize gridSize)
 /**
  * Returns a const reference to the tiles in this tileset.
  */
-inline const QMap<int, Tile *> &Tileset::tiles() const
+inline const QList<Tile*> &Tileset::tiles() const
 {
     return mTiles;
-}
-
-/**
- * Returns a const reference to the tiles in this tileset.
- */
-inline const QList<int> &Tileset::sortedTileIds() const
-{
-    return mSortedTileIds;
 }
 
 /**
@@ -444,7 +435,7 @@ inline const QList<int> &Tileset::sortedTileIds() const
  */
 inline Tile *Tileset::findTile(int id) const
 {
-    return mTiles.value(id);
+    return mTilesById.value(id);
 }
 
 /**

src/tiled/adjusttileindexes.cpp
@@ -224,17 +224,15 @@ AdjustTileMetaData::AdjustTileMetaData(TilesetDocument *tilesetDocument)
                       adjustAnimationFrames(fromTile->frames()));
     };
 
-    QMapIterator<int, Tile *> iterator{tileset.tiles()};
-
     if (newColumnCount > oldColumnCount) {
         // Increasing column count means information is copied to higher tiles,
         // so we need to iterate backwards.
-        iterator.toBack();
-        while (iterator.hasPrevious())
-            moveMetaData(iterator.previous().value());
+        const auto &tiles = tileset.tiles();
+        for (auto it = tiles.rbegin(); it != tiles.rend(); ++it)
+            moveMetaData(*it);
     } else {
-        while (iterator.hasNext())
-            moveMetaData(iterator.next().value());
+        for (Tile *tile : tileset.tiles())
+            moveMetaData(tile);
     }
 
     // Reset meta data on tiles that nothing was copied to

src/tiled/editabletileset.cpp
@@ -64,7 +64,7 @@ EditableTileset::EditableTileset(TilesetDocument *tilesetDocument,
 
 EditableTileset::~EditableTileset()
 {
-    detachTiles(tileset()->tiles().values());
+    detachTiles(tileset()->tiles());
     detachWangSets(tileset()->wangSets());
 
     EditableManager::instance().mEditableTilesets.remove(tileset());

src/tiled/relocatetile.cpp
@@ -28,18 +28,18 @@
 namespace Tiled {
 
 RelocateTile::RelocateTile(TilesetDocument *tilesetDocument,
-                     Tile *tile,
-                     int location)
+                           Tile *tile,
+                           int location)
     : QUndoCommand(QCoreApplication::translate("Undo Commands",
                                                "Relocate Tile"))
     , mTilesetDocument(tilesetDocument)
     , mTile(tile)
     , mLocation(location)
 {
-    mPrevLocation = mTilesetDocument->tileset()->findTileLocation(tile->id());
+    mPrevLocation = mTilesetDocument->tileset()->findTileLocation(tile);
 }
 
-void RelocateTile::relocate(const Tile *tile, int location)
+void RelocateTile::relocate(Tile *tile, int location)
 {
     mTilesetDocument->relocateTile(tile, location);
 }

src/tiled/relocatetile.h
@@ -44,14 +44,14 @@ public:
      * @param location        the target location
      */
     RelocateTile(TilesetDocument *tilesetDocument,
-              Tile *tile,
-              int location);
+                 Tile *tile,
+                 int location);
 
     void undo() { relocate(mTile, mPrevLocation); }
     void redo() { relocate(mTile, mLocation); }
 
 private:
-    void relocate(const Tile *tile, int location);
+    void relocate(Tile *tile, int location);
 
     TilesetDocument *mTilesetDocument;
     Tile *mTile;

commit cd4e361529093a75dbb2cd6e75e136809464d20c

Fixed double clone of tiles in Tileset::clone
Bug introduced in aac7b4159b26ad58a4f0c29e9933e383b52c3286.

src/libtiled/tileset.cpp
@@ -673,7 +673,7 @@ SharedTileset Tileset::clone() const
         const int id = tile->id();
         Tile *clonedTile = tile->clone(c.data());
 
-        c->mTilesById.insert(id, tile->clone(c.data()));
+        c->mTilesById.insert(id, clonedTile);
         c->mTiles.append(clonedTile);
     }
 