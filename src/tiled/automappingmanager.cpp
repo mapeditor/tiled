mapeditor/tiled

commit 957ec326ebabe1afc993d562a327c1e6e2cdc437

Update AutoMapping rules file when project properties might be changed
Closes #3176

src/tiled/automappingmanager.cpp
@@ -234,8 +234,11 @@ bool AutomappingManager::loadFile(const QString &filePath)
 }
 
 /**
- * The rules file is determind based on the map location, but can be overridden
- * by passing \a rulesFile.
+ * The rules file is determined based on the map location, or taken from the
+ * current project if a "rules.txt" file does not exist alongside the map (or
+ * when the map is not saved).
+ *
+ * Alternatively, it can can be overridden by passing a non-empty \a rulesFile.
  */
 void AutomappingManager::setMapDocument(MapDocument *mapDocument, const QString &rulesFile)
 {
@@ -260,12 +263,18 @@ void AutomappingManager::setMapDocument(MapDocument *mapDocument, const QString 
     refreshRulesFile(rulesFile);
 }
 
+/**
+ * Needs to be called when the project rules file path is changed.
+ *
+ * It is called automatically when the file name of the current MapDocument
+ * changes.
+ */
 void AutomappingManager::refreshRulesFile(const QString &ruleFileOverride)
 {
     mRulesFileOverride = !ruleFileOverride.isEmpty();
     QString rulesFile = ruleFileOverride;
 
-    if (rulesFile.isEmpty() && mMapDocument) {
+    if (rulesFile.isEmpty() && mMapDocument && !mMapDocument->fileName().isEmpty()) {
         const QString mapPath = QFileInfo(mMapDocument->fileName()).path();
         rulesFile = mapPath + QLatin1String("/rules.txt");
 

src/tiled/automappingmanager.h
@@ -52,8 +52,9 @@ public:
 
     void setMapDocument(MapDocument *mapDocument, const QString &rulesFile = QString());
 
-    QString errorString() const { return mError; }
+    void refreshRulesFile(const QString &ruleFileOverride = QString());
 
+    QString errorString() const { return mError; }
     QString warningString() const { return mWarning; }
 
     /**
@@ -80,8 +81,6 @@ private:
     void onRegionEdited(const QRegion &where, Layer *touchedLayer);
     void onMapFileNameChanged();
     void onFileChanged();
-
-    void refreshRulesFile(const QString &ruleFileOverride = QString());
 
     /**
      * This function parses a rules file.

src/tiled/mainwindow.cpp
@@ -1558,6 +1558,7 @@ void MainWindow::projectProperties()
         Preferences::instance()->setObjectTypesFile(project.mObjectTypesFile);
 
         ScriptManager::instance().refreshExtensionsPaths();
+        mAutomappingManager->refreshRulesFile();
     }
 }
 

commit 211d79b7eff2a6b9cd920744853a9cb91b647636

AutoMapping: Fixed ability to AutoMap using project rules in unsaved maps
Looks like a regression in 957ec326ebabe1afc993d562a327c1e6e2cdc437.

src/tiled/automappingmanager.cpp
@@ -59,7 +59,7 @@ void AutomappingManager::autoMap()
     QRegion region = mMapDocument->selectedArea();
 
     if (region.isEmpty()) {
-        Map *map = mMapDocument->map();
+        const Map *map = mMapDocument->map();
 
         if (map->infinite()) {
             LayerIterator iterator(map);
@@ -71,9 +71,7 @@ void AutomappingManager::autoMap()
             }
             region = bounds;
         } else {
-            int w = map->width();
-            int h = map->height();
-            region = QRect(0, 0, w, h);
+            region = QRect(0, 0, map->width(), map->height());
         }
     }
 
@@ -294,11 +292,13 @@ void AutomappingManager::refreshRulesFile(const QString &ruleFileOverride)
     mRulesFileOverride = !ruleFileOverride.isEmpty();
     QString rulesFile = ruleFileOverride;
 
-    if (rulesFile.isEmpty() && mMapDocument && !mMapDocument->fileName().isEmpty()) {
-        const QString mapPath = QFileInfo(mMapDocument->fileName()).path();
-        rulesFile = mapPath + QLatin1String("/rules.txt");
+    if (rulesFile.isEmpty() && mMapDocument) {
+        if (!mMapDocument->fileName().isEmpty()) {
+            const QDir mapDir = QFileInfo(mMapDocument->fileName()).dir();
+            rulesFile = mapDir.filePath(QStringLiteral("rules.txt"));
+        }
 
-        if (!QFileInfo::exists(rulesFile)) {
+        if (rulesFile.isEmpty() || !QFileInfo::exists(rulesFile)) {
             auto &project = ProjectManager::instance()->project();
             if (!project.mAutomappingRulesFile.isEmpty())
                 rulesFile = project.mAutomappingRulesFile;