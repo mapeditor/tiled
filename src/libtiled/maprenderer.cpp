mapeditor/tiled

commit ba252f2bb1666510a2cf3851d105db443968f37c

Refactor Map constructor (#3109)
This way, it should be less error-prone to add new parameters, since they
will be automatically included in copy constructor and in various other
places where they are copied.

Related to issue #2874

src/libtiled/maprenderer.cpp
@@ -479,9 +479,11 @@ void CellRenderer::paintTileCollisionShapes()
 {
     const Tileset *tileset = mTile->tileset();
     const bool isIsometric = tileset->orientation() == Tileset::Isometric;
-    const Map map(isIsometric ? Map::Isometric : Map::Orthogonal,
-                  QSize(1, 1),
-                  tileset->gridSize());
+    Map::Parameters mapParameters;
+    mapParameters.orientation = isIsometric ? Map::Isometric : Map::Orthogonal;
+    mapParameters.tileWidth = tileset->gridSize().width();
+    mapParameters.tileHeight = tileset->gridSize().height();
+    const Map map(mapParameters);
     const auto renderer = MapRenderer::create(&map);
 
     const qreal lineWidth = mRenderer->objectLineWidth();

src/libtiled/map.cpp
@@ -42,29 +42,26 @@
 using namespace Tiled;
 
 Map::Map()
+    : Map(Parameters())
+{
+}
+
+Map::Map(const Parameters &parameters)
     : Object(MapType)
+    , mParameters(parameters)
 {
 }
 
 Map::Map(Orientation orientation,
-         int width, int height, int tileWidth, int tileHeight, bool infinite)
-    : Object(MapType)
-    , mOrientation(orientation)
-    , mWidth(width)
-    , mHeight(height)
-    , mTileWidth(tileWidth)
-    , mTileHeight(tileHeight)
-    , mInfinite(infinite)
+         int width, int height,
+         int tileWidth, int tileHeight)
+    : Map()
 {
-}
-
-Map::Map(Orientation orientation,
-         QSize size, QSize tileSize, bool infinite)
-    : Map(orientation,
-          size.width(), size.height(),
-          tileSize.width(), tileSize.height(),
-          infinite)
-{
+    mParameters.orientation = orientation;
+    mParameters.width = width;
+    mParameters.height = height;
+    mParameters.tileWidth = tileWidth;
+    mParameters.tileHeight = tileHeight;
 }
 
 Map::~Map()
@@ -133,8 +130,8 @@ void Map::recomputeDrawMargins() const
     // We subtract the tile size of the map, since that part does not
     // contribute to additional margin.
     mDrawMargins = QMargins(offsetMargins.left(),
-                            offsetMargins.top() + maxTileSize - mTileHeight,
-                            offsetMargins.right() + maxTileSize - mTileWidth,
+                            offsetMargins.top() + maxTileSize - tileHeight(),
+                            offsetMargins.right() + maxTileSize - tileWidth(),
                             offsetMargins.bottom());
 
     mDrawMarginsDirty = false;
@@ -347,17 +344,11 @@ bool Map::isTilesetUsed(const Tileset *tileset) const
 
 std::unique_ptr<Map> Map::clone() const
 {
-    auto o = std::make_unique<Map>(mOrientation, mWidth, mHeight, mTileWidth, mTileHeight, mInfinite);
+    auto o = std::make_unique<Map>(mParameters);
     o->fileName = fileName;
     o->exportFileName = exportFileName;
     o->exportFormat = exportFormat;
-    o->mRenderOrder = mRenderOrder;
-    o->mCompressionLevel = mCompressionLevel;
-    o->mHexSideLength = mHexSideLength;
-    o->mStaggerAxis = mStaggerAxis;
-    o->mStaggerIndex = mStaggerIndex;
-    o->mBackgroundColor = mBackgroundColor;
-    o->mChunkSize = mChunkSize;
+    o->mEditorSettings = mEditorSettings;
     o->mDrawMargins = mDrawMargins;
     o->mDrawMarginsDirty = mDrawMarginsDirty;
     for (const Layer *layer : mLayers) {
@@ -366,7 +357,6 @@ std::unique_ptr<Map> Map::clone() const
         o->mLayers.append(clone);
     }
     o->mTilesets = mTilesets;
-    o->mLayerDataFormat = mLayerDataFormat;
     o->mNextLayerId = mNextLayerId;
     o->mNextObjectId = mNextObjectId;
     o->setProperties(properties());
@@ -538,12 +528,11 @@ QString Tiled::renderOrderToString(Map::RenderOrder renderOrder)
 Map::RenderOrder Tiled::renderOrderFromString(const QString &string)
 {
     Map::RenderOrder renderOrder = Map::RightDown;
-    if (string == QLatin1String("right-up")) {
+    if (string == QLatin1String("right-up"))
         renderOrder = Map::RightUp;
-    } else if (string == QLatin1String("left-down")) {
+    else if (string == QLatin1String("left-down"))
         renderOrder = Map::LeftDown;
-    } else if (string == QLatin1String("left-up")) {
+    else if (string == QLatin1String("left-up"))
         renderOrder = Map::LeftUp;
-    }
     return renderOrder;
 }

src/libtiled/map.h
@@ -147,22 +147,43 @@ public:
         StaggerEven = 1
     };
 
+    struct Parameters
+    {
+        Orientation orientation = Orthogonal;
+        RenderOrder renderOrder = RightDown;
+        int width = 0;
+        int height = 0;
+        int tileWidth = 0;
+        int tileHeight = 0;
+        bool infinite = false;
+        int hexSideLength = 0;
+        StaggerAxis staggerAxis = StaggerY;
+        StaggerIndex staggerIndex = StaggerOdd;
+        QColor backgroundColor;
+    };
+
+    struct EditorSettings
+    {
+        int compressionLevel = -1;
+        QSize chunkSize = QSize(CHUNK_SIZE, CHUNK_SIZE);
+        LayerDataFormat layerDataFormat = Base64Zlib;
+    };
+
     Map();
+    Map(const Parameters &parameters);
 
     /**
      * Constructor taking map orientation, size and tile size as parameters.
+     *
+     * @deprecated Only kept around for the Python API!
      */
     Map(Orientation orientation,
         int width, int height,
-        int tileWidth, int tileHeight,
-        bool infinite = false);
-
-    Map(Orientation orientation,
-        QSize size,
-        QSize tileSize,
-        bool infinite = false);
+        int tileWidth, int tileHeight);
 
     ~Map();
+
+    const Parameters &parameters() const;
 
     Orientation orientation() const;
     void setOrientation(Orientation orientation);
@@ -292,47 +313,43 @@ private:
 
     void recomputeDrawMargins() const;
 
-    Orientation mOrientation = Orthogonal;
-    RenderOrder mRenderOrder = RightDown;
-    int mCompressionLevel = -1;
-    int mWidth = 0;
-    int mHeight = 0;
-    int mTileWidth = 0;
-    int mTileHeight = 0;
-    bool mInfinite = false;
-    int mHexSideLength = 0;
-    StaggerAxis mStaggerAxis = StaggerY;
-    StaggerIndex mStaggerIndex = StaggerOdd;
-    QColor mBackgroundColor;
-    QSize mChunkSize = QSize(CHUNK_SIZE, CHUNK_SIZE);
+    Parameters mParameters;
+    EditorSettings mEditorSettings;
+
     mutable QMargins mDrawMargins;
     mutable bool mDrawMarginsDirty = true;
+
     QList<Layer*> mLayers;
     QVector<SharedTileset> mTilesets;
-    LayerDataFormat mLayerDataFormat = Base64Zlib;
+
     int mNextLayerId = 1;
     int mNextObjectId = 1;
 };
 
 
+inline const Map::Parameters &Map::parameters() const
+{
+    return mParameters;
+}
+
 inline Map::Orientation Map::orientation() const
 {
-    return mOrientation;
+    return mParameters.orientation;
 }
 
 inline void Map::setOrientation(Map::Orientation orientation)
 {
-    mOrientation = orientation;
+    mParameters.orientation = orientation;
 }
 
 inline Map::RenderOrder Map::renderOrder() const
 {
-    return mRenderOrder;
+    return mParameters.renderOrder;
 }
 
 inline void Map::setRenderOrder(Map::RenderOrder renderOrder)
 {
-    mRenderOrder = renderOrder;
+    mParameters.renderOrder = renderOrder;
 }
 
 /**
@@ -340,12 +357,12 @@ inline void Map::setRenderOrder(Map::RenderOrder renderOrder)
  */
 inline int Map::compressionLevel() const
 {
-    return mCompressionLevel;
+    return mEditorSettings.compressionLevel;
 }
 
 inline void Map::setCompressionLevel(int compressionLevel)
 {
-    mCompressionLevel = compressionLevel;
+    mEditorSettings.compressionLevel = compressionLevel;
 }
 
 /**
@@ -353,7 +370,7 @@ inline void Map::setCompressionLevel(int compressionLevel)
  */
 inline int Map::width() const
 {
-    return mWidth;
+    return mParameters.width;
 }
 
 /**
@@ -361,7 +378,7 @@ inline int Map::width() const
  */
 inline void Map::setWidth(int width)
 {
-    mWidth = width;
+    mParameters.width = width;
 }
 
 /**
@@ -369,7 +386,7 @@ inline void Map::setWidth(int width)
  */
 inline int Map::height() const
 {
-    return mHeight;
+    return mParameters.height;
 }
 
 /**
@@ -377,7 +394,7 @@ inline int Map::height() const
  */
 inline void Map::setHeight(int height)
 {
-    mHeight = height;
+    mParameters.height = height;
 }
 
 /**
@@ -385,7 +402,7 @@ inline void Map::setHeight(int height)
  */
 inline QSize Map::size() const
 {
-    return QSize(mWidth, mHeight);
+    return QSize(mParameters.width, mParameters.height);
 }
 
 /**
@@ -393,7 +410,7 @@ inline QSize Map::size() const
  */
 inline int Map::tileWidth() const
 {
-    return mTileWidth;
+    return mParameters.tileWidth;
 }
 
 /**
@@ -401,7 +418,7 @@ inline int Map::tileWidth() const
  */
 inline void Map::setTileWidth(int width)
 {
-    mTileWidth = width;
+    mParameters.tileWidth = width;
 }
 
 /**
@@ -409,7 +426,7 @@ inline void Map::setTileWidth(int width)
  */
 inline int Map::tileHeight() const
 {
-    return mTileHeight;
+    return mParameters.tileHeight;
 }
 
 /**
@@ -417,7 +434,7 @@ inline int Map::tileHeight() const
  */
 inline void Map::setTileHeight(int height)
 {
-    mTileHeight = height;
+    mParameters.tileHeight = height;
 }
 
 /**
@@ -425,52 +442,52 @@ inline void Map::setTileHeight(int height)
  */
 inline QSize Map::tileSize() const
 {
-    return QSize(mTileWidth, mTileHeight);
+    return QSize(mParameters.tileWidth, mParameters.tileHeight);
 }
 
 inline bool Map::infinite() const
 {
-    return mInfinite;
+    return mParameters.infinite;
 }
 
 inline void Map::setInfinite(bool infinite)
 {
-    mInfinite = infinite;
+    mParameters.infinite = infinite;
 }
 
 inline int Map::hexSideLength() const
 {
-    return mHexSideLength;
+    return mParameters.hexSideLength;
 }
 
 inline void Map::setHexSideLength(int hexSideLength)
 {
-    mHexSideLength = hexSideLength;
+    mParameters.hexSideLength = hexSideLength;
 }
 
 inline Map::StaggerAxis Map::staggerAxis() const
 {
-    return mStaggerAxis;
+    return mParameters.staggerAxis;
 }
 
 inline void Map::setStaggerAxis(StaggerAxis staggerAxis)
 {
-    mStaggerAxis = staggerAxis;
+    mParameters.staggerAxis = staggerAxis;
 }
 
 inline Map::StaggerIndex Map::staggerIndex() const
 {
-    return mStaggerIndex;
+    return mParameters.staggerIndex;
 }
 
 inline void Map::setStaggerIndex(StaggerIndex staggerIndex)
 {
-    mStaggerIndex = staggerIndex;
+    mParameters.staggerIndex = staggerIndex;
 }
 
 inline void Map::invertStaggerIndex()
 {
-    mStaggerIndex = static_cast<StaggerIndex>(!mStaggerIndex);
+    mParameters.staggerIndex = static_cast<StaggerIndex>(!mParameters.staggerIndex);
 }
 
 inline void Map::invalidateDrawMargins()
@@ -584,12 +601,12 @@ inline const QVector<SharedTileset> &Map::tilesets() const
  */
 inline const QColor &Map::backgroundColor() const
 {
-    return mBackgroundColor;
+    return mParameters.backgroundColor;
 }
 
 inline void Map::setBackgroundColor(QColor color)
 {
-    mBackgroundColor = color;
+    mParameters.backgroundColor = color;
 }
 
 /**
@@ -597,12 +614,12 @@ inline void Map::setBackgroundColor(QColor color)
  */
 inline QSize Map::chunkSize() const
 {
-    return mChunkSize;
+    return mEditorSettings.chunkSize;
 }
 
 inline void Map::setChunkSize(QSize size)
 {
-    mChunkSize = size;
+    mEditorSettings.chunkSize = size;
 }
 
 /**
@@ -615,12 +632,12 @@ inline bool Map::isStaggered() const
 
 inline Map::LayerDataFormat Map::layerDataFormat() const
 {
-    return mLayerDataFormat;
+    return mEditorSettings.layerDataFormat;
 }
 
 inline void Map::setLayerDataFormat(Map::LayerDataFormat format)
 {
-    mLayerDataFormat = format;
+    mEditorSettings.layerDataFormat = format;
 }
 
 /**

src/libtiled/mapreader.cpp
@@ -244,19 +244,14 @@ std::unique_ptr<Map> MapReaderPrivate::readMap()
     Q_ASSERT(xml.isStartElement() && xml.name() == QLatin1String("map"));
 
     const QXmlStreamAttributes atts = xml.attributes();
-    const int mapWidth = atts.value(QLatin1String("width")).toInt();
-    const int mapHeight = atts.value(QLatin1String("height")).toInt();
-    const int tileWidth = atts.value(QLatin1String("tilewidth")).toInt();
-    const int tileHeight = atts.value(QLatin1String("tileheight")).toInt();
-    const int infinite = atts.value(QLatin1String("infinite")).toInt();
-    const int hexSideLength = atts.value(QLatin1String("hexsidelength")).toInt();
 
     const QString orientationString =
             atts.value(QLatin1String("orientation")).toString();
-    const Map::Orientation orientation =
-            orientationFromString(orientationString);
 
-    if (orientation == Map::Unknown) {
+    Map::Parameters mapParameters;
+    mapParameters.orientation = orientationFromString(orientationString);
+
+    if (mapParameters.orientation == Map::Unknown) {
         xml.raiseError(tr("Unsupported map orientation: \"%1\"")
                        .arg(orientationString));
     }
@@ -265,27 +260,33 @@ std::unique_ptr<Map> MapReaderPrivate::readMap()
     const QString staggerIndex = atts.value(QLatin1String("staggerindex")).toString();
     const QString renderOrder = atts.value(QLatin1String("renderorder")).toString();
 
+    mapParameters.renderOrder = renderOrderFromString(renderOrder);
+    mapParameters.width = atts.value(QLatin1String("width")).toInt();
+    mapParameters.height = atts.value(QLatin1String("height")).toInt();
+    mapParameters.tileWidth = atts.value(QLatin1String("tilewidth")).toInt();
+    mapParameters.tileHeight = atts.value(QLatin1String("tileheight")).toInt();
+    mapParameters.infinite = atts.value(QLatin1String("infinite")).toInt();
+    mapParameters.hexSideLength = atts.value(QLatin1String("hexsidelength")).toInt();
+    mapParameters.staggerAxis = staggerAxisFromString(staggerAxis);
+    mapParameters.staggerIndex = staggerIndexFromString(staggerIndex);
+
+    const QString backgroundColor = atts.value(QLatin1String("backgroundcolor")).toString();
+    if (QColor::isValidColor(backgroundColor))
+        mapParameters.backgroundColor = QColor(backgroundColor);
+
+    mMap = std::make_unique<Map>(mapParameters);
+
     bool compressionLevelOk;
     const int compressionLevel = atts.value(QLatin1String("compressionlevel")).toInt(&compressionLevelOk);
-
     const int nextLayerId = atts.value(QLatin1String("nextlayerid")).toInt();
     const int nextObjectId = atts.value(QLatin1String("nextobjectid")).toInt();
 
-    mMap = std::make_unique<Map>(orientation, mapWidth, mapHeight, tileWidth, tileHeight, infinite);
-    mMap->setHexSideLength(hexSideLength);
-    mMap->setStaggerAxis(staggerAxisFromString(staggerAxis));
-    mMap->setStaggerIndex(staggerIndexFromString(staggerIndex));
-    mMap->setRenderOrder(renderOrderFromString(renderOrder));
     if (compressionLevelOk)
         mMap->setCompressionLevel(compressionLevel);
     if (nextLayerId)
         mMap->setNextLayerId(nextLayerId);
     if (nextObjectId)
         mMap->setNextObjectId(nextObjectId);
-
-    const QString backgroundColor = atts.value(QLatin1String("backgroundcolor")).toString();
-    if (QColor::isValidColor(backgroundColor))
-        mMap->setBackgroundColor(QColor(backgroundColor));
 
     while (xml.readNextStartElement()) {
         if (xml.name() == QLatin1String("editorsettings"))

src/libtiled/varianttomapconverter.cpp
@@ -55,9 +55,10 @@ std::unique_ptr<Map> VariantToMapConverter::toMap(const QVariant &variant,
     const QVariantMap variantMap = variant.toMap();
     const QString orientationString = variantMap[QStringLiteral("orientation")].toString();
 
-    Map::Orientation orientation = orientationFromString(orientationString);
+    Map::Parameters mapParameters;
+    mapParameters.orientation = orientationFromString(orientationString);
 
-    if (orientation == Map::Unknown) {
+    if (mapParameters.orientation == Map::Unknown) {
         mError = tr("Unsupported map orientation: \"%1\"")
                 .arg(orientationString);
         return nullptr;
@@ -70,16 +71,21 @@ std::unique_ptr<Map> VariantToMapConverter::toMap(const QVariant &variant,
     const int nextLayerId = variantMap[QStringLiteral("nextlayerid")].toInt();
     const int nextObjectId = variantMap[QStringLiteral("nextobjectid")].toInt();
 
-    std::unique_ptr<Map> map(new Map(orientation,
-                                     variantMap[QStringLiteral("width")].toInt(),
-                                     variantMap[QStringLiteral("height")].toInt(),
-                                     variantMap[QStringLiteral("tilewidth")].toInt(),
-                                     variantMap[QStringLiteral("tileheight")].toInt(),
-                                     variantMap[QStringLiteral("infinite")].toInt()));
-    map->setHexSideLength(variantMap[QStringLiteral("hexsidelength")].toInt());
-    map->setStaggerAxis(staggerAxisFromString(staggerAxis));
-    map->setStaggerIndex(staggerIndexFromString(staggerIndex));
-    map->setRenderOrder(renderOrderFromString(renderOrder));
+    mapParameters.renderOrder = renderOrderFromString(renderOrder);
+    mapParameters.width = variantMap[QStringLiteral("width")].toInt();
+    mapParameters.height = variantMap[QStringLiteral("height")].toInt();
+    mapParameters.tileWidth = variantMap[QStringLiteral("tilewidth")].toInt();
+    mapParameters.tileHeight = variantMap[QStringLiteral("tileheight")].toInt();
+    mapParameters.infinite = variantMap[QStringLiteral("infinite")].toInt();
+    mapParameters.hexSideLength = variantMap[QStringLiteral("hexsidelength")].toInt();
+    mapParameters.staggerAxis = staggerAxisFromString(staggerAxis);
+    mapParameters.staggerIndex = staggerIndexFromString(staggerIndex);
+
+    const QString bgColor = variantMap[QStringLiteral("backgroundcolor")].toString();
+    if (QColor::isValidColor(bgColor))
+        mapParameters.backgroundColor = QColor(bgColor);
+
+    auto map = std::make_unique<Map>(mapParameters);
     if (nextLayerId)
         map->setNextLayerId(nextLayerId);
     if (nextObjectId)
@@ -89,10 +95,6 @@ std::unique_ptr<Map> VariantToMapConverter::toMap(const QVariant &variant,
 
     mMap = map.get();
     map->setProperties(extractProperties(variantMap));
-
-    const QString bgColor = variantMap[QStringLiteral("backgroundcolor")].toString();
-    if (QColor::isValidColor(bgColor))
-        map->setBackgroundColor(QColor(bgColor));
 
     const auto tilesetVariants = variantMap[QStringLiteral("tilesets")].toList();
     for (const QVariant &tilesetVariant : tilesetVariants) {

src/plugins/droidcraft/droidcraftplugin.cpp
@@ -60,7 +60,12 @@ std::unique_ptr<Tiled::Map> DroidcraftPlugin::read(const QString &fileName)
     // Build 48 x 48 map
     // Create a Map -> Create a Tileset -> Add Tileset to map
     // -> Create a TileLayer -> Fill layer -> Add TileLayer to Map
-    std::unique_ptr<Map> map { new Map(Map::Orthogonal, 48, 48, 32, 32) };
+    Map::Parameters mapParameters;
+    mapParameters.width = 48;
+    mapParameters.height = 48;
+    mapParameters.tileWidth = 32;
+    mapParameters.tileHeight = 32;
+    auto map = std::make_unique<Map>(mapParameters);
 
     SharedTileset mapTileset(Tileset::create("tileset", 32, 32));
     mapTileset->loadFromImage(QImage(":/tileset.png"), QUrl("qrc://tileset.png"));

src/plugins/flare/flareplugin.cpp
@@ -61,7 +61,14 @@ std::unique_ptr<Tiled::Map> FlarePlugin::read(const QString &fileName)
     }
 
     // default to values of the original Flare alpha game.
-    auto map = std::make_unique<Map>(Map::Isometric, 256, 256, 64, 32);
+    Map::Parameters mapParameters;
+    mapParameters.orientation = Map::Isometric;
+    mapParameters.width = 256;
+    mapParameters.height = 256;
+    mapParameters.tileWidth = 64;
+    mapParameters.tileHeight = 32;
+
+    auto map = std::make_unique<Map>(mapParameters);
 
     QTextStream stream (&file);
     QString line;

src/plugins/replicaisland/replicaislandplugin.cpp
@@ -71,8 +71,11 @@ std::unique_ptr<Tiled::Map> ReplicaIslandPlugin::read(const QString &fileName)
         return nullptr;
     }
 
-    // Create our map, setting width and height to 0 until we load a layer.
-    std::unique_ptr<Map> map { new Map(Map::Orthogonal, 0, 0, 32, 32) };
+    // Create our map, leaving width and height to 0 until we load a layer.
+    Map::Parameters mapParameters;
+    mapParameters.tileWidth = 32;
+    mapParameters.tileHeight = 32;
+    auto map = std::make_unique<Map>(mapParameters);
     map->setProperty("background_index", QString::number(backgroundIndex));
 
     // Load our Tilesets.

commit 1dbdad42e1f5a26a90fe06ef3f6855812b089a3a

Fixed offset of tile collision shapes on isometric maps
Regression in ba252f2bb1666510a2cf3851d105db443968f37c. For isometric
maps, the position of the tile collision shapes depended on the map
size, which unintentionally changed from 1x1 to 0x0 in this context.

Closes #3138

src/libtiled/maprenderer.cpp
@@ -516,6 +516,8 @@ void CellRenderer::paintTileCollisionShapes()
     const bool isIsometric = tileset->orientation() == Tileset::Isometric;
     Map::Parameters mapParameters;
     mapParameters.orientation = isIsometric ? Map::Isometric : Map::Orthogonal;
+    mapParameters.width = 1;
+    mapParameters.height = 1;
     mapParameters.tileWidth = tileset->gridSize().width();
     mapParameters.tileHeight = tileset->gridSize().height();
     const Map map(mapParameters);