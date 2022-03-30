mapeditor/tiled

commit f9a806a43c858c4d4a7471c2eb1ac3c8028d5e9c

Fixed missing name for the undo command that moves a map in a world
Technically breaks string freeze, but better to have an English name
than no name at all.

src/tiled/worldmovemaptool.cpp
@@ -56,7 +56,8 @@ class SetMapRectCommand : public QUndoCommand
 {
 public:
     SetMapRectCommand(const QString &mapName, QRect rect)
-        : mMapName(mapName)
+        : QUndoCommand(QCoreApplication::translate("Undo Commands", "Move Map"))
+        , mMapName(mapName)
         , mRect(rect)
     {
         const WorldManager &manager = WorldManager::instance();
@@ -81,12 +82,12 @@ private:
 
 
 WorldMoveMapTool::WorldMoveMapTool(QObject *parent)
-    : AbstractWorldTool("WorldMoveMapTool", tr("World Tool"),
+    : AbstractWorldTool("WorldMoveMapTool",
+                        tr("World Tool"),
                         QIcon(QLatin1String(":images/22/world-move-tool.png")),
                         QKeySequence(Qt::Key_N),
                         parent)
 {
-    languageChanged();
 }
 
 WorldMoveMapTool::~WorldMoveMapTool()

commit 47dab982d3beb8cfcc6bf92827eb38bb1282f3c7

Fixed some World Tool actions no longer having names
Regression in f9a806a43c858c4d4a7471c2eb1ac3c8028d5e9c.

I had removed the languageChanged() call since it appeared to be
superfluous and it caused a warning about calling a virtual function in
the constructor. However, it wasn't entirely unnecessary due to the
implementation in the base class. Resolved while still avoiding to call
a virtual function in the constructor.

src/tiled/abstractworldtool.cpp
@@ -145,6 +145,8 @@ AbstractWorldTool::AbstractWorldTool(Id id,
     mRemoveMapFromWorldAction->setShortcut(Qt::SHIFT + Qt::Key_D);
     ActionManager::registerAction(mRemoveMapFromWorldAction, "RemoveMap");
     connect(mRemoveMapFromWorldAction, &QAction::triggered, this, &AbstractWorldTool::removeCurrentMapFromWorld);
+
+    languageChangedImpl();
 }
 
 AbstractWorldTool::~AbstractWorldTool() = default;
@@ -194,6 +196,11 @@ void AbstractWorldTool::mousePressed(QGraphicsSceneMouseEvent *event)
 }
 
 void AbstractWorldTool::languageChanged()
+{
+    languageChangedImpl();
+}
+
+void AbstractWorldTool::languageChangedImpl()
 {
     mAddAnotherMapToWorldAction->setText(tr("Add another map to the current world"));
     mAddMapToWorldAction->setText(tr("Add the current map to a loaded world"));

src/tiled/abstractworldtool.h
@@ -89,6 +89,8 @@ protected:
     void showContextMenu(QGraphicsSceneMouseEvent *);
 
 private:
+    void languageChangedImpl();
+
     MapDocument *mTargetMap = nullptr;
 
     QAction *mAddAnotherMapToWorldAction;

src/tiled/worldmovemaptool.cpp
@@ -160,10 +160,6 @@ void WorldMoveMapTool::mouseEntered()
 {
 }
 
-void WorldMoveMapTool::mouseLeft()
-{
-}
-
 void WorldMoveMapTool::mousePressed(QGraphicsSceneMouseEvent *event)
 {
     if (mDraggingMap)

src/tiled/worldmovemaptool.h
@@ -36,9 +36,8 @@ public:
     explicit WorldMoveMapTool(QObject *parent = nullptr);
     ~WorldMoveMapTool() override;
 
-    void keyPressed(QKeyEvent *) override;
+    void keyPressed(QKeyEvent *event) override;
     void mouseEntered() override;
-    void mouseLeft() override;
     void mouseMoved(const QPointF &pos,
                     Qt::KeyboardModifiers modifiers) override;
     void mousePressed(QGraphicsSceneMouseEvent *event) override;