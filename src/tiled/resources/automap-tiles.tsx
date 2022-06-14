95af3a7db05836994afaa94da7c674981d8d4a58
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2022-06-14 17:25:57 +0200

    Modified the Automapping Rules tileset based on feedback
    See https://github.com/mapeditor/tiled/issues/3100#issuecomment-1154423548

--- src/tiled/resources/automap-tiles.tsx
+++ src/tiled/resources/automap-tiles.tsx
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="automap-tiles" tilewidth="32" tileheight="32" tilecount="5" columns="5" tilerendersize="grid" fillmode="preserve-aspect-fit">
+<tileset version="1.9" tiledversion="1.8.5" name="AutoMap Rules" tilewidth="32" tileheight="32" tilecount="5" columns="5" tilerendersize="grid" fillmode="preserve-aspect-fit">
  <image source="automap-tiles.svg" width="160" height="32"/>
- <tile id="0">
+ <tile id="3">
   <properties>
-   <property name="MatchType" value="Forbid"/>
+   <property name="MatchType" value="Empty"/>
   </properties>
  </tile>
  <tile id="1">
@@ -16,14 +16,14 @@
    <property name="MatchType" value="NonEmpty"/>
   </properties>
  </tile>
- <tile id="3">
-  <properties>
-   <property name="MatchType" value="Empty"/>
-  </properties>
- </tile>
  <tile id="4">
   <properties>
    <property name="MatchType" value="Other"/>
+  </properties>
+ </tile>
+ <tile id="0">
+  <properties>
+   <property name="MatchType" value="Forbid"/>
   </properties>
  </tile>
 </tileset>
