mapeditor/tiled

commit 517fb0fb9ee53ae77b6ab05cf7a1a9c8f38dcbd3

Simplified change object transform commands (#3349)
The previous implementation relied on changing objects directly and then 
creating an undo command when finishing up the move, resize or rotate
action. Now the undo command merges and becomes obsolete as expected, so
it can be used during the action.

This also affects changes to object position, size and rotation done 
through the Properties view.

src/tiled/mapdocument.cpp
@@ -44,7 +44,6 @@
 #include "mapobjectmodel.h"
 #include "maprenderer.h"
 #include "movelayer.h"
-#include "movemapobject.h"
 #include "movemapobjecttogroup.h"
 #include "objectgroup.h"
 #include "objecttemplate.h"
@@ -54,11 +53,11 @@
 #include "reparentlayers.h"
 #include "resizemap.h"
 #include "resizetilelayer.h"
-#include "rotatemapobject.h"
 #include "templatemanager.h"
 #include "tile.h"
 #include "tilelayer.h"
 #include "tilesetdocument.h"
+#include "transformmapobjects.h"
 
 #include <QFileInfo>
 #include <QRect>
@@ -337,6 +336,7 @@ void MapDocument::resizeMap(QSize size, QPoint offset, bool removeObjects)
     QUndoCommand *command = new QUndoCommand(tr("Resize Map"));
 
     QList<MapObject *> objectsToRemove;
+    QList<MapObject *> objectsToMove;
 
     LayerIterator iterator(map());
     while (Layer *layer = iterator.next()) {
@@ -350,14 +350,11 @@ void MapDocument::resizeMap(QSize size, QPoint offset, bool removeObjects)
             ObjectGroup *objectGroup = static_cast<ObjectGroup*>(layer);
 
             for (MapObject *o : objectGroup->objects()) {
-                if (removeObjects && !visibleIn(visibleArea, o, *renderer())) {
-                    // Remove objects that will fall outside of the map
+                // Remove objects that will fall outside of the map
+                if (removeObjects && !visibleIn(visibleArea, o, *renderer()))
                     objectsToRemove.append(o);
-                } else {
-                    QPointF oldPos = o->position();
-                    QPointF newPos = oldPos + pixelOffset;
-                    new MoveMapObject(this, o, newPos, oldPos, command);
-                }
+                else if (!pixelOffset.isNull())
+                    objectsToMove.append(o);
             }
             break;
         }
@@ -378,6 +375,15 @@ void MapDocument::resizeMap(QSize size, QPoint offset, bool removeObjects)
 
     if (!objectsToRemove.isEmpty())
         new RemoveMapObjects(this, objectsToRemove, command);
+
+    if (!objectsToMove.isEmpty()) {
+        QVector<TransformState> states;
+        for (MapObject *o : qAsConst(objectsToMove)) {
+            states.append(TransformState(o));
+            states.last().setPosition(o->position() + pixelOffset);
+        }
+        new TransformMapObjects(this, objectsToMove, states, command);
+    }
 
     new ResizeMap(this, size, command);
     new ChangeSelectedArea(this, movedSelection, command);
@@ -446,12 +452,11 @@ void MapDocument::rotateSelectedObjects(RotateDirection direction)
     if (mSelectedObjects.isEmpty())
         return;
 
-    undoStack()->beginMacro(tr("Rotate %n Object(s)", "",
-                               mSelectedObjects.size()));
+    QVector<TransformState> states;
+    states.reserve(mSelectedObjects.size());
 
     // TODO: Rotate them properly as a group
-    const auto &selectedObjects = mSelectedObjects;
-    for (MapObject *mapObject : selectedObjects) {
+    for (MapObject *mapObject : qAsConst(mSelectedObjects)) {
         const qreal oldRotation = mapObject->rotation();
         qreal newRotation = oldRotation;
 
@@ -465,10 +470,13 @@ void MapDocument::rotateSelectedObjects(RotateDirection direction)
                 newRotation -= 360;
         }
 
-        undoStack()->push(new RotateMapObject(this, mapObject,
-                                              newRotation, oldRotation));
+        states.append(TransformState(mapObject));
+        auto &state = states.last();
+        state.setRotation(newRotation);
     }
-    undoStack()->endMacro();
+
+    auto command = new TransformMapObjects(this, mSelectedObjects, states);
+    command->setText(tr("Rotate %n Object(s)", "", mSelectedObjects.size()));
 }
 
 /**

src/tiled/abstractobjecttool.cpp
@@ -34,7 +34,6 @@
 #include "mapscene.h"
 #include "objectgroup.h"
 #include "raiselowerhelper.h"
-#include "resizemapobject.h"
 #include "session.h"
 #include "templatemanager.h"
 #include "tile.h"
@@ -363,10 +362,10 @@ void AbstractObjectTool::resetTileSize()
         if (!isResizedTileObject(mapObject))
             continue;
 
-        commands << new ResizeMapObject(mapDocument(),
+        commands << new ChangeMapObject(mapDocument(),
                                         mapObject,
-                                        mapObject->cell().tile()->size(),
-                                        mapObject->size());
+                                        MapObject::SizeProperty,
+                                        mapObject->cell().tile()->size());
     }
 
     if (!commands.isEmpty()) {
@@ -401,7 +400,7 @@ void AbstractObjectTool::convertRectanglesToPolygons()
 
         commands << new ChangePolygon(mapDocument(),
                                       mapObject,
-                                      polygon, mapObject->polygon());
+                                      polygon);
     }
 
     if (!commands.isEmpty()) {

src/tiled/changepolygon.cpp
@@ -32,22 +32,10 @@ using namespace Tiled;
 
 ChangePolygon::ChangePolygon(Document *document,
                              MapObject *mapObject,
-                             const QPolygonF &oldPolygon)
-    : ChangePolygon(document,
-                    mapObject,
-                    mapObject->polygon(),
-                    oldPolygon)
-{
-    setText(QCoreApplication::translate("Undo Commands", "Change Polygon"));
-}
-
-ChangePolygon::ChangePolygon(Document *document,
-                             MapObject *mapObject,
-                             const QPolygonF &newPolygon,
-                             const QPolygonF &oldPolygon)
+                             const QPolygonF &newPolygon)
     : mDocument(document)
     , mMapObject(mapObject)
-    , mOldPolygon(oldPolygon)
+    , mOldPolygon(mapObject->polygon())
     , mNewPolygon(newPolygon)
     , mOldChangeState(mapObject->propertyChanged(MapObject::ShapeProperty))
 {

src/tiled/changepolygon.h
@@ -40,18 +40,9 @@ class MapDocument;
 class ChangePolygon : public QUndoCommand
 {
 public:
-    /**
-     * This constructor expects the polygon to be already changed, and needs
-     * the \a oldPolygon.
-     */
     ChangePolygon(Document *document,
                   MapObject *mapObject,
-                  const QPolygonF &oldPolygon);
-
-    ChangePolygon(Document *document,
-                  MapObject *mapObject,
-                  const QPolygonF &newPolygon,
-                  const QPolygonF &oldPolygon);
+                  const QPolygonF &newPolygon);
 
     void undo() override;
     void redo() override;

src/tiled/changevalue.h
@@ -20,6 +20,8 @@
 
 #pragma once
 
+#include "undocommands.h"
+
 #include <QList>
 #include <QVector>
 #include <QUndoCommand>
@@ -61,20 +63,34 @@ public:
         Q_ASSERT(mObjects.size() == mValues.size());
     }
 
-    void undo() final { setValues(std::exchange(mValues, getValues())); }
-    void redo() final { setValues(std::exchange(mValues, getValues())); }
-
-    bool mergeWith(const QUndoCommand *other) final
+    void undo() override
     {
-        // If the same property is changed of the same layer, the commands can
-        // be trivially merged. The value is already changed on the layer, and
-        // the old value already remembered on this undo command.
+        setValues(std::exchange(mValues, getValues()));
+        QUndoCommand::undo();   // undo child commands
+    }
+
+    void redo() override
+    {
+        QUndoCommand::redo();   // redo child commands
+        setValues(std::exchange(mValues, getValues()));
+    }
+
+    bool mergeWith(const QUndoCommand *other) override
+    {
         auto o = static_cast<const ChangeValue*>(other);
-        if (mDocument == o->mDocument && mObjects == o->mObjects) {
-            setObsolete(getValues() == mValues);
-            return true;
-        }
-        return false;
+        if (mDocument != o->mDocument || mObjects != o->mObjects)
+            return false;
+
+        // In case of child undo commands, we can only merge when all children
+        // can be cloned.
+        if (!cloneChildren(other, this))
+            return false;
+
+        // If the same property is changed on the same objects, the commands
+        // can be trivially merged. The value is already changed on the objects,
+        // and the old value already remembered on this undo command.
+        setObsolete(childCount() == 0 && getValues() == mValues);
+        return true;
     }
 
 protected:

src/tiled/createpolygonobjecttool.cpp
@@ -296,7 +296,7 @@ void CreatePolygonObjectTool::applySegment()
             } else {
                 mapDocument()->undoStack()->push(new ChangePolygon(mapDocument(),
                                                                    newObject,
-                                                                   newPolygon, newObject->polygon()));
+                                                                   newPolygon));
                 finishExtendingMapObject();
             }
 
@@ -307,7 +307,7 @@ void CreatePolygonObjectTool::applySegment()
         }
     }
 
-    QPolygonF current = newObject->polygon();
+    const QPolygonF current = newObject->polygon();
     QPolygonF next = mOverlayPolygonObject->polygon();
 
     // Ignore press when the mouse is still in the same place
@@ -329,7 +329,7 @@ void CreatePolygonObjectTool::applySegment()
     } else {
         mapDocument()->undoStack()->push(new ChangePolygon(mapDocument(),
                                                            newObject,
-                                                           next, current));
+                                                           next));
     }
 
     // Add a new editable point to the overlay

src/tiled/editablemapobject.cpp
@@ -26,7 +26,6 @@
 #include "editablemap.h"
 #include "editableobjectgroup.h"
 #include "editabletile.h"
-#include "movemapobject.h"
 #include "scriptmanager.h"
 
 #include <QCoreApplication>
@@ -154,13 +153,7 @@ void EditableMapObject::setType(QString type)
 
 void EditableMapObject::setPos(QPointF pos)
 {
-    if (Document *doc = document()) {
-        asset()->push(new MoveMapObject(doc, mapObject(),
-                                        pos, mapObject()->position()));
-    } else if (!checkReadOnly()) {
-        mapObject()->setPosition(pos);
-        mapObject()->setPropertyChanged(MapObject::PositionProperty);
-    }
+    setMapObjectProperty(MapObject::PositionProperty, pos);
 }
 
 void EditableMapObject::setSize(QSizeF size)
@@ -202,10 +195,7 @@ void EditableMapObject::setPolygon(QJSValue polygonValue)
     }
 
     if (Document *doc = document()) {
-        asset()->push(new ChangePolygon(doc,
-                                        mapObject(),
-                                        polygon,
-                                        mapObject()->polygon()));
+        asset()->push(new ChangePolygon(doc, mapObject(), polygon));
     } else if (!checkReadOnly()) {
         mapObject()->setPolygon(polygon);
         mapObject()->setPropertyChanged(MapObject::ShapeProperty);

src/tiled/editpolygontool.cpp
@@ -40,6 +40,7 @@
 #include "selectionrectangle.h"
 #include "snaphelper.h"
 #include "toolmanager.h"
+#include "transformmapobjects.h"
 #include "utils.h"
 
 #include <QApplication>
@@ -51,6 +52,28 @@
 #include <cstdlib>
 
 using namespace Tiled;
+
+static TransformMapObjects *createChangePolygonsCommand(Document *document,
+                                                        const QHash<MapObject*, QPolygonF> &newPolygons)
+{
+    QList<MapObject*> mapObjects;
+    mapObjects.reserve(newPolygons.size());
+
+    QVector<TransformState> states;
+    states.reserve(newPolygons.size());
+
+    QHashIterator<MapObject*, QPolygonF> it(newPolygons);
+    while (it.hasNext()) {
+        it.next();
+
+        MapObject *object = it.key();
+        mapObjects.append(object);
+        states.append(TransformState { object });
+        states.last().setPolygon(it.value());
+    }
+
+    return new TransformMapObjects(document, mapObjects, states);
+}
 
 EditPolygonTool::EditPolygonTool(QObject *parent)
     : AbstractObjectTool("EditPolygonTool",
@@ -95,7 +118,7 @@ void EditPolygonTool::deactivate(MapScene *scene)
     disconnect(scene, &MapScene::parallaxParametersChanged,
                this, &EditPolygonTool::updateHandles);
 
-    abortCurrentAction();
+    abortCurrentAction(Deactivated);
 
     // Delete all handles
     QHashIterator<MapObject*, QList<PointHandle*> > i(mHandles);
@@ -117,7 +140,7 @@ void EditPolygonTool::keyPressed(QKeyEvent *event)
     case Qt::Key_Escape:
         if (mAction != NoAction) {
             // Abort the current action if any is being performed
-            abortCurrentAction();
+            abortCurrentAction(UserInteraction);
         } else if (!mSelectedHandles.isEmpty()) {
             // Clear the handle selection if there is one
             setSelectedHandles(QSet<PointHandle*>());
@@ -279,7 +302,7 @@ void EditPolygonTool::mouseReleased(QGraphicsSceneMouseEvent *event)
         mAction = NoAction;
         break;
     case Moving:
-        finishMoving(event->scenePos());
+        finishMoving();
         break;
     }
 
@@ -296,14 +319,12 @@ void EditPolygonTool::mouseDoubleClicked(QGraphicsSceneMouseEvent *event)
 
     if (mAction == NoAction && mClickedSegment) {
         // Split the segment at the location nearest to the mouse
-        QPolygonF oldPolygon = mClickedSegment.object->polygon();
-        QPolygonF newPolygon = oldPolygon;
+        QPolygonF newPolygon = mClickedSegment.object->polygon();
         newPolygon.insert(mClickedSegment.index + 1, mClickedSegment.nearestPointOnLine);
 
         auto splitSegment = new ChangePolygon(mapDocument(),
                                               mClickedSegment.object,
-                                              newPolygon,
-                                              oldPolygon);
+                                              newPolygon);
         splitSegment->setText(tr("Split Segment"));
 
         mapDocument()->undoStack()->push(splitSegment);
@@ -432,13 +453,12 @@ void EditPolygonTool::updateHandles()
 void EditPolygonTool::objectsAboutToBeRemoved(const QList<MapObject *> &objects)
 {
     if (mAction == Moving) {
-        // Make sure we're not going to try to still change these objects when
-        // finishing the move operation.
+        // Make sure we're not going to try to still change these objects.
         // TODO: In addition to avoiding crashes, it would also be good to
         // disallow other actions while moving.
         for (MapObject *object : objects) {
             if (mOldPolygons.contains(object)) {
-                abortCurrentAction(objects);
+                abortCurrentAction(ObjectsRemoved);
                 break;
             }
         }
@@ -540,6 +560,8 @@ void EditPolygonTool::updateMovingItems(const QPointF &pos,
         diff = renderer->pixelToScreenCoords(newAlignPixelPos) - alignScreenPos;
     }
 
+    QHash<MapObject*, QPolygonF> newPolygons = mOldPolygons;
+
     int i = 0;
     for (PointHandle *handle : qAsConst(mSelectedHandles)) {
         // update handle position
@@ -554,41 +576,31 @@ void EditPolygonTool::updateMovingItems(const QPointF &pos,
         QPointF newPixelPos = renderer->screenToPixelCoords(newScreenPos);
 
         // update the polygon
-        QPolygonF polygon = object->polygon();
+        QPolygonF &polygon = newPolygons[object];
         polygon[handle->pointIndex()] = newPixelPos - object->position();
-        object->setPolygon(polygon);
-        emit mapDocument()->changed(MapObjectsChangeEvent(object, MapObject::ShapeProperty));
 
         ++i;
     }
+
+    auto command = createChangePolygonsCommand(mapDocument(), newPolygons);
+    if (command->hasAnyChanges()) {
+        command->setText(tr("Move %n Node(s)", "", mSelectedHandles.size()));
+        mapDocument()->undoStack()->push(command);
+    } else {
+        delete command;
+    }
 }
 
-void EditPolygonTool::finishMoving(const QPointF &pos)
+void EditPolygonTool::finishMoving()
 {
     Q_ASSERT(mAction == Moving);
     mAction = NoAction;
-
-    if (mStart == pos || mOldPolygons.isEmpty()) // Move is a no-op
-        return;
-
-    QUndoStack *undoStack = mapDocument()->undoStack();
-    undoStack->beginMacro(tr("Move %n Node(s)", "", mSelectedHandles.size()));
-
-    // TODO: This isn't really optimal. Would be better to have a single undo
-    // command that supports changing multiple map objects.
-    QHashIterator<MapObject*, QPolygonF> i(mOldPolygons);
-    while (i.hasNext()) {
-        i.next();
-        undoStack->push(new ChangePolygon(mapDocument(), i.key(), i.value()));
-    }
-
-    undoStack->endMacro();
 
     mOldHandlePositions.clear();
     mOldPolygons.clear();
 }
 
-void EditPolygonTool::abortCurrentAction(const QList<MapObject *> &removedObjects)
+void EditPolygonTool::abortCurrentAction(AbortReason reason)
 {
     switch (mAction) {
     case NoAction:
@@ -597,21 +609,15 @@ void EditPolygonTool::abortCurrentAction(const QList<MapObject *> &removedObject
         mapScene()->removeItem(mSelectionRectangle.get());
         break;
     case Moving:
-        // Reset the polygons
-        QHashIterator<MapObject*, QPolygonF> i(mOldPolygons);
-        while (i.hasNext()) {
-            i.next();
+        if (reason == UserInteraction) {
+            auto command = createChangePolygonsCommand(mapDocument(), mOldPolygons);
+            if (command->hasAnyChanges())
+                mapDocument()->undoStack()->push(command);
+            else
+                delete command;
 
-            MapObject *object = i.key();
-            const QPolygonF &oldPolygon = i.value();
-
-            object->setPolygon(oldPolygon);
-
-            if (!removedObjects.contains(object))
-                emit mapDocument()->changed(MapObjectsChangeEvent(object, MapObject::ShapeProperty));
+            mOldPolygons.clear();
         }
-
-        mOldPolygons.clear();
         break;
     }
 
@@ -731,8 +737,7 @@ void EditPolygonTool::deleteNodes()
         MapObject *object = i.next().key();
         const RangeSet<int> &indexRanges = i.value();
 
-        QPolygonF oldPolygon = object->polygon();
-        QPolygonF newPolygon = oldPolygon;
+        QPolygonF newPolygon = object->polygon();
 
         // Remove points, back to front to keep the indexes valid
         RangeSet<int>::Range it = indexRanges.end();
@@ -748,8 +753,7 @@ void EditPolygonTool::deleteNodes()
             undoStack->push(new RemoveMapObjects(mapDocument(), object));
         } else {
             undoStack->push(new ChangePolygon(mapDocument(), object,
-                                              newPolygon,
-                                              oldPolygon));
+                                              newPolygon));
         }
     }
 
@@ -924,19 +928,18 @@ void EditPolygonTool::joinNodes()
         const RangeSet<int> &indexRanges = i.value();
 
         const bool closed = object->shape() == MapObject::Polygon;
-        QPolygonF oldPolygon = object->polygon();
-        QPolygonF newPolygon = joinPolygonNodes(oldPolygon, indexRanges,
+        QPolygonF newPolygon = joinPolygonNodes(object->polygon(),
+                                                indexRanges,
                                                 closed);
 
-        if (newPolygon.size() < oldPolygon.size()) {
+        if (newPolygon.size() < object->polygon().size()) {
             if (!macroStarted) {
                 undoStack->beginMacro(tr("Join Nodes"));
                 macroStarted = true;
             }
 
             undoStack->push(new ChangePolygon(mapDocument(), object,
-                                              newPolygon,
-                                              oldPolygon));
+                                              newPolygon));
         }
     }
 
@@ -960,19 +963,18 @@ void EditPolygonTool::splitSegments()
         const RangeSet<int> &indexRanges = i.value();
 
         const bool closed = object->shape() == MapObject::Polygon;
-        QPolygonF oldPolygon = object->polygon();
-        QPolygonF newPolygon = splitPolygonSegments(oldPolygon, indexRanges,
+        QPolygonF newPolygon = splitPolygonSegments(object->polygon(),
+                                                    indexRanges,
                                                     closed);
 
-        if (newPolygon.size() > oldPolygon.size()) {
+        if (newPolygon.size() > object->polygon().size()) {
             if (!macroStarted) {
                 undoStack->beginMacro(tr("Split Segments"));
                 macroStarted = true;
             }
 
             undoStack->push(new ChangePolygon(mapDocument(), object,
-                                              newPolygon,
-                                              oldPolygon));
+                                              newPolygon));
         }
     }
 
@@ -1017,7 +1019,7 @@ void EditPolygonTool::deleteSegment()
             mapDocument()->undoStack()->push(new SplitPolyline(mapDocument(), mapObject, minIndex));
         }
     } else {
-        QPolygonF polygon = mapObject->polygon();
+        const QPolygonF &polygon = mapObject->polygon();
         QPolygonF newPolygon(polygon);
 
         int indexDifference = std::abs(firstHandle->pointIndex() - secondHandle->pointIndex());
@@ -1034,7 +1036,7 @@ void EditPolygonTool::deleteSegment()
         setSelectedHandles(QSet<PointHandle*>());
 
         mapDocument()->undoStack()->beginMacro(tr("Delete Segment"));
-        mapDocument()->undoStack()->push(new ChangePolygon(mapDocument(), mapObject, newPolygon, polygon));
+        mapDocument()->undoStack()->push(new ChangePolygon(mapDocument(), mapObject, newPolygon));
         mapDocument()->undoStack()->push(new ChangeMapObject(mapDocument(), mapObject,
                                                              MapObject::ShapeProperty, MapObject::Polyline));
         mapDocument()->undoStack()->endMacro();

commit e67282f8215b9af235753aca9fbe23fe00829236

Fixed Rotate Left / Right actions for objects
Regression in 517fb0fb9ee53ae77b6ab05cf7a1a9c8f38dcbd3.

src/tiled/mapdocument.cpp
@@ -476,8 +476,7 @@ void MapDocument::rotateSelectedObjects(RotateDirection direction)
         state.setRotation(newRotation);
     }
 
-    auto command = new TransformMapObjects(this, mSelectedObjects, states);
-    command->setText(tr("Rotate %n Object(s)", "", mSelectedObjects.size()));
+    undoStack()->push(new TransformMapObjects(this, mSelectedObjects, states));
 }
 
 /**