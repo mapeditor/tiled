mapeditor/tiled

commit b81264e66a3712827d4a6d8a6e0412761b43a1ca

Don't scale point objects with the zoom level
Implemented by ignoring viewport transformations for these objects,
which means point objects are still scaled when exporting a map as an
image.

Closes #3356

src/tiled/mapview.cpp
@@ -467,6 +467,9 @@ void MapView::paintEvent(QPaintEvent *event)
             fitMapInView();
     }
 
+    if (mMapDocument)
+        mMapDocument->renderer()->setPainterScale(scale());
+
     QGraphicsView::paintEvent(event);
 }
 

src/tiled/abstractobjecttool.cpp
@@ -41,6 +41,7 @@
 #include "utils.h"
 
 #include <QFileDialog>
+#include <QGraphicsView>
 #include <QKeyEvent>
 #include <QMenu>
 #include <QMessageBox>
@@ -241,7 +242,11 @@ ObjectGroup *AbstractObjectTool::currentObjectGroup() const
 
 QList<MapObject*> AbstractObjectTool::mapObjectsAt(const QPointF &pos) const
 {
-    const QList<QGraphicsItem *> &items = mapScene()->items(pos);
+    const QTransform viewTransform = mapScene()->views().first()->transform();
+    const QList<QGraphicsItem *> items = mapScene()->items(pos,
+                                                           Qt::IntersectsItemShape,
+                                                           Qt::DescendingOrder,
+                                                           viewTransform);
 
     QList<MapObject*> objectList;
 
@@ -260,7 +265,11 @@ QList<MapObject*> AbstractObjectTool::mapObjectsAt(const QPointF &pos) const
 
 MapObject *AbstractObjectTool::topMostMapObjectAt(const QPointF &pos) const
 {
-    const QList<QGraphicsItem *> &items = mapScene()->items(pos);
+    const QTransform viewTransform = mapScene()->views().first()->transform();
+    const QList<QGraphicsItem *> items = mapScene()->items(pos,
+                                                           Qt::IntersectsItemShape,
+                                                           Qt::DescendingOrder,
+                                                           viewTransform);
     const SelectionBehavior behavior = selectionBehavior();
 
     MapObject *topMost = nullptr;

src/tiled/mapobjectitem.cpp
@@ -27,12 +27,10 @@
 #include "mapobject.h"
 #include "maprenderer.h"
 #include "mapscene.h"
-#include "mapview.h"
 #include "objectgroup.h"
 #include "objectgroupitem.h"
 #include "tile.h"
 #include "utils.h"
-#include "zoomable.h"
 
 #include <QPainter>
 
@@ -99,6 +97,8 @@ void MapObjectItem::syncWithMapObject()
     }
 
     setVisible(mObject->isVisible());
+    setFlag(QGraphicsItem::ItemIgnoresTransformations,
+            mObject->shape() == MapObject::Point);
 }
 
 void MapObjectItem::setIsHoverIndicator(bool isHoverIndicator)
@@ -135,26 +135,29 @@ QPainterPath MapObjectItem::shape() const
 
 void MapObjectItem::paint(QPainter *painter,
                           const QStyleOptionGraphicsItem *,
-                          QWidget *widget)
+                          QWidget *)
 {
-    const qreal scale = static_cast<MapView*>(widget->parent())->zoomable()->scale();
+    const auto renderer = mMapDocument->renderer();
+    const qreal painterScale = renderer->painterScale();
     const QColor color = mIsHoveredIndicator ? mColor.lighter() : mColor;
     const qreal previousOpacity = painter->opacity();
+
+    if (flags() & QGraphicsItem::ItemIgnoresTransformations)
+        renderer->setPainterScale(1);
 
     if (mIsHoveredIndicator)
         painter->setOpacity(0.4);
 
     painter->translate(-pos());
-    mMapDocument->renderer()->setPainterScale(scale);
-    mMapDocument->renderer()->drawMapObject(painter, mObject, color);
+    renderer->drawMapObject(painter, mObject, color);
     painter->translate(pos());
 
     if (mIsHoveredIndicator) {
         painter->setOpacity(0.6);
 
         // TODO: Code mostly duplicated in MapObjectOutline
-        const QPointF pixelPos = mMapDocument->renderer()->pixelToScreenCoords(mObject->position());
-        QRectF bounds = mObject->screenBounds(*mMapDocument->renderer());
+        const QPointF pixelPos = renderer->pixelToScreenCoords(mObject->position());
+        QRectF bounds = mObject->screenBounds(*renderer);
         bounds.translate(-pixelPos);
 
         const QLineF lines[4] = {
@@ -183,6 +186,8 @@ void MapObjectItem::paint(QPainter *painter,
 
         painter->setOpacity(previousOpacity);
     }
+
+    renderer->setPainterScale(painterScale);
 }
 
 void MapObjectItem::setPolygon(const QPolygonF &polygon)

src/tiled/objectselectionitem.cpp
@@ -110,6 +110,8 @@ void MapObjectOutline::syncWithMapObject(const MapRenderer &renderer)
 
     setPos(pixelPos);
     setRotation(mObject->rotation());
+    setFlag(QGraphicsItem::ItemIgnoresTransformations,
+            mObject->shape() == MapObject::Point);
 
     if (mBoundingRect != bounds) {
         prepareGeometryChange();

src/tiled/objectselectiontool.cpp
@@ -1166,7 +1166,11 @@ QList<MapObject*> ObjectSelectionTool::objectsAboutToBeSelected(const QPointF &p
                                                                : Qt::ContainsItemShape;
     }
 
-    const QList<QGraphicsItem *> &items = mapScene()->items(rect, selectionMode);
+    const QTransform viewTransform = mapScene()->views().first()->transform();
+    const QList<QGraphicsItem *> items = mapScene()->items(rect,
+                                                           selectionMode,
+                                                           Qt::DescendingOrder,
+                                                           viewTransform);
     for (QGraphicsItem *item : items) {
         if (!item->isEnabled())
             continue;

src/tiled/tilelayeritem.cpp
@@ -23,11 +23,8 @@
 #include "map.h"
 #include "mapdocument.h"
 #include "maprenderer.h"
-#include "mapview.h"
 #include "tile.h"
-#include "zoomable.h"
 
-#include <QPainter>
 #include <QStyleOptionGraphicsItem>
 
 using namespace Tiled;
@@ -68,12 +65,9 @@ QRectF TileLayerItem::boundingRect() const
 
 void TileLayerItem::paint(QPainter *painter,
                           const QStyleOptionGraphicsItem *option,
-                          QWidget *widget)
+                          QWidget *)
 {
-    const qreal scale = static_cast<MapView*>(widget->parent())->zoomable()->scale();
-
     MapRenderer *renderer = mMapDocument->renderer();
-    renderer->setPainterScale(scale);
     // TODO: Display a border around the layer when selected
     renderer->drawTileLayer(painter, tileLayer(), option->exposedRect);
 }

commit 45701374af529aaf54209f8cfbebc12871d2c294

Fixed shadow offset for other maps in a world
The painter scale is used to calculate the needed offset for shadows,
but since it is stored in the MapRenderer, we need to make sure this
value is updated for all maps displayed in a world.

This was a regression in b81264e66a3712827d4a6d, before which the scale
was being set by each tile layer item and object item.

Closes #3429

src/tiled/mapscene.cpp
@@ -136,6 +136,16 @@ void MapScene::setParallaxEnabled(bool enabled)
 }
 
 /**
+ * Sets the painter scale on the MapRenderer instances for all MapItem
+ * instances.
+ */
+void MapScene::setPainterScale(qreal painterScale)
+{
+    for (auto mapItem : qAsConst(mMapItems))
+        mapItem->mapDocument()->renderer()->setPainterScale(painterScale);
+}
+
+/**
  * Returns the bounding rect of the map. This can be different from the
  * sceneRect() when multiple maps are displayed.
  */

src/tiled/mapscene.h
@@ -64,6 +64,7 @@ public:
 
     void setShowTileCollisionShapes(bool enabled);
     void setParallaxEnabled(bool enabled);
+    void setPainterScale(qreal painterScale);
 
     QRectF mapBoundingRect() const;
 

src/tiled/mapview.cpp
@@ -467,8 +467,8 @@ void MapView::paintEvent(QPaintEvent *event)
             fitMapInView();
     }
 
-    if (mMapDocument)
-        mMapDocument->renderer()->setPainterScale(scale());
+    if (auto scene = mapScene())
+        scene->setPainterScale(scale());
 
     QGraphicsView::paintEvent(event);
 }