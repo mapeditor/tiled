ef74b238bb6368da70c4f6d79a496802195a5ead
dogboydog <dogboydog@users.noreply.github.com>
2022-09-22 09:45:53 -0400

    Update index.d.ts

--- docs/scripting-doc/index.d.ts
+++ docs/scripting-doc/index.d.ts
@@ -472,7 +472,7 @@ declare namespace Qt {
     /**
      * Signal emitted when the user selects a different option. Provides the index
      * into the list of values for the new selection.
-     * Warning: Most builds of Tiled use Qt5, where this signal is bugged and index is the chosen text rather than the index number.
+     * @warning Most builds of Tiled use Qt5, where this signal is bugged and index is the chosen text rather than the index number.
      *          When using this event, instead of using index, you can get the updated index using QtComboBox.currentIndex.
      */
     currentIndexChanged: Signal<number>;
