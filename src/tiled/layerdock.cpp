a41b3c55712b1a12e0bcecd47ce831268c2681a9
Logan Higinbotham <lgh8@pitt.edu>
2022-10-24 21:18:57 -0400

    Add "Select All" to context menu in layer view
    	- fixes the second request made in #3444

--- src/tiled/layerdock.cpp
+++ src/tiled/layerdock.cpp
@@ -453,6 +453,12 @@ void LayerView::contextMenuEvent(QContextMenuEvent *event)
         menu.addAction(handler->actionMergeLayersDown());
         menu.addAction(handler->actionRemoveLayers());
         menu.addSeparator();
+    }
+    
+    // independent of whether index is valid but maintaining positioning
+    menu.addAction(handler->actionSelectAllLayers());
+
+    if (proxyIndex.isValid()) {
         menu.addAction(handler->actionMoveLayersUp());
         menu.addAction(handler->actionMoveLayersDown());
         menu.addSeparator();
