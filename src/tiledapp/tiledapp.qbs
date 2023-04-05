mapeditor/tiled

commit 16229d03c3999e1107c6ec605270b9243a494531

Qbs: Use the texttemplate module for the tiled.rc file

src/tiledapp/tiledapp.qbs
@@ -11,6 +11,7 @@ TiledQtGuiApplication {
     Depends { name: "libtilededitor" }
     Depends { name: "ib"; condition: qbs.targetOS.contains("macos") }
     Depends { name: "Qt.gui-private"; condition: qbs.targetOS.contains("windows") && Qt.core.versionMajor >= 6 }
+    Depends { name: "texttemplate" }
 
     property bool qtcRunnable: true
 
@@ -172,49 +173,19 @@ TiledQtGuiApplication {
     // Generate the tiled.rc file in order to dynamically specify the version
     Group {
         name: "RC file (Windows)"
+        condition: qbs.targetOS.contains("windows")
         files: [ "tiled.rc.in" ]
-        fileTags: ["rcIn"]
-    }
-    Rule {
-        inputs: ["rcIn"]
-        Artifact {
-            filePath: {
-                var destdir = FileInfo.joinPaths(product.moduleProperty("Qt.core",
-                                                         "generatedFilesDir"), input.fileName);
-                return destdir.replace(/\.[^\.]*$/,'')
-            }
-            fileTags: "rc"
-        }
-        prepare: {
-            var cmd = new JavaScriptCommand();
-            cmd.description = "prepare " + FileInfo.fileName(output.filePath);
-            cmd.highlight = "codegen";
 
-            cmd.sourceCode = function() {
-                var i;
-                var vars = {};
-                var inf = new TextFile(input.filePath);
-                var all = inf.readAll();
+        texttemplate.outputTag: "rc"
+        texttemplate.dict: {
+            var versionArray = project.version.split(".");
+            if (versionArray.length == 3)
+                versionArray.push("0");
 
-                var versionArray = project.version.split(".");
-                if (versionArray.length == 3)
-                    versionArray.push("0");
-
-                // replace vars
-                vars['VERSION'] = project.version;
-                vars['VERSION_CSV'] = versionArray.join(",");
-
-                for (i in vars) {
-                    all = all.replace(new RegExp('@' + i + '@(?!\w)', 'g'), vars[i]);
-                }
-
-                var file = new TextFile(output.filePath, TextFile.WriteOnly);
-                file.truncate();
-                file.write(all);
-                file.close();
-            }
-
-            return cmd;
+            return {
+                version: project.version,
+                version_csv: versionArray.join(",")
+            };
         }
     }
 }

src/tiledapp/tiled.rc.in
@@ -4,7 +4,7 @@ IDI_ICON1               ICON    DISCARDABLE     "images/tiled-icon.ico"
 
 1 VERSIONINFO
  FILEVERSION     1,0,0,0
- PRODUCTVERSION  @VERSION_CSV@
+ PRODUCTVERSION  ${version_csv}
  FILEFLAGSMASK   VS_FFI_FILEFLAGSMASK
  FILEFLAGS       0
  FILEOS          VOS_NT_WINDOWS32
@@ -21,7 +21,7 @@ BEGIN
             VALUE "InternalName", "tiled\0"
             VALUE "OriginalFilename", "tiled.exe\0"
             VALUE "ProductName", "Tiled\0"
-            VALUE "ProductVersion", "@VERSION@\0"
+            VALUE "ProductVersion", "${version}\0"
         END
     END
 

commit 3b2a3c47cb1c8affe59865f671663b7c6ca2e8ef

Fixed icon for tiled.exe
Fixing the path as in 358b6a2d21cdcf12b006706d4 was not enough, because
the icon had already broken in 16229d03c3999e1107c6ec605270b9243a4945.

It appears the texttemplate module properties were ineffective in the
Group item, but it failed to generate an error.

src/tiledapp/tiledapp.qbs
@@ -11,7 +11,7 @@ TiledQtGuiApplication {
     Depends { name: "libtilededitor" }
     Depends { name: "ib"; condition: qbs.targetOS.contains("macos") }
     Depends { name: "Qt.gui-private"; condition: qbs.targetOS.contains("windows") && Qt.core.versionMajor >= 6 }
-    Depends { name: "texttemplate" }
+    Depends { name: "texttemplate"; condition: qbs.targetOS.contains("windows") }
 
     property bool qtcRunnable: true
 
@@ -45,6 +45,22 @@ TiledQtGuiApplication {
         bundle.identifierPrefix: "org.mapeditor"
         ib.appIconName: "tiled-icon-mac"
         targetName: "Tiled"
+    }
+
+    Properties {
+        condition: qbs.targetOS.contains("windows")
+
+        texttemplate.outputTag: "rc"
+        texttemplate.dict: {
+            var versionArray = project.version.split(".");
+            if (versionArray.length == 3)
+                versionArray.push("0");
+
+            return {
+                version: project.version,
+                version_csv: versionArray.join(",")
+            };
+        }
     }
 
     Group {
@@ -175,17 +191,5 @@ TiledQtGuiApplication {
         name: "RC file (Windows)"
         condition: qbs.targetOS.contains("windows")
         files: [ "tiled.rc.in" ]
-
-        texttemplate.outputTag: "rc"
-        texttemplate.dict: {
-            var versionArray = project.version.split(".");
-            if (versionArray.length == 3)
-                versionArray.push("0");
-
-            return {
-                version: project.version,
-                version_csv: versionArray.join(",")
-            };
-        }
     }
 }