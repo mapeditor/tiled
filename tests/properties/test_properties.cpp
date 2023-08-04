mapeditor/tiled

commit d8b6b111ea6315c8fed9408227f65182c4bd52ef

Fixed serialization of nested custom property types
Rearranged the conversion to and from "property value" to "export
value", such that the original custom class information can be used when
de-serializing the members of that class. This was necessary to
correctly load nested custom classes and enums, because the custom type
is not saved out for member values.

It should also fix cases where the type is lost. In the JSON format, the
information about whether a property value is a path or an object
reference is not saved on member values of custom classes.

src/libtiled/properties.cpp
@@ -175,7 +175,7 @@ static int nameToType(const QString &name)
     if (name == QLatin1String("object"))
         return objectRefTypeId();
     if (name == QLatin1String("class"))
-        return propertyValueId();
+        return QMetaType::QVariantMap;
 
     return QVariant::nameToType(name.toLatin1().constData());
 }
@@ -214,8 +214,7 @@ ExportValue ExportContext::toExportValue(const QVariant &value) const
         const PropertyValue propertyValue = value.value<PropertyValue>();
 
         if (const PropertyType *propertyType = mTypes.findTypeById(propertyValue.typeId)) {
-            exportValue = toExportValue(propertyType->unwrap(propertyValue.value));
-            exportValue.propertyTypeName = propertyType->name;
+            exportValue = propertyType->toExportValue(propertyValue.value, *this);
         } else {
             // the type may have been deleted
             exportValue = toExportValue(propertyValue.value);
@@ -243,26 +242,35 @@ ExportValue ExportContext::toExportValue(const QVariant &value) const
 
 QVariant ExportContext::toPropertyValue(const ExportValue &exportValue) const
 {
-    QVariant propertyValue = exportValue.value;
     const int metaType = nameToType(exportValue.typeName);
-
-    if (metaType == filePathTypeId()) {
-        const QUrl url = toUrl(exportValue.value.toString(), mPath);
-        propertyValue = QVariant::fromValue(FilePath { url });
-    } else if (metaType == objectRefTypeId()) {
-        propertyValue = QVariant::fromValue(ObjectRef::fromInt(exportValue.value.toInt()));
-    } else if (metaType == propertyValueId()) {
-        // should be covered by property type below
-    } else if (exportValue.value.userType() != metaType && metaType != QMetaType::UnknownType) {
-        propertyValue.convert(metaType);
-    }
+    QVariant propertyValue = toPropertyValue(exportValue.value, metaType);
 
     // Wrap the value in its custom property type when applicable
     if (!exportValue.propertyTypeName.isEmpty())
         if (const PropertyType *propertyType = mTypes.findTypeByName(exportValue.propertyTypeName))
-            propertyValue = propertyType->wrap(propertyValue);
+            propertyValue = propertyType->toPropertyValue(propertyValue, *this);
 
     return propertyValue;
+}
+
+QVariant ExportContext::toPropertyValue(const QVariant &value, int metaType) const
+{
+    if (metaType == QMetaType::UnknownType || value.userType() == metaType)
+        return value;   // value possibly already converted
+
+    if (metaType == QMetaType::QVariantMap || metaType == propertyValueId())
+        return value;   // should be covered by property type
+
+    if (metaType == filePathTypeId()) {
+        const QUrl url = toUrl(value.toString(), mPath);
+        return QVariant::fromValue(FilePath { url });
+    }
+
+    if (metaType == objectRefTypeId())
+        return QVariant::fromValue(ObjectRef::fromInt(value.toInt()));
+
+    QVariant convertedValue = value;
+    return convertedValue.convert(metaType) ? convertedValue : value;
 }
 
 void initializeMetatypes()

src/libtiled/properties.h
@@ -78,14 +78,6 @@ public:
     static ObjectRef fromInt(int id) { return ObjectRef { id }; }
 };
 
-class TILEDSHARED_EXPORT ExportValue
-{
-public:
-    QVariant value;
-    QString typeName;
-    QString propertyTypeName;
-};
-
 class TILEDSHARED_EXPORT ExportContext
 {
 public:
@@ -98,10 +90,12 @@ public:
     // need to prevent this one since we're only holding a reference to the types
     ExportContext(const PropertyTypes &&types, const QString &path) = delete;
 
+    const PropertyTypes &types() const { return mTypes; }
     const QString &path() const { return mPath; }
 
     ExportValue toExportValue(const QVariant &value) const;
     QVariant toPropertyValue(const ExportValue &exportValue) const;
+    QVariant toPropertyValue(const QVariant &value, int metaType) const;
 
 private:
     const PropertyTypes &mTypes;
@@ -112,14 +106,11 @@ class TILEDSHARED_EXPORT AggregatedPropertyData
 {
 public:
     AggregatedPropertyData()
-        : mPresenceCount(0)
-        , mValueConsistent(true)
     {}
 
     explicit AggregatedPropertyData(const QVariant &value)
         : mValue(value)
         , mPresenceCount(1)
-        , mValueConsistent(true)
     {}
 
     void aggregate(const QVariant &value)
@@ -141,8 +132,8 @@ public:
 
 private:
     QVariant mValue;
-    int mPresenceCount;
-    bool mValueConsistent;
+    int mPresenceCount = 0;
+    bool mValueConsistent = true;
 };
 
 /**

src/libtiled/propertytype.cpp
@@ -33,6 +33,8 @@
 
 #include <QVector>
 
+#include <algorithm>
+
 namespace Tiled {
 
 int PropertyType::nextId = 0;
@@ -50,11 +52,18 @@ QVariant PropertyType::wrap(const QVariant &value) const
  * This function is called with the value stored in a PropertyValue. It is
  * supposed to prepare the value for saving.
  *
- * The default implementation just returns the value as-is.
+ * The default implementation just calls ExportContext::toExportValue.
  */
-QVariant PropertyType::unwrap(const QVariant &value) const
+ExportValue PropertyType::toExportValue(const QVariant &value, const ExportContext &context) const
 {
-    return value;
+    ExportValue result = context.toExportValue(value);
+    result.propertyTypeName = name;
+    return result;
+}
+
+QVariant PropertyType::toPropertyValue(const QVariant &value, const ExportContext &) const
+{
+    return wrap(value);
 }
 
 QVariantMap PropertyType::toVariant(const ExportContext &) const
@@ -125,7 +134,35 @@ QString PropertyType::typeToString(Type type)
 
 // EnumPropertyType
 
-QVariant EnumPropertyType::wrap(const QVariant &value) const
+ExportValue EnumPropertyType::toExportValue(const QVariant &value, const ExportContext &context) const
+{
+    ExportValue result;
+
+    // Convert enum values to their string if desired
+    if (value.userType() == QMetaType::Int && storageType == StringValue) {
+        const int intValue = value.toInt();
+
+        if (valuesAsFlags) {
+            QString stringValue;
+
+            for (int i = 0; i < values.size(); ++i) {
+                if (intValue & (1 << i)) {
+                    if (!stringValue.isEmpty())
+                        stringValue.append(QLatin1Char(','));
+                    stringValue.append(values.at(i));
+                }
+            }
+
+            return PropertyType::toExportValue(stringValue, context);
+        } else if (intValue >= 0 && intValue < values.size()) {
+            return PropertyType::toExportValue(values.at(intValue), context);
+        }
+    }
+
+    return PropertyType::toExportValue(value, context);
+}
+
+QVariant EnumPropertyType::toPropertyValue(const QVariant &value, const ExportContext &) const
 {
     // Convert enum values stored as string, if possible
     if (value.userType() == QMetaType::QString) {
@@ -148,47 +185,20 @@ QVariant EnumPropertyType::wrap(const QVariant &value) const
                 // In case of any unrecognized flag name we keep the original
                 // string value, to prevent silent data loss.
                 if (index == -1)
-                    return PropertyType::wrap(value);
+                    return wrap(value);
 
                 flags |= 1 << index;
             }
 
-            return PropertyType::wrap(flags);
+            return wrap(flags);
         }
 
         const int index = values.indexOf(stringValue);
         if (index != -1)
-            return PropertyType::wrap(index);
+            return wrap(index);
     }
 
-    return PropertyType::wrap(value);
-}
-
-QVariant EnumPropertyType::unwrap(const QVariant &value) const
-{
-    // Convert enum values to their string if desired
-    if (value.userType() == QMetaType::Int && storageType == StringValue) {
-        const int intValue = value.toInt();
-
-        if (valuesAsFlags) {
-            QString stringValue;
-
-            for (int i = 0; i < values.size(); ++i) {
-                if (intValue & (1 << i)) {
-                    if (!stringValue.isEmpty())
-                        stringValue.append(QLatin1Char(','));
-                    stringValue.append(values.at(i));
-                }
-            }
-
-            return stringValue;
-        }
-
-        if (intValue >= 0 && intValue < values.size())
-            return values.at(intValue);
-    }
-
-    return value;
+    return wrap(value);
 }
 
 QVariant EnumPropertyType::defaultValue() const
@@ -231,6 +241,49 @@ QString EnumPropertyType::storageTypeToString(StorageType type)
 }
 
 // ClassPropertyType
+
+ExportValue ClassPropertyType::toExportValue(const QVariant &value, const ExportContext &context) const
+{
+    ExportValue result;
+    Properties properties = value.toMap();
+
+    QMutableMapIterator<QString, QVariant> it(properties);
+    while (it.hasNext()) {
+        it.next();
+
+        ExportValue exportValue = context.toExportValue(it.value());
+        it.setValue(exportValue.value);
+    }
+
+    return PropertyType::toExportValue(properties, context);
+}
+
+QVariant ClassPropertyType::toPropertyValue(const QVariant &value, const ExportContext &context) const
+{
+    Properties properties = value.toMap();
+
+    QMutableMapIterator<QString, QVariant> it(properties);
+    while (it.hasNext()) {
+        it.next();
+
+        const QVariant classMember = members.value(it.key());
+        if (!classMember.isValid())  // ignore removed members
+            continue;
+
+        QVariant propertyValue = context.toPropertyValue(it.value(), classMember.userType());
+
+        // Wrap the value in its custom property type when applicable
+        if (classMember.userType() == propertyValueId()) {
+            const PropertyValue classMemberValue = classMember.value<PropertyValue>();
+            if (const PropertyType *propertyType = context.types().findTypeById(classMemberValue.typeId))
+                propertyValue = propertyType->toPropertyValue(propertyValue, context);
+        }
+
+        it.setValue(propertyValue);
+    }
+
+    return wrap(properties);
+}
 
 QVariant ClassPropertyType::defaultValue() const
 {
@@ -323,12 +376,9 @@ PropertyTypes::~PropertyTypes()
 
 size_t PropertyTypes::count(PropertyType::Type type) const
 {
-    size_t count = 0;
-    for (const auto propertyType : mTypes) {
-        if (propertyType->type == type)
-            ++count;
-    }
-    return count;
+    return std::count_if(mTypes.begin(), mTypes.end(), [&] (const PropertyType *propertyType) {
+        return propertyType->type == type;
+    });
 }
 
 /**
@@ -337,11 +387,10 @@ size_t PropertyTypes::count(PropertyType::Type type) const
  */
 const PropertyType *PropertyTypes::findTypeById(int typeId) const
 {
-    for (const auto propertyType : mTypes) {
-        if (propertyType->id == typeId)
-            return propertyType;
-    }
-    return nullptr;
+    auto it = std::find_if(mTypes.begin(), mTypes.end(), [&] (const PropertyType *type) {
+        return type->id == typeId;
+    });
+    return it == mTypes.end() ? nullptr : *it;
 }
 
 /**
@@ -350,11 +399,10 @@ const PropertyType *PropertyTypes::findTypeById(int typeId) const
  */
 const PropertyType *PropertyTypes::findTypeByName(const QString &name) const
 {
-    for (const auto propertyType : mTypes) {
-        if (propertyType->name == name)
-            return propertyType;
-    }
-    return nullptr;
+    auto it = std::find_if(mTypes.begin(), mTypes.end(), [&] (const PropertyType *type) {
+        return type->name == name;
+    });
+    return it == mTypes.end() ? nullptr : *it;
 }
 
 void PropertyTypes::loadFrom(const QVariantList &list, const QString &path)

src/libtiled/propertytype.h
@@ -38,11 +38,18 @@
 #include "tiled_global.h"
 
 #include <memory>
-#include <vector>
 
 namespace Tiled {
 
 class ExportContext;
+
+class TILEDSHARED_EXPORT ExportValue
+{
+public:
+    QVariant value;
+    QString typeName;
+    QString propertyTypeName;
+};
 
 /**
  * The base class for custom property types.
@@ -62,8 +69,10 @@ public:
 
     virtual ~PropertyType() = default;
 
-    virtual QVariant wrap(const QVariant &value) const;
-    virtual QVariant unwrap(const QVariant &value) const;
+    QVariant wrap(const QVariant &value) const;
+
+    virtual ExportValue toExportValue(const QVariant &value, const ExportContext &) const;
+    virtual QVariant toPropertyValue(const QVariant &value, const ExportContext &) const;
 
     virtual QVariant defaultValue() const = 0;
 
@@ -97,14 +106,14 @@ public:
         IntValue
     };
 
-    StorageType storageType = StringValue;  // TODO: Allow user to change this
+    StorageType storageType = StringValue;
     QStringList values;
-    bool valuesAsFlags = true;             // TODO: Allow user to change this
+    bool valuesAsFlags = false;
 
     EnumPropertyType(const QString &name) : PropertyType(PT_Enum, name) {}
 
-    QVariant wrap(const QVariant &value) const override;
-    QVariant unwrap(const QVariant &value) const override;
+    ExportValue toExportValue(const QVariant &value, const ExportContext &) const override;
+    QVariant toPropertyValue(const QVariant &value, const ExportContext &) const override;
 
     QVariant defaultValue() const override;
 
@@ -124,6 +133,9 @@ public:
     QVariantMap members;
 
     ClassPropertyType(const QString &name) : PropertyType(PT_Class, name) {}
+
+    ExportValue toExportValue(const QVariant &value, const ExportContext &) const override;
+    QVariant toPropertyValue(const QVariant &value, const ExportContext &) const override;
 
     QVariant defaultValue() const override;
 

commit fc3848166983400a1fe4a0dd2ab9c1b34b053763

Fixed loading of invalid color properties
This was regression in d8b6b111ea6315c8fed9408227f65182c4bd52ef since it
used the return value of QVariant::convert to decide whether to use the
converted value or to keep the original (string) value. This, combined
with the fact that invalid colors are written out as empty strings,
broke the loading of invalid color values.

I seem to have added the check on the return value of convert to prevent
data loss when a conversion to the expected type fails. However, I'm not
entirely sure in which case this was preferable, so this change removes
the check again.

Also added an autotest, which can be adjusted or extended when we might
eventually run into the use-case for checking the "convert" return
value.

In the process, fixed the loadCircularReference test, which seems to
have been broken due to a change of behavior. And fixed the
loadAndSavePropertyTypes test, which broke due to adding "drawFill".

Closes #3793

src/libtiled/properties.cpp
@@ -352,7 +352,8 @@ QVariant ExportContext::toPropertyValue(const QVariant &value, int metaType) con
         return QVariant::fromValue(ObjectRef::fromInt(value.toInt()));
 
     QVariant convertedValue = value;
-    return convertedValue.convert(metaType) ? convertedValue : value;
+    convertedValue.convert(metaType);
+    return convertedValue;
 }
 
 void initializeMetatypes()

tests/properties/test_properties.cpp
@@ -55,6 +55,7 @@ constexpr auto propertyTypesJson = R"([
     },
     {
         "color": "#ffa0a0a4",
+        "drawFill": true,
         "id": 5,
         "members": [
             {
@@ -240,6 +241,9 @@ class test_Properties : public QObject
 private slots:
     void initTestCase();
 
+    void toPropertyValue();
+    void toPropertyValue_data();
+
     void loadAndSavePropertyTypes();
     void loadCircularReference();
     void loadEnumInNestedClass();
@@ -289,6 +293,49 @@ void test_Properties::initTestCase()
     classType.members.insert(QStringLiteral("enumFlagsInt"), flagsIntValue);
 }
 
+void test_Properties::toPropertyValue()
+{
+    QFETCH(QString, value);
+    QFETCH(QString, type);
+    QFETCH(QVariant, expected);
+
+    ExportContext context;
+    ExportValue exportValue;
+    exportValue.value = value;
+    exportValue.typeName = type;
+
+    const auto actual = context.toPropertyValue(exportValue);
+    QCOMPARE(actual, expected);
+}
+
+void test_Properties::toPropertyValue_data()
+{
+    QTest::addColumn<QString>("value");
+    QTest::addColumn<QString>("type");
+    QTest::addColumn<QVariant>("expected");
+
+    QTest::newRow("color") << QStringLiteral("#ff0000ff") << QStringLiteral("color") << QVariant::fromValue(QColor(Qt::blue));
+    QTest::newRow("invalid-color") << QString() << QStringLiteral("color") << QVariant::fromValue(QColor());
+    QTest::newRow("int") << QStringLiteral("42") << QStringLiteral("int") << QVariant::fromValue(42);
+    QTest::newRow("float") << QStringLiteral("42.0") << QStringLiteral("float") << QVariant::fromValue(42.0);
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+    QTest::newRow("invalid-int") << QStringLiteral("foo") << QStringLiteral("int") << QVariant(QMetaType(QMetaType::Int));
+    QTest::newRow("invalid-float") << QStringLiteral("foo") << QStringLiteral("float") << QVariant(QMetaType(QMetaType::Double));
+#else
+    QTest::newRow("invalid-int") << QStringLiteral("foo") << QStringLiteral("int") << QVariant(QVariant::Int);
+    QTest::newRow("invalid-float") << QStringLiteral("foo") << QStringLiteral("float") << QVariant(QVariant::Double);
+#endif
+    QTest::newRow("bool-true") << QStringLiteral("true") << QStringLiteral("bool") << QVariant::fromValue(true);
+    QTest::newRow("bool-false") << QStringLiteral("false") << QStringLiteral("bool") << QVariant::fromValue(false);
+    QTest::newRow("string") << QStringLiteral("foo") << QStringLiteral("string") << QVariant::fromValue(QStringLiteral("foo"));
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+    QTest::newRow("file") << QStringLiteral("/foo") << QStringLiteral("file") << QVariant::fromValue(FilePath { QUrl::fromLocalFile(QStringLiteral("/foo")) });
+#endif
+    QTest::newRow("object") << QStringLiteral("1") << QStringLiteral("object") << QVariant::fromValue(ObjectRef { 1 });
+
+    // todo: test enums and classes, and also add a test for toExportValue
+}
+
 void test_Properties::loadAndSavePropertyTypes()
 {
     QJsonParseError error;
@@ -326,11 +373,11 @@ void test_Properties::loadCircularReference()
     types.loadFromJson(doc.array(), QString());
 
     // Verify the back reference is not present
-    const auto b = types.findPropertyValueType(QStringLiteral("B"));
-    QVERIFY(b);
-    QCOMPARE(b->type, PropertyType::PT_Class);
-    const auto &membersB = static_cast<const ClassPropertyType*>(b)->members;
-    QVERIFY(!membersB.contains(QStringLiteral("a")));
+    const auto a = types.findPropertyValueType(QStringLiteral("A"));
+    QVERIFY(a);
+    QCOMPARE(a->type, PropertyType::PT_Class);
+    const auto &membersA = static_cast<const ClassPropertyType*>(a)->members;
+    QVERIFY(!membersA.contains(QStringLiteral("b")));
 }
 
 void test_Properties::loadEnumInNestedClass()