f66c972323ff91fa738296b2d377514df7b52610
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2023-08-28 17:16:35 +0200

    JSON format: Fixed tile order when loading a tileset using the old format
    Since Tiled 1.7, the order of tiles in the tileset file determines the
    display order. But this should not be the case for tilesets stored in
    the old JSON format (which was replaced in Tiled 1.2), since it used an
    object with the tile IDs as string keys for storing tile meta-data and
    these keys get sorted alphabetically.
    
    This was resulting in tiles with associated data getting displayed in an
    order like "1, 10, 11, 12, 13, ..., 2, 21, ...".

--- src/libtiled/tileset.h
+++ src/libtiled/tileset.h
@@ -228,6 +228,7 @@ public:
     QList<int> relocateTiles(const QList<Tile *> &tiles, int location);
 
     bool anyTileOutOfOrder() const;
+    void resetTileOrder();
 
     void setNextTileId(int nextId);
     int nextTileId() const;
