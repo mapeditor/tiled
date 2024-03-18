mapeditor/tiled

commit fb2390aeb01eb183da8a5adc1970cbef1c1043f9

Small code tweaks here and there
* No need to call QStaticText::prepare, it will be done when needed.
* Removed some superfluous includes.
* Use convenient QProcess::startCommand from Qt 6 onwards.
* Fixed leaking of IssueDelegate (thanks AddressSanitizer).

src/tiled/actionsearch.cpp
@@ -150,7 +150,6 @@ void ActionMatchDelegate::paint(QPainter *painter,
     QStaticText staticText(nameHtml);
     staticText.setTextOption(textOption);
     staticText.setTextFormat(Qt::RichText);
-    staticText.prepare(painter->transform(), fonts.big);
 
     painter->setFont(fonts.big);
     painter->drawStaticText(nameRect.topLeft(), staticText);
@@ -170,7 +169,6 @@ void ActionMatchDelegate::paint(QPainter *painter,
 
         staticText.setTextFormat(Qt::PlainText);
         staticText.setText(shortcutText);
-        staticText.prepare(painter->transform(), fonts.small);
 
         const int centeringMargin = (shortcutRect.height() - smallFontMetrics.height()) / 2;
         painter->setOpacity(0.75);

src/tiled/command.cpp
@@ -298,10 +298,12 @@ CommandProcess::CommandProcess(const Command &command, bool inTerminal, bool sho
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     start(mFinalCommand);
-#else
+#elif QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QStringList args = QProcess::splitCommand(mFinalCommand);
     const QString executable = args.takeFirst();
     start(executable, args);
+#else
+    startCommand(mFinalCommand);
 #endif
 }
 

src/tiled/issuesdock.cpp
@@ -163,7 +163,7 @@ IssuesDock::IssuesDock(QWidget *parent)
 
     mIssuesView->setModel(mProxyModel);
     mIssuesView->setIconSize(Utils::dpiScaled(QSize(16, 16)));
-    mIssuesView->setItemDelegate(new IssueDelegate);
+    mIssuesView->setItemDelegate(new IssueDelegate(this));
     mIssuesView->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
 
     mFilterEdit->setFilteredView(mIssuesView);

src/tiled/locatorwidget.cpp
@@ -173,13 +173,11 @@ void FileMatchDelegate::paint(QPainter *painter,
     QStaticText staticText(fileNameHtml);
     staticText.setTextOption(textOption);
     staticText.setTextFormat(Qt::RichText);
-    staticText.prepare(painter->transform(), fonts.big);
 
     painter->setFont(fonts.big);
     painter->drawStaticText(fileNameRect.topLeft(), staticText);
 
     staticText.setText(filePathHtml);
-    staticText.prepare(painter->transform(), fonts.small);
 
     painter->setOpacity(0.75);
     painter->setFont(fonts.small);

src/tmxrasterizer/tmxrasterizer.cpp
@@ -31,8 +31,6 @@
 #include "imagelayer.h"
 #include "map.h"
 #include "mapformat.h"
-#include "mapreader.h"
-#include "maprenderer.h"
 #include "objectgroup.h"
 #include "tilelayer.h"
 #include "tilesetmanager.h"

src/tmxrasterizer/tmxrasterizer.h
@@ -30,8 +30,8 @@
 
 #include "layer.h"
 
-#include "map.h"
-#include "mapreader.h"
+#include "maprenderer.h"
+
 #include <QString>
 #include <QStringList>
 

commit 4e218f0145a23064df229c7ce6b563f7ef236a36

Fixed alignment of shortcuts in action search
Issue introduced in fb2390aeb01eb183da8a5adc1970cbef1c1043f9.

We do need to call QStaticText::prepare in this case, because we need to
know its width in the small font, before painting the text.

src/tiled/actionsearch.cpp
@@ -154,7 +154,7 @@ void ActionMatchDelegate::paint(QPainter *painter,
     painter->setFont(fonts.big);
     painter->drawStaticText(nameRect.topLeft(), staticText);
 
-    const QIcon icon = index.data(Qt::DecorationRole).value<QIcon>();
+    const auto icon = index.data(Qt::DecorationRole).value<QIcon>();
     if (!icon.isNull()) {
         const auto iconRect = QRect(option.rect.topLeft() + QPoint(margin, margin),
                                     QSize(iconSize, iconSize));
@@ -162,13 +162,14 @@ void ActionMatchDelegate::paint(QPainter *painter,
         icon.paint(painter, iconRect);
     }
 
-    const QKeySequence shortcut = index.data(ActionLocatorSource::ShortcutRole).value<QKeySequence>();
+    const auto shortcut = index.data(ActionLocatorSource::ShortcutRole).value<QKeySequence>();
     if (!shortcut.isEmpty()) {
         const QString shortcutText = shortcut.toString(QKeySequence::NativeText);
         const QFontMetrics smallFontMetrics(fonts.small);
 
         staticText.setTextFormat(Qt::PlainText);
         staticText.setText(shortcutText);
+        staticText.prepare(painter->transform(), fonts.small);
 
         const int centeringMargin = (shortcutRect.height() - smallFontMetrics.height()) / 2;
         painter->setOpacity(0.75);
@@ -242,7 +243,7 @@ QString ActionLocatorSource::placeholderText() const
 
 QVector<ActionLocatorSource::Match> ActionLocatorSource::findActions(const QStringList &words)
 {
-    const QRegularExpression re(QLatin1String("(?<=^|[^&])&"));
+    static const QRegularExpression re(QLatin1String("(?<=^|[^&])&"));
     const QList<Id> actions = ActionManager::actions();
     const Id searchActionsId("SearchActions");
 