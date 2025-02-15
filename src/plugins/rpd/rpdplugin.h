4c2905ff7d16278281cc942d325567e9f604327e
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-02-15 19:25:22 +0300

    Header: license & guards

--- src/plugins/rpd/rpdplugin.h
+++ src/plugins/rpd/rpdplugin.h
@@ -1,5 +1,24 @@
-#ifndef RPDPLUGIN_H
-#define RPDPLUGIN_H
+/*
+ * Remixed Dungeon Tiled Plugin
+ * Copyright 2025, Mikhael Danilov <mikhael.danilov@gmail.com>
+ *
+ * This file is part of Tiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include "rpd_global.h"
 
@@ -86,4 +105,3 @@ protected:
 
 } // namespace Rpd
 
-#endif // RPDPLUGIN_H
