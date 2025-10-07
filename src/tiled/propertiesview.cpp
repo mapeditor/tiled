mapeditor/tiled

commit b081934a7fa12c2df0fc0213d59de3200d030896

Allow JS expressions in number inputs (#4234)
Replaced QSpinBox and QDoubleSpinBox throughout the app, including in
script dialogs and properties view, with subclasses that allows setting
a value using a JavaScript expression.

Any prefix or suffix is temporarily hidden while a spin box has focus 
because it gets in the way or is automatically added when it shouldn't.
This way we also don't need to parse them out before evaluating the
text.

QSpinBox::setValue and QDoubleSpinBox::setValue cause the text to get
reset to the current value, but we don't want to do this while the user
is still typing. So in the Properties view we now compare values first
to not call setValue needlessly.

We don't evaluate the expression in the validate implementation because
this is called a bit too much to be executing JS. It also gets called
when plain number values are set on the spin box, for example during
initialization.

Co-authored-by: Thorbjørn Lindeijer <bjorn@lindeijer.nl>

src/tiled/propertiesview.cpp
@@ -20,6 +20,7 @@
 
 #include "propertiesview.h"
 
+#include "expressionspinbox.h"
 #include "fileedit.h"
 #include "textpropertyedit.h"
 #include "utils.h"
@@ -39,7 +40,6 @@
 #include <QPointer>
 #include <QResizeEvent>
 #include <QSpacerItem>
-#include <QSpinBox>
 #include <QToolButton>
 
 namespace Tiled {
@@ -190,8 +190,7 @@ QWidget *StringProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [=] {
         // Avoid affecting cursor position when the text is the same
-        const QString v = value();
-        if (editor->text() != v)
+        if (const auto v = value(); editor->text() != v)
             editor->setText(v);
     };
     syncEditor();
@@ -272,7 +271,7 @@ QWidget *IntProperty::createEditor(QWidget *parent)
         connect(slider, &QSlider::valueChanged, this, &IntProperty::setValue);
     }
 
-    auto spinBox = new SpinBox(parent);
+    auto spinBox = new ExpressionSpinBox(parent);
     spinBox->setRange(m_minimum, m_maximum);
     spinBox->setSingleStep(m_singleStep);
     spinBox->setSuffix(m_suffix);
@@ -282,7 +281,8 @@ QWidget *IntProperty::createEditor(QWidget *parent)
 
     connect(this, &Property::valueChanged, spinBox, [this, spinBox] {
         const QSignalBlocker blocker(spinBox);
-        spinBox->setValue(value());
+        if (const auto v = value(); spinBox->value() != v)
+            spinBox->setValue(v);
     });
     connect(spinBox, &SpinBox::valueChanged, this, &IntProperty::setValue);
 
@@ -298,7 +298,8 @@ QWidget *FloatProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [=] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -357,7 +358,8 @@ QWidget *PointProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -376,7 +378,8 @@ QWidget *PointFProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -396,7 +399,8 @@ QWidget *SizeProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -413,7 +417,8 @@ QWidget *SizeFProperty::createEditor(QWidget *parent)
     auto editor = new SizeFEdit(parent);
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -432,7 +437,8 @@ QWidget *RectProperty::createEditor(QWidget *parent)
 
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -459,7 +465,8 @@ QWidget *RectFProperty::createEditor(QWidget *parent)
     auto editor = new RectFEdit(parent);
     auto syncEditor = [this, editor] {
         const QSignalBlocker blocker(editor);
-        editor->setValue(value());
+        if (const auto v = value(); editor->value() != v)
+            editor->setValue(v);
     };
     syncEditor();
 
@@ -497,7 +504,7 @@ QWidget *FontProperty::createEditor(QWidget *parent)
     auto editor = new QWidget(parent);
     auto fontComboBox = new QFontComboBox(editor);
 
-    auto sizeSpinBox = new QSpinBox(editor);
+    auto sizeSpinBox = new ExpressionSpinBox(editor);
     sizeSpinBox->setRange(1, 999);
     sizeSpinBox->setSuffix(tr(" px"));
     sizeSpinBox->setKeyboardTracking(false);

src/tiled/expressionspinbox.cpp
@@ -1,0 +1,137 @@
+/*
+ * expressionspinbox.cpp
+ * Copyright 2025, dogboydog
+ *
+ * This file is part of Tiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "expressionspinbox.h"
+
+#include <QJSEngine>
+
+namespace Tiled {
+
+QJSEngine *ExpressionEvaluator::mEngine;
+
+void ExpressionEvaluator::deleteInstance()
+{
+    delete mEngine;
+    mEngine = nullptr;
+}
+
+QJSValue ExpressionEvaluator::evaluate(const QString &program)
+{
+    if (!mEngine)
+        mEngine = new QJSEngine;
+    return mEngine->evaluate(program);
+}
+
+
+// ExpressionSpinBox
+
+ExpressionSpinBox::ExpressionSpinBox(QWidget *parent)
+    : QSpinBox(parent)
+{}
+
+int ExpressionSpinBox::valueFromText(const QString &text) const
+{
+    const QJSValue result = ExpressionEvaluator::evaluate(text);
+    if (result.isNumber())
+        return result.toNumber();
+
+    return value();
+}
+
+QValidator::State ExpressionSpinBox::validate(QString &/*text*/, int &/*pos*/) const
+{
+    return QValidator::Acceptable;
+}
+
+void ExpressionSpinBox::focusInEvent(QFocusEvent *event)
+{
+    // Remember current prefix/suffix and remove them
+    mPrefix = prefix();
+    mSuffix = suffix();
+    if (!mPrefix.isEmpty())
+        setPrefix(QString());
+    if (!mSuffix.isEmpty())
+        setSuffix(QString());
+
+    QSpinBox::focusInEvent(event);
+}
+
+void ExpressionSpinBox::focusOutEvent(QFocusEvent *event)
+{
+    QSpinBox::focusOutEvent(event);
+
+    // Restore any previously removed prefix/suffix
+    if (!mPrefix.isEmpty())
+        setPrefix(mPrefix);
+    if (!mSuffix.isEmpty())
+        setSuffix(mSuffix);
+    mPrefix.clear();
+    mSuffix.clear();
+}
+
+// ExpressionDoubleSpinBox
+
+ExpressionDoubleSpinBox::ExpressionDoubleSpinBox(QWidget *parent)
+    : QDoubleSpinBox(parent)
+{}
+
+double ExpressionDoubleSpinBox::valueFromText(const QString &text) const
+{
+    const QJSValue result = ExpressionEvaluator::evaluate(text);
+    if (result.isNumber())
+        return result.toNumber();
+
+    return value();
+}
+
+QValidator::State ExpressionDoubleSpinBox::validate(QString &/*text*/, int &/*pos*/) const
+{
+    return QValidator::Acceptable;
+}
+
+void ExpressionDoubleSpinBox::focusInEvent(QFocusEvent *event)
+{
+    // Remember current prefix/suffix and remove them
+    mPrefix = prefix();
+    mSuffix = suffix();
+    if (!mPrefix.isEmpty())
+        setPrefix(QString());
+    if (!mSuffix.isEmpty())
+        setSuffix(QString());
+
+    QDoubleSpinBox::focusInEvent(event);
+}
+
+void ExpressionDoubleSpinBox::focusOutEvent(QFocusEvent *event)
+{
+    QDoubleSpinBox::focusOutEvent(event);
+
+    // Restore any previously removed prefix/suffix
+    if (!mPrefix.isEmpty())
+        setPrefix(mPrefix);
+    if (!mSuffix.isEmpty())
+        setSuffix(mSuffix);
+    mPrefix.clear();
+    mSuffix.clear();
+}
+
+} // namespace Tiled
+
+#include "moc_expressionspinbox.cpp"

src/tiled/expressionspinbox.h
@@ -1,0 +1,82 @@
+/*
+ * expressionspinbox.h
+ * Copyright 2025, dogboydog
+ *
+ * This file is part of Tiled.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QJSValue>
+#include <QSpinBox>
+
+namespace Tiled {
+
+/**
+ * @brief The ExpressionEvaluator class can evaluate simple Javascript expressions
+ *        that do not require access to the Tiled scripting API, nor the Tiled project.
+ */
+class ExpressionEvaluator
+{
+public:
+    static void deleteInstance();
+    static QJSValue evaluate(const QString &program);
+
+private:
+    static QJSEngine *mEngine;
+};
+
+
+class ExpressionSpinBox : public QSpinBox
+{
+    Q_OBJECT
+
+public:
+    ExpressionSpinBox(QWidget *parent);
+
+protected:
+    int valueFromText(const QString &text) const override;
+    QValidator::State validate(QString &text, int &pos) const override;
+
+    void focusInEvent(QFocusEvent *event) override;
+    void focusOutEvent(QFocusEvent *event) override;
+
+private:
+    QString mPrefix;
+    QString mSuffix;
+};
+
+
+class ExpressionDoubleSpinBox : public QDoubleSpinBox
+{
+    Q_OBJECT
+
+public:
+    ExpressionDoubleSpinBox(QWidget *parent);
+
+protected:
+    double valueFromText(const QString &text) const override;
+    QValidator::State validate(QString &text, int &pos) const override;
+
+    void focusInEvent(QFocusEvent *event) override;
+    void focusOutEvent(QFocusEvent *event) override;
+
+private:
+    QString mPrefix;
+    QString mSuffix;
+};
+
+} // namespace Tiled

src/tiled/libtilededitor.qbs
@@ -247,6 +247,8 @@ DynamicLibrary {
         "exportasimagedialog.ui",
         "exporthelper.cpp",
         "exporthelper.h",
+        "expressionspinbox.cpp",
+        "expressionspinbox.h",
         "filechangedwarning.cpp",
         "filechangedwarning.h",
         "fileedit.cpp",

src/tiled/newmapdialog.ui
@@ -15,7 +15,7 @@
   </property>
   <layout class="QGridLayout" name="gridLayout_3">
    <property name="sizeConstraint">
-    <enum>QLayout::SetFixedSize</enum>
+    <enum>QLayout::SizeConstraint::SetFixedSize</enum>
    </property>
    <item row="0" column="0" colspan="2">
     <widget class="QGroupBox" name="groupBox">
@@ -91,7 +91,7 @@
       <item>
        <layout class="QGridLayout" name="gridLayout_2">
         <item row="1" column="2">
-         <widget class="QSpinBox" name="mapHeight">
+         <widget class="Tiled::ExpressionSpinBox" name="mapHeight">
           <property name="suffix">
            <string> tiles</string>
           </property>
@@ -117,7 +117,7 @@
          </widget>
         </item>
         <item row="0" column="2">
-         <widget class="QSpinBox" name="mapWidth">
+         <widget class="Tiled::ExpressionSpinBox" name="mapWidth">
           <property name="suffix">
            <string extracomment="Remember starting with a space."> tiles</string>
           </property>
@@ -152,10 +152,10 @@
         <item row="1" column="0">
          <spacer name="fixedSizeSpacer">
           <property name="orientation">
-           <enum>Qt::Horizontal</enum>
+           <enum>Qt::Orientation::Horizontal</enum>
           </property>
           <property name="sizeType">
-           <enum>QSizePolicy::Fixed</enum>
+           <enum>QSizePolicy::Policy::Fixed</enum>
           </property>
           <property name="sizeHint" stdset="0">
            <size>
@@ -194,7 +194,7 @@
        </widget>
       </item>
       <item row="0" column="1">
-       <widget class="QSpinBox" name="tileWidth">
+       <widget class="Tiled::ExpressionSpinBox" name="tileWidth">
         <property name="suffix">
          <string extracomment="Remember starting with a space."> px</string>
         </property>
@@ -220,7 +220,7 @@
        </widget>
       </item>
       <item row="1" column="1">
-       <widget class="QSpinBox" name="tileHeight">
+       <widget class="Tiled::ExpressionSpinBox" name="tileHeight">
         <property name="suffix">
          <string> px</string>
         </property>
@@ -238,7 +238,7 @@
       <item row="2" column="0" colspan="2">
        <spacer name="verticalSpacer_2">
         <property name="orientation">
-         <enum>Qt::Vertical</enum>
+         <enum>Qt::Orientation::Vertical</enum>
         </property>
         <property name="sizeHint" stdset="0">
          <size>
@@ -254,7 +254,7 @@
    <item row="2" column="0">
     <spacer name="verticalSpacer">
      <property name="orientation">
-      <enum>Qt::Vertical</enum>
+      <enum>Qt::Orientation::Vertical</enum>
      </property>
      <property name="sizeHint" stdset="0">
       <size>
@@ -267,15 +267,22 @@
    <item row="3" column="0" colspan="2">
     <widget class="QDialogButtonBox" name="buttonBox">
      <property name="orientation">
-      <enum>Qt::Horizontal</enum>
+      <enum>Qt::Orientation::Horizontal</enum>
      </property>
      <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
+      <set>QDialogButtonBox::StandardButton::Cancel|QDialogButtonBox::StandardButton::Ok</set>
      </property>
     </widget>
    </item>
   </layout>
  </widget>
+ <customwidgets>
+  <customwidget>
+   <class>Tiled::ExpressionSpinBox</class>
+   <extends>QSpinBox</extends>
+   <header>expressionspinbox.h</header>
+  </customwidget>
+ </customwidgets>
  <tabstops>
   <tabstop>orientation</tabstop>
   <tabstop>layerFormat</tabstop>

src/tiled/newtilesetdialog.ui
@@ -15,7 +15,7 @@
   </property>
   <layout class="QVBoxLayout" name="verticalLayout">
    <property name="sizeConstraint">
-    <enum>QLayout::SetMinAndMaxSize</enum>
+    <enum>QLayout::SizeConstraint::SetMinAndMaxSize</enum>
    </property>
    <item>
     <widget class="QGroupBox" name="tilesetGroupBox">
@@ -79,7 +79,7 @@
       <item row="1" column="2">
        <spacer name="horizontalSpacer_4">
         <property name="orientation">
-         <enum>Qt::Horizontal</enum>
+         <enum>Qt::Orientation::Horizontal</enum>
         </property>
         <property name="sizeHint" stdset="0">
          <size>
@@ -151,7 +151,7 @@
       <item row="1" column="4">
        <spacer name="horizontalSpacer_2">
         <property name="orientation">
-         <enum>Qt::Horizontal</enum>
+         <enum>Qt::Orientation::Horizontal</enum>
         </property>
         <property name="sizeHint" stdset="0">
          <size>
@@ -164,7 +164,7 @@
       <item row="2" column="0" colspan="6">
        <layout class="QGridLayout" name="gridLayout_3">
         <item row="1" column="1">
-         <widget class="QSpinBox" name="tileHeight">
+         <widget class="Tiled::ExpressionSpinBox" name="tileHeight">
           <property name="suffix">
            <string> px</string>
           </property>
@@ -189,10 +189,10 @@
         <item row="1" column="2">
          <spacer name="horizontalSpacer">
           <property name="orientation">
-           <enum>Qt::Horizontal</enum>
+           <enum>Qt::Orientation::Horizontal</enum>
           </property>
           <property name="sizeType">
-           <enum>QSizePolicy::Preferred</enum>
+           <enum>QSizePolicy::Policy::Preferred</enum>
           </property>
           <property name="sizeHint" stdset="0">
            <size>
@@ -203,7 +203,7 @@
          </spacer>
         </item>
         <item row="0" column="1">
-         <widget class="QSpinBox" name="tileWidth">
+         <widget class="Tiled::ExpressionSpinBox" name="tileWidth">
           <property name="enabled">
            <bool>true</bool>
           </property>
@@ -232,7 +232,7 @@
          </widget>
         </item>
         <item row="0" column="4">
-         <widget class="QSpinBox" name="margin">
+         <widget class="Tiled::ExpressionSpinBox" name="margin">
           <property name="suffix">
            <string> px</string>
           </property>
@@ -248,7 +248,7 @@
          </widget>
         </item>
         <item row="1" column="4">
-         <widget class="QSpinBox" name="spacing">
+         <widget class="Tiled::ExpressionSpinBox" name="spacing">
           <property name="suffix">
            <string> px</string>
           </property>
@@ -283,7 +283,7 @@
         <item row="1" column="5">
          <spacer name="horizontalSpacer_3">
           <property name="orientation">
-           <enum>Qt::Horizontal</enum>
+           <enum>Qt::Orientation::Horizontal</enum>
           </property>
           <property name="sizeHint" stdset="0">
            <size>
@@ -321,10 +321,10 @@
    <item>
     <widget class="QDialogButtonBox" name="buttonBox">
      <property name="orientation">
-      <enum>Qt::Horizontal</enum>
+      <enum>Qt::Orientation::Horizontal</enum>
      </property>
      <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
+      <set>QDialogButtonBox::StandardButton::Cancel|QDialogButtonBox::StandardButton::Ok</set>
      </property>
     </widget>
    </item>
@@ -335,6 +335,11 @@
    <class>Tiled::ColorButton</class>
    <extends>QToolButton</extends>
    <header>colorbutton.h</header>
+  </customwidget>
+  <customwidget>
+   <class>Tiled::ExpressionSpinBox</class>
+   <extends>QSpinBox</extends>
+   <header>expressionspinbox.h</header>
   </customwidget>
  </customwidgets>
  <tabstops>

src/tiled/offsetmapdialog.ui
@@ -21,7 +21,7 @@
   </property>
   <layout class="QVBoxLayout" name="verticalLayout_5">
    <property name="sizeConstraint">
-    <enum>QLayout::SetFixedSize</enum>
+    <enum>QLayout::SizeConstraint::SetFixedSize</enum>
    </property>
    <item>
     <widget class="QGroupBox" name="offsetGroup">
@@ -37,7 +37,7 @@
        </widget>
       </item>
       <item row="2" column="2">
-       <widget class="QSpinBox" name="xOffset">
+       <widget class="Tiled::ExpressionSpinBox" name="xOffset">
         <property name="enabled">
          <bool>true</bool>
         </property>
@@ -72,10 +72,10 @@
       <item row="4" column="1">
        <spacer name="horizontalSpacer">
         <property name="orientation">
-         <enum>Qt::Horizontal</enum>
+         <enum>Qt::Orientation::Horizontal</enum>
         </property>
         <property name="sizeType">
-         <enum>QSizePolicy::Fixed</enum>
+         <enum>QSizePolicy::Policy::Fixed</enum>
         </property>
         <property name="sizeHint" stdset="0">
          <size>
@@ -86,7 +86,7 @@
        </spacer>
       </item>
       <item row="4" column="2">
-       <widget class="QSpinBox" name="yOffset">
+       <widget class="Tiled::ExpressionSpinBox" name="yOffset">
         <property name="suffix">
          <string> tiles</string>
         </property>
@@ -161,10 +161,10 @@
    <item>
     <spacer name="verticalSpacer">
      <property name="orientation">
-      <enum>Qt::Vertical</enum>
+      <enum>Qt::Orientation::Vertical</enum>
      </property>
      <property name="sizeType">
-      <enum>QSizePolicy::MinimumExpanding</enum>
+      <enum>QSizePolicy::Policy::MinimumExpanding</enum>
      </property>
      <property name="sizeHint" stdset="0">
       <size>
@@ -177,15 +177,22 @@
    <item>
     <widget class="QDialogButtonBox" name="buttonBox">
      <property name="orientation">
-      <enum>Qt::Horizontal</enum>
+      <enum>Qt::Orientation::Horizontal</enum>
      </property>
      <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
+      <set>QDialogButtonBox::StandardButton::Cancel|QDialogButtonBox::StandardButton::Ok</set>
      </property>
     </widget>
    </item>
   </layout>
  </widget>
+ <customwidgets>
+  <customwidget>
+   <class>Tiled::ExpressionSpinBox</class>
+   <extends>QSpinBox</extends>
+   <header>expressionspinbox.h</header>
+  </customwidget>
+ </customwidgets>
  <resources/>
  <connections>
   <connection>

src/tiled/preferencesdialog.ui
@@ -15,7 +15,7 @@
   </property>
   <layout class="QVBoxLayout" name="verticalLayout">
    <property name="sizeConstraint">
-    <enum>QLayout::SetMinimumSize</enum>
+    <enum>QLayout::SizeConstraint::SetMinimumSize</enum>
    </property>
    <item>
     <widget class="QTabWidget" name="tabWidget">
@@ -130,7 +130,7 @@
           <item>
            <spacer name="horizontalSpacer_3">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -155,7 +155,7 @@
                <string>(&lt;a href=&quot;https://www.mapeditor.org/crash-reporting&quot;&gt;more information&lt;/a&gt;)</string>
               </property>
               <property name="textFormat">
-               <enum>Qt::RichText</enum>
+               <enum>Qt::TextFormat::RichText</enum>
               </property>
               <property name="buddy">
                <cstring>sendCrashReports</cstring>
@@ -170,7 +170,7 @@
        <item>
         <spacer name="verticalSpacer">
          <property name="orientation">
-          <enum>Qt::Vertical</enum>
+          <enum>Qt::Orientation::Vertical</enum>
          </property>
          <property name="sizeHint" stdset="0">
           <size>
@@ -196,7 +196,7 @@
           <item row="4" column="3">
            <spacer name="horizontalSpacer_5">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -209,7 +209,7 @@
           <item row="0" column="2" colspan="2">
            <spacer name="horizontalSpacer_2">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -266,7 +266,7 @@
           <item row="4" column="1" colspan="2">
            <layout class="QHBoxLayout" name="horizontalLayout_3">
             <item>
-             <widget class="QSpinBox" name="gridMajorX">
+             <widget class="Tiled::ExpressionSpinBox" name="gridMajorX">
               <property name="suffix">
                <string> tiles</string>
               </property>
@@ -286,7 +286,7 @@
              </widget>
             </item>
             <item>
-             <widget class="QSpinBox" name="gridMajorY">
+             <widget class="Tiled::ExpressionSpinBox" name="gridMajorY">
               <property name="suffix">
                <string> tiles</string>
               </property>
@@ -321,7 +321,7 @@
            <widget class="QComboBox" name="languageCombo"/>
           </item>
           <item row="5" column="1" colspan="2">
-           <widget class="QDoubleSpinBox" name="objectLineWidth">
+           <widget class="Tiled::ExpressionDoubleSpinBox" name="objectLineWidth">
             <property name="suffix">
              <string> pixels</string>
             </property>
@@ -337,7 +337,7 @@
            </widget>
           </item>
           <item row="3" column="1" colspan="2">
-           <widget class="QSpinBox" name="gridFine">
+           <widget class="Tiled::ExpressionSpinBox" name="gridFine">
             <property name="minimum">
              <number>1</number>
             </property>
@@ -378,7 +378,7 @@
           <item row="0" column="2">
            <spacer name="horizontalSpacer_6">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -429,7 +429,7 @@
        <item>
         <spacer name="verticalSpacer_3">
          <property name="orientation">
-          <enum>Qt::Vertical</enum>
+          <enum>Qt::Orientation::Vertical</enum>
          </property>
          <property name="sizeHint" stdset="0">
           <size>
@@ -460,10 +460,10 @@
           <item row="0" column="1">
            <spacer name="horizontalSpacer_4">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeType">
-             <enum>QSizePolicy::Fixed</enum>
+             <enum>QSizePolicy::Policy::Fixed</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -502,7 +502,7 @@
           <item row="0" column="3">
            <spacer name="horizontalSpacer">
             <property name="orientation">
-             <enum>Qt::Horizontal</enum>
+             <enum>Qt::Orientation::Horizontal</enum>
             </property>
             <property name="sizeHint" stdset="0">
              <size>
@@ -561,7 +561,7 @@
            </widget>
           </item>
           <item>
-           <widget class="QSpinBox" name="fontSize">
+           <widget class="Tiled::ExpressionSpinBox" name="fontSize">
             <property name="minimum">
              <number>6</number>
             </property>
@@ -573,7 +573,7 @@
        <item>
         <spacer name="verticalSpacer_2">
          <property name="orientation">
-          <enum>Qt::Vertical</enum>
+          <enum>Qt::Orientation::Vertical</enum>
          </property>
          <property name="sizeHint" stdset="0">
           <size>
@@ -639,10 +639,10 @@
    <item>
     <widget class="QDialogButtonBox" name="buttonBox">
      <property name="orientation">
-      <enum>Qt::Horizontal</enum>
+      <enum>Qt::Orientation::Horizontal</enum>
      </property>
      <property name="standardButtons">
-      <set>QDialogButtonBox::Close</set>
+      <set>QDialogButtonBox::StandardButton::Close</set>
      </property>
     </widget>
    </item>
@@ -655,10 +655,20 @@
    <header>colorbutton.h</header>
   </customwidget>
   <customwidget>
+   <class>Tiled::ExpressionSpinBox</class>
+   <extends>QSpinBox</extends>
+   <header>expressionspinbox.h</header>
+  </customwidget>
+  <customwidget>
    <class>Tiled::ShortcutSettingsPage</class>
    <extends>QWidget</extends>
    <header>shortcutsettingspage.h</header>
    <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>Tiled::ExpressionDoubleSpinBox</class>
+   <extends>QDoubleSpinBox</extends>
+   <header>expressionspinbox.h</header>
   </customwidget>
  </customwidgets>
  <tabstops>

commit 32da598969906b8554122845dad5e839d3006ae9

Fixed wheel behavior on spin boxes in Properties view
Small regression in b081934a7fa12c2df0fc0213d59de3200d030896 caused the
mouse wheel to affect the value of unfocused spin boxes again.

src/tiled/propertiesview.cpp
@@ -283,7 +283,7 @@ QWidget *IntProperty::createEditor(QWidget *parent)
         connect(slider, &QSlider::valueChanged, this, &IntProperty::setValue);
     }
 
-    auto spinBox = new ExpressionSpinBox(parent);
+    auto spinBox = new SpinBox(parent);
     spinBox->setRange(m_minimum, m_maximum);
     spinBox->setSingleStep(m_singleStep);
     spinBox->setSuffix(m_suffix);
@@ -516,7 +516,7 @@ QWidget *FontProperty::createEditor(QWidget *parent)
     auto editor = new QWidget(parent);
     auto fontComboBox = new QFontComboBox(editor);
 
-    auto sizeSpinBox = new ExpressionSpinBox(editor);
+    auto sizeSpinBox = new SpinBox(editor);
     sizeSpinBox->setRange(1, 999);
     sizeSpinBox->setSuffix(tr(" px"));
     sizeSpinBox->setKeyboardTracking(false);