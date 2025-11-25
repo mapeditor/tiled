564a65c5f101f62477d7323fe73edfddfcb4b3f7
lukes5316 <130879232+lukes5316@users.noreply.github.com>
2025-11-25 11:38:02 +0000

    clean up after merge conflict / slight restructure of some parts of code / removal of redundant code

--- src/tiled/contentbrowserdock.cpp
+++ src/tiled/contentbrowserdock.cpp
@@ -2,8 +2,6 @@
 
 #include "documentmanager.h"
 #include "mainwindow.h"
-#include "projectdock.h"
-#include "project.h"
 
 #include <QFileSystemModel>
 #include <QTreeView>
@@ -15,15 +13,23 @@
 #include <QDir>
 #include <QMimeData>
 #include <QDrag>
-#include <QMouseEvent>
 #include <QPixmap>
+#include <QSettings>
+#include <QCoreApplication>
+#include <QDebug>
 
 namespace Tiled {
 
 ContentBrowserDock::ContentBrowserDock(QWidget *parent)
     : QDockWidget(tr("Content Browser"), parent)
 {
+    setFeatures(QDockWidget::DockWidgetClosable |
+                QDockWidget::DockWidgetMovable |
+                QDockWidget::DockWidgetFloatable);
 
+    //
+    // File Model
+    //
     mFileModel = new QFileSystemModel(this);
     mFileModel->setFilter(QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot);
     mFileModel->setNameFilterDisables(false);
@@ -39,8 +45,9 @@ ContentBrowserDock::ContentBrowserDock(QWidget *parent)
     };
     mFileModel->setNameFilters(nameFilters);
 
-    setFeatures(QDockWidget::DockWidgetClosable | QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable);    mTreeView = new QTreeView(this);
-
+    //
+    // Proxy Models (Tree + Details)
+    //
     mTreeProxy = new QSortFilterProxyModel(this);
     mTreeProxy->setSourceModel(mFileModel);
     mTreeProxy->setFilterCaseSensitivity(Qt::CaseInsensitive);
@@ -49,30 +56,44 @@ ContentBrowserDock::ContentBrowserDock(QWidget *parent)
     mDetailsProxy->setSourceModel(mFileModel);
     mDetailsProxy->setFilterCaseSensitivity(Qt::CaseInsensitive);
 
+    //
+    // Filter Box
+    //
     mFilterEdit = new QLineEdit(this);
     mFilterEdit->setPlaceholderText(tr("Filter assets..."));
-    //we use to have just 1 proxy model now we have 2 (tree and list) therefore we must rename the creation of the ui widgets and make 2 1 for the tree view (which we had) and a new one for the list view.
+
+    //
+    // Tree View
+    //
     mTreeView = new QTreeView(this);
     mTreeView->setHeaderHidden(true);
     mTreeView->setSelectionMode(QAbstractItemView::SingleSelection);
     mTreeView->setDragEnabled(true);
     mTreeView->setModel(mTreeProxy);
 
+    //
+    // Details (List) View
+    //
     mDetailsView = new QListView(this);
     mDetailsView->setViewMode(QListView::IconMode);
     mDetailsView->setResizeMode(QListView::Adjust);
     mDetailsView->setIconSize(QSize(96, 96));
-    mDetailsView->setGridSize(QSize(128,128));
+    mDetailsView->setGridSize(QSize(128, 128));
     mDetailsView->setSpacing(12);
     mDetailsView->setDragEnabled(true);
     mDetailsView->setModel(mDetailsProxy);
 
+    //
+    // Split View
+    //
     mSplitter = new QSplitter(Qt::Horizontal, this);
     mSplitter->addWidget(mTreeView);
     mSplitter->addWidget(mDetailsView);
-    mSplitter->setSizes({200,600});
+    mSplitter->setSizes({200, 600});
 
-
+    //
+    // Layout
+    //
     auto *mainLayout = new QVBoxLayout;
     mainLayout->setContentsMargins(4, 4, 4, 4);
     mainLayout->setSpacing(4);
@@ -83,34 +104,52 @@ ContentBrowserDock::ContentBrowserDock(QWidget *parent)
     container->setLayout(mainLayout);
     setWidget(container);
 
-    connect(mFilterEdit, &QLineEdit::textChanged, this, &ContentBrowserDock::onFilterChanged);
-    connect(mTreeView->selectionModel(), &QItemSelectionModel::currentChanged, this, [this](const QModelIndex &current, const QModelIndex &)
-    {
-        if(current.isValid())
-            onTreeClicked(current);
-    });
-    connect(mDetailsView, &QListView::doubleClicked, this, &ContentBrowserDock::onDetailsDoubleClicked);
+    //
+    // Connections
+    //
+    connect(mFilterEdit, &QLineEdit::textChanged,
+            this, &ContentBrowserDock::onFilterChanged);
 
-    QString rootPath = QCoreApplication::applicationDirPath() + QStringLiteral("/assets");  // Relative to Tiled.exe
+    connect(mTreeView->selectionModel(), &QItemSelectionModel::currentChanged,
+            this, [this](const QModelIndex &current, const QModelIndex &) {
+                if (current.isValid())
+                    onTreeClicked(current);
+            });
 
-    // Optional: Override with a config file (e.g., for your game project)
-    QSettings settings(QStringLiteral("YourGameEngine"), QStringLiteral("ContentBrowser"));  // Change "YourGameEngine" to your app name
+    connect(mDetailsView, &QListView::doubleClicked,
+            this, &ContentBrowserDock::onDetailsDoubleClicked);
+
+    //
+    // Determine root assets path
+    //
+    QString rootPath = QCoreApplication::applicationDirPath() + QStringLiteral("/assets");
+
+    QSettings settings(QStringLiteral("IonixEngine2025"), QStringLiteral("ContentBrowser"));
     QString customRoot = settings.value("projectAssetsPath").toString();
-    if (!customRoot.isEmpty() && QDir(customRoot).exists()) {
+
+    if (!customRoot.isEmpty() && QDir(customRoot).exists())
+    {
         rootPath = customRoot;
         qDebug() << "Using custom project path:" << rootPath;
     }
 
-    if (!QDir(rootPath).exists()) {
-        rootPath = QDir::currentPath();  // Fallback to current dir
+    if (!QDir(rootPath).exists())
+    {
+        rootPath = QDir::currentPath();
         qDebug() << "Assets folder not found, using current dir:" << rootPath;
     }
 
+    //
+    // Apply root to views
+    //
     mFileModel->setRootPath(rootPath);
     mTreeView->setRootIndex(mTreeProxy->mapFromSource(mFileModel->index(rootPath)));
     navigateToFolder(rootPath);
 }
 
+//
+// Navigation
+//
 void ContentBrowserDock::navigateToFolder(const QString &path)
 {
     mCurrentFolder = path;
@@ -120,81 +159,53 @@ void ContentBrowserDock::navigateToFolder(const QString &path)
     QModelIndex detailsIndex = mDetailsProxy->mapFromSource(sourceIndex);
 
     mTreeView->setCurrentIndex(treeIndex);
-    mDetailsView->setCurrentIndex(detailsIndex);
+    mDetailsView->setRootIndex(detailsIndex);
 }
 
+//
+// Tree click (folder navigation)
+//
 void ContentBrowserDock::onTreeClicked(const QModelIndex &proxyIndex)
 {
     QModelIndex sourceIndex = mTreeProxy->mapToSource(proxyIndex);
     QString path = mFileModel->filePath(sourceIndex);
 
-    if(QFileInfo(path).isDir())
-    {
+    if (QFileInfo(path).isDir())
         navigateToFolder(path);
-    }
 }
 
+//
+// Details double-click (open file OR enter folder)
+//
 void ContentBrowserDock::onDetailsDoubleClicked(const QModelIndex &proxyIndex)
 {
     QModelIndex sourceIndex = mDetailsProxy->mapToSource(proxyIndex);
     QString path = mFileModel->filePath(sourceIndex);
     QFileInfo info(path);
 
-    if(info.isDir())
+    if (info.isDir())
     {
         navigateToFolder(path);
         return;
     }
 
-    #include "documentmanager.h"
-    #include "mainwindow.h"
-    using namespace Tiled;
     DocumentManager::instance()->openFile(path);
 
-    if(auto *mainWindow = Tiled::MainWindow::instance())
-    {
+    if (auto *mainWindow = MainWindow::instance())
         mainWindow->raise();
-    }
 }
+
+//
+// Filtering
+//
 void ContentBrowserDock::onFilterChanged(const QString &text)
 {
-    QString pattern = text.isEmpty() ? QString() : QStringLiteral("*") + text + QStringLiteral("*");
+    QString pattern = text.isEmpty()
+                      ? QString()
+                      : QStringLiteral("*") + text + QStringLiteral("*");
+
     mTreeProxy->setFilterWildcard(pattern);
     mDetailsProxy->setFilterWildcard(pattern);
 }
 
-
-class TreeViewWithDrag : public QTreeView
-{
-protected:
-    void startDrag(Qt::DropActions supportedActions) override
-    {
-        Q_UNUSED(supportedActions);
-        QModelIndex index = currentIndex();
-        if (!index.isValid())
-            return;
-
-        QSortFilterProxyModel *proxy = qobject_cast<QSortFilterProxyModel*>(model());
-        QFileSystemModel *fsModel = qobject_cast<QFileSystemModel*>(proxy->sourceModel());
-        QString path = fsModel->filePath(proxy->mapToSource(index));
-
-        QMimeData *mime = new QMimeData;
-        mime->setUrls({QUrl::fromLocalFile(path)});
-
-        QDrag *drag = new QDrag(this);
-        drag->setMimeData(mime);
-
-        // optional thumbnail - something to add later
-        if (path.endsWith(QStringLiteral(".png"), Qt::CaseInsensitive) ||
-            path.endsWith(QStringLiteral(".jpg"), Qt::CaseInsensitive)) {
-            QPixmap pix(path);
-            if (!pix.isNull())
-                drag->setPixmap(pix.scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-        }
-
-        drag->exec(Qt::CopyAction);
-    }
-};
-
-}
-
+} // namespace Tiled
