45b806585bbe8325d8c54cccb7c61ceaac9c871c
Roland <122157211+Kiamo2@users.noreply.github.com>
2026-02-11 21:19:43 +0100

    Fixed alpha component of tint color not applying correctly to opaque images (#4310)
    Closes #2974
    
    Co-authored-by: Thorbjørn Lindeijer <bjorn@lindeijer.nl>

--- src/libtiled/maprenderer.cpp
+++ src/libtiled/maprenderer.cpp
@@ -107,12 +107,20 @@ static QPixmap tinted(const QPixmap &pixmap, const QRect &rect, const QColor &co
         return *cached;
 
     QPixmap resultImage = pixmap.copy(rect);
+
+    // tinting with a non-fully opaque color needs an alpha channel to work properly
+    if (color.alpha() < 255 && !resultImage.hasAlphaChannel()) {
+        auto imageWithAlpha = resultImage.toImage();
+        imageWithAlpha.convertTo(QImage::Format_ARGB32_Premultiplied);
+        resultImage = QPixmap::fromImage(std::move(imageWithAlpha), Qt::NoOpaqueDetection);
+    }
+
     QPainter painter(&resultImage);
 
     QColor fullOpacity = color;
     fullOpacity.setAlpha(255);
-    // tint the final color (this will will mess up the alpha which we will fix
-    // in the next lines)
+    // tint the final color (this will mess up the alpha which we will fix in
+    // the next lines)
     painter.setCompositionMode(QPainter::CompositionMode_Multiply);
     painter.fillRect(resultImage.rect(), fullOpacity);
 
