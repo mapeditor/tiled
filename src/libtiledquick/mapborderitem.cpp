e6f86ca3a9500a63f924147838fd03893dcd481b
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2026-03-04 19:00:40 -0600

    Added MapBorderItem component for rendering an overlaying map border

---
+++ src/libtiledquick/mapborderitem.cpp
@@ -0,0 +1,60 @@
+#include "mapborderitem.h"
+
+#include <QQuickWindow>
+#include <QSGFlatColorMaterial>
+
+using namespace TiledQuick;
+
+MapBorderItem::MapBorderItem(QQuickItem *parent)
+    : QQuickItem(parent)
+{
+    setFlag(ItemHasContents);
+}
+
+MapBorderItem::~MapBorderItem() = default;
+
+QSGNode *MapBorderItem::updatePaintNode(QSGNode *node, QQuickItem::UpdatePaintNodeData *)
+{
+    QSGGeometryNode *borderNode = static_cast<QSGGeometryNode *>(node);
+
+    if (!borderNode) {
+        borderNode = new QSGGeometryNode;
+
+        auto *geometry = new QSGGeometry(QSGGeometry::defaultAttributes_Point2D(), 5);
+        geometry->setDrawingMode(QSGGeometry::DrawLineStrip);
+
+        borderNode->setGeometry(geometry);
+        borderNode->setFlag(QSGNode::OwnsGeometry);
+
+        auto *vertex = geometry->vertexDataAsPoint2D();
+        vertex[0].set(0, 0);
+        vertex[1].set(width(), 0);
+        vertex[2].set(width(), height());
+        vertex[3].set(0, height());
+        vertex[4].set(0, 0);
+
+        auto *material = new QSGFlatColorMaterial;
+        material->setColor(Qt::black);
+
+        borderNode->setMaterial(material);
+        borderNode->setFlag(QSGNode::OwnsMaterial);
+
+        borderNode->markDirty(QSGNode::DirtyGeometry);
+    }
+
+    return borderNode;
+}
+
+void MapBorderItem::setColor(const QColor &c)
+{
+    if (mColor != c) {
+        mColor = c;
+        emit colorChanged();
+        update();
+    }
+}
+
+QColor MapBorderItem::color() const
+{
+    return mColor;
+}
---
+++ src/libtiledquick/mapborderitem.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QQuickItem>
+#include <QColor>
+
+#include "tiledquick_global.h"
+
+namespace Tiled {
+class MapRenderer;
+}
+
+namespace TiledQuick {
+
+class TILEDQUICK_SHARED_EXPORT MapBorderItem : public QQuickItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
+
+public:
+    explicit MapBorderItem(QQuickItem *parent = nullptr);
+    ~MapBorderItem() override;
+
+    QSGNode *updatePaintNode(QSGNode *node, QQuickItem::UpdatePaintNodeData *) override;
+
+    QColor color() const;
+    void setColor(const QColor &c);
+
+signals:
+    void colorChanged();
+
+private:
+    QColor mColor = Qt::black;
+};
+
+} // namespace TiledQuick
