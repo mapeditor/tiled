7309931ade646693857b6de0d0e7043abad5d647
whatever_kunaal <143793407+kunal649@users.noreply.github.com>
2026-03-25 01:21:20 +0530

    Fix status bar pixel coords being rounded instead of floored (#4426)
    Fixes #3833
    
    (cherry picked from commit 640ba2bf2265cf7f019dfb3905f16d634b0c1679)

--- src/tiled/abstractworldtool.cpp
+++ src/tiled/abstractworldtool.cpp
@@ -112,7 +112,7 @@ void AbstractWorldTool::mouseMoved(const QPointF &pos,
     if (Layer *layer = currentLayer())
         offsetPos -= mapScene()->absolutePositionForLayer(*layer);
 
-    const QPoint pixelPos = offsetPos.toPoint();
+    const QPoint pixelPos(qFloor(offsetPos.x()), qFloor(offsetPos.y()));
     const QPointF tilePosF = mapDocument()->renderer()->screenToTileCoords(offsetPos);
     const int x = qFloor(tilePosF.x());
     const int y = qFloor(tilePosF.y());
