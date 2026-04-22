mapeditor/tiled

commit 17a47f40107e3354b7103592e3261e55d22bc6df

Fix Properties view flicker when switching between objects (#4460)
Suppress updates on the scroll widget during the properties rebuild and
re-enable them with a double deferred QTimer::singleShot(0) call. This
ensures that all scheduled layout requests are fully processed before
the next paint.

src/tiled/propertieswidget.cpp
@@ -57,6 +57,7 @@
 #include <QMenu>
 #include <QPushButton>
 #include <QScopedValueRollback>
+#include <QTimer>
 #include <QToolBar>
 #include <QToolButton>
 #include <QUndoStack>
@@ -65,6 +66,41 @@
 #include <algorithm>
 
 namespace Tiled {
+
+/**
+ * Suppresses widget updates for the duration of a scope. Updates are
+ * re-enabled with a double-deferred timer to ensure all cascading layout
+ * requests have been processed before the next paint.
+ */
+class ScopedUpdatesDisabler
+{
+public:
+    explicit ScopedUpdatesDisabler(QWidget *widget)
+        : mWidget(widget)
+        , mHadUpdatesEnabled(widget->updatesEnabled())
+    {
+        widget->setUpdatesEnabled(false);
+    }
+
+    ~ScopedUpdatesDisabler()
+    {
+        if (!mHadUpdatesEnabled)
+            return;
+
+        QTimer::singleShot(0, mWidget, [w = mWidget] {
+            QTimer::singleShot(0, w, [w] {
+                w->setUpdatesEnabled(true);
+            });
+        });
+    }
+
+    Q_DISABLE_COPY_MOVE(ScopedUpdatesDisabler)
+
+private:
+    QWidget *mWidget;
+    bool mHadUpdatesEnabled;
+};
+
 
 template<> EnumData enumData<Alignment>()
 {
@@ -2390,6 +2426,8 @@ void PropertiesWidget::selectCustomProperty(const QString &name, bool focus)
 
 void PropertiesWidget::currentObjectChanged(Object *object)
 {
+    const ScopedUpdatesDisabler updatesDisabler(mPropertiesView);
+
     if (mPropertiesObject) {
         // Remember the expanded states
         const auto &subProperties = mPropertiesObject->subProperties();

commit 5b3bf7613d66b432313bd3aecf0f472415968d58

Fix Properties view getting stuck with updates disabled
Regression introduced in 17a47f40107e3354b7103592e3261e55d22bc6df.

When switching from a map to the tile collision editor and back while a
collision object was selected, the Properties view could stop updating
entirely.

ScopedUpdatesDisabler unconditionally called setUpdatesEnabled(false) on
the widget, but only called setUpdatesEnabled(true) when they were
previously enabled. However, updates can be disabled due to them being
disabled on a parent widget as well, in which case the updates could
remain explicitly disabled for the child widget.

Now we skip the disable when updates are already effectively disabled,
so they will effectively re-enable as well.

src/tiled/propertieswidget.cpp
@@ -76,15 +76,16 @@ class ScopedUpdatesDisabler
 {
 public:
     explicit ScopedUpdatesDisabler(QWidget *widget)
-        : mWidget(widget)
-        , mHadUpdatesEnabled(widget->updatesEnabled())
+        // If updates are already effectively disabled, leave the widget alone
+        : mWidget(widget->updatesEnabled() ? widget : nullptr)
     {
-        widget->setUpdatesEnabled(false);
+        if (mWidget)
+            mWidget->setUpdatesEnabled(false);
     }
 
     ~ScopedUpdatesDisabler()
     {
-        if (!mHadUpdatesEnabled)
+        if (!mWidget)
             return;
 
         QTimer::singleShot(0, mWidget, [w = mWidget] {
@@ -98,7 +99,6 @@ public:
 
 private:
     QWidget *mWidget;
-    bool mHadUpdatesEnabled;
 };
 
 