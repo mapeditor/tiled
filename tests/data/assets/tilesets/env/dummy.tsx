ccadfee2c7b415a5b74473cab274dc45f46d5a1d
Ryan Kurz <ryankurz0303@gmail.com>
2026-05-02 17:29:04 -0400

    Added mapreader tileset tests

---
+++ tests/data/assets/tilesets/env/dummy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dummy" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="dummy.png" width="224" height="224"/>
+</tileset>
